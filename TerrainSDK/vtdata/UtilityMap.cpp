f5ef6439d79814c6e7b1758bba88a321bb9afb83
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2001-12-15 01:28:46 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@165 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainSDK/vtdata/UtilityMap.cpp
@@ -0,0 +1,187 @@
+//
+// UtilityMap.cpp
+//
+// Copyright (c) 2001 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#include <stdlib.h>
+#include "UtilityMap.h"
+#include "vtdata/shapelib/shapefil.h"
+
+vtUtilityMap::vtUtilityMap()
+{
+}
+
+vtUtilityMap::~vtUtilityMap()
+{
+}
+
+
+bool vtUtilityMap::ImportPolesFromSHP(const char *fname)
+{
+	SHPHandle hSHP;
+    int		nEntities, nShapeType;
+    double 	adfMinBound[4], adfMaxBound[4];
+	DPoint2 point;
+
+	hSHP = SHPOpen(fname, "rb");
+	if (!hSHP)
+		return false;
+
+	SHPGetInfo(hSHP, &nEntities, &nShapeType, adfMinBound, adfMaxBound);
+	if (nShapeType != SHPT_POINT)
+		return false;
+
+	for (int i = 0; i < nEntities; i++)
+	{
+		SHPObject *psShape = SHPReadObject(hSHP, i);
+		point.x = psShape->padfX[0];
+		point.y = psShape->padfY[0];
+		vtPole *pole = new vtPole;
+		pole->m_p = point;
+		m_Poles.Append(pole);
+		SHPDestroyObject(psShape);
+	}
+	SHPClose(hSHP);
+	return true;
+}
+
+vtPole *vtUtilityMap::ClosestPole(const DPoint2 &p)
+{
+	int k, ret, npoles = m_Poles.GetSize();
+	double	dist, closest = 1E9;
+
+	for (k = 0; k < npoles; k++)
+	{
+		dist = (p - m_Poles[k]->m_p).Length();
+		if (dist < closest)
+		{
+			closest = dist;
+			ret = k;
+		}
+	}
+	return m_Poles[ret];
+}
+
+bool vtUtilityMap::ImportLinesFromSHP(const char *fname)
+{
+	SHPHandle hSHP;
+    int		nEntities, nShapeType;
+    double 	adfMinBound[4], adfMaxBound[4];
+	int		i, j, verts;
+
+	hSHP = SHPOpen(fname, "rb");
+	if (!hSHP)
+		return false;
+
+	SHPGetInfo(hSHP, &nEntities, &nShapeType, adfMinBound, adfMaxBound);
+	if (nShapeType != SHPT_ARC)
+		return false;
+
+	for (i = 0; i < nEntities; i++)
+	{
+		SHPObject *psShape = SHPReadObject(hSHP, i);
+
+		verts = psShape->nVertices;
+		vtLine *line = new vtLine;
+		line->SetSize(verts);
+
+		// Store each SHP Poly Coord in Line
+		for (j = 0; j < verts; j++)
+		{
+			line->GetAt(j).x = psShape->padfX[j];
+			line->GetAt(j).y = psShape->padfY[j];
+		}
+		SHPDestroyObject(psShape);
+
+		// Guess source and destination poles by using location
+		line->m_src = ClosestPole(line->GetAt(0));
+		line->m_dst = ClosestPole(line->GetAt(verts-1));
+
+		// avoid degenerate lines
+		if (line->m_src == line->m_dst)
+		{
+			delete line;
+			continue;
+		}
+
+		// tweak start and end of line to match poles
+		line->SetAt(0, line->m_src->m_p);
+		line->SetAt(verts-1, line->m_dst->m_p);
+
+		m_Lines.Append(line);
+	}
+	SHPClose(hSHP);
+	return true;
+}
+
+
+bool vtUtilityMap::ImportFromSHP(const char *dirname, vtProjection &proj)
+{
+	char	fname[256];
+
+	m_proj = proj;
+
+	// 1. Pole Positions
+	//
+	strcpy(fname, dirname);
+	strcat(fname, "/poles.shp");
+	if (!ImportPolesFromSHP(fname))
+		return false;
+
+	// 2. Fuses
+	//
+	strcpy(fname, dirname);
+	strcat(fname, "/fuses.shp");
+	if (!ImportPolesFromSHP(fname))
+		return false;
+
+	// 3. Transformers
+	//
+	strcpy(fname, dirname);
+	strcat(fname, "/transformers.shp");
+	if (!ImportPolesFromSHP(fname))
+		return false;
+
+	// 4. Servpnts
+	//
+	strcpy(fname, dirname);
+	strcat(fname, "/servpnts.shp");
+	if (!ImportPolesFromSHP(fname))
+		return false;
+
+	// Now, lines
+	//
+	// 1. Primaries
+	//
+	strcpy(fname, dirname);
+	strcat(fname, "/primaries.shp");
+	if (!ImportLinesFromSHP(fname))
+		return false;
+
+	// 2. Secondaries
+	//
+	strcpy(fname, dirname);
+	strcat(fname, "/secondaries.shp");
+	if (!ImportLinesFromSHP(fname))
+		return false;
+	return true;
+}
+
+void vtUtilityMap::GetPoleExtents(DRECT &rect)
+{
+	if (m_Poles.IsEmpty())
+		return;
+
+	rect.SetRect(1E9, -1E9, -1E9, 1E9);
+
+	DRECT rect2;
+	int i, size = m_Poles.GetSize();
+	for (i = 0; i < size; i++)
+	{
+		vtPole *pole = m_Poles.GetAt(i);
+		rect.GrowToContainPoint(pole->m_p);
+	}
+}
+
---
+++ TerrainSDK/vtdata/UtilityMap.h
@@ -0,0 +1,65 @@
+//
+// UtilityMap.h
+//
+// High-level representation of a utility network, currently supporting
+// power networks (poles and lines).
+//
+// Copyright (c) 2001 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#ifndef UTILITYH
+#define UTILITYH
+
+#include "MathTypes.h"
+#include "Projections.h"
+
+/**
+ * vtPole represent any kind of node in a power network, such as a
+ * simple utility pole, a transmission tower, or simply a connection
+ * point at a switch or transformer.
+ */
+struct vtPole
+{
+	int m_type;
+	DPoint2 m_p;
+};
+
+#define LF_UNDERGROUND	1
+
+/**
+ * vtLine represent any connection between two poles.  It does not
+ * explicitly specify the numer or location of each conductor, merely
+ * the high-level topology of the power network.
+ */
+class vtLine : public DLine2
+{
+public:
+	int m_flags;
+	vtPole *m_src, *m_dst;
+};
+
+/**
+ * vtUtilityMap represents a full high-level network of poles and lines.
+ */
+class vtUtilityMap
+{
+public:
+	vtUtilityMap();
+	~vtUtilityMap();
+
+	void GetPoleExtents(DRECT &rect);
+	bool ImportFromSHP(const char *filename, vtProjection &proj);
+
+protected:
+	bool ImportPolesFromSHP(const char *fname);
+	bool ImportLinesFromSHP(const char *fname);
+	vtPole *ClosestPole(const DPoint2 &p);
+
+	Array<vtPole *> m_Poles;
+	Array<vtLine *> m_Lines;
+
+	vtProjection m_proj;
+};
+
+#endif // UTILITYH
