392cfc5def9f0a719b4c597bbbcecc1692ed4b99
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-01-16 09:23:25 +0000

    added get/test for extents, added support for StructInstance
    git-svn-id: http://vtp.googlecode.com/svn/trunk@234 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtdata/Structure.cpp
+++ TerrainSDK/vtdata/Structure.cpp
@@ -5,17 +5,95 @@
 // Free for all uses, see license.txt for details.
 //
 
+#include <stdio.h>
 #include "Structure.h"
 #include "Building.h"
 #include "Fence.h"
 
+vtStructInstance::vtStructInstance()
+{
+	m_p.Set(0, 0);
+	m_fRotation = 0.0f;
+}
+
+void vtStructInstance::WriteXML(FILE *fp, bool bDegrees)
+{
+	int i;
+
+	const char *coord_format;
+	if (bDegrees)
+		coord_format = "%lg";
+	else
+		coord_format = "%.2lf";
+
+	fprintf(fp, "\t<structure type=\"instance\">\n");
+
+	// first write the placement
+	fprintf(fp, "\t\t<placement location=\"");
+	fprintf(fp, coord_format, m_p.x);
+	fprintf(fp, " ");
+	fprintf(fp, coord_format, m_p.y);
+	fprintf(fp, "\"");
+	if (m_fRotation != 0.0f)
+	{
+		fprintf(fp, " rotation=\"%f\"", m_fRotation);
+	}
+	fprintf(fp, " />\n");
+
+	// now write any and all tags
+	for (i = 0; i < NumTags(); i++)
+	{
+		vtTag *tag = GetTag(i);
+		fprintf(fp, "\t\t<%s>%s</%s>\n", (const char *)tag->name,
+			(const char *)tag->value, (const char *)tag->name);
+	}
+
+	fprintf(fp, "\t</structure>\n");
+}
+
+bool vtStructInstance::GetExtents(DRECT &rect)
+{
+	// we have no way (yet) of knowing the extents of an external
+	// reference, so just give a placeholder of a single point.
+	rect.SetRect(m_p.x, m_p.y, m_p.x, m_p.y);
+	return true;
+}
+
+
+///////////////////////////////////////////////////////////////////////
+
 bool vtStructure::GetExtents(DRECT &rect)
 {
-	if (m_type == ST_BUILDING)
+	switch (m_type)
+	{
+	case ST_BUILDING:
 		return m_pBuilding->GetExtents(rect);
 
-	if (m_type == ST_FENCE)
+	case ST_FENCE:
 		return m_pFence->GetExtents(rect);
 
+	case ST_INSTANCE:
+		return m_pInstance->GetExtents(rect);
+	}
 	return false;
 }
+
+bool vtStructure::IsContainedBy(const DRECT &rect)
+{
+	switch (m_type)
+	{
+	case ST_BUILDING:
+		return rect.ContainsPoint(m_pBuilding->GetLocation());
+
+	case ST_FENCE:
+	{
+		const DLine2 &pts = m_pFence->GetFencePoints();
+		return rect.ContainsLine(pts);
+	}
+
+	case ST_INSTANCE:
+		return rect.ContainsPoint(m_pInstance->m_p);
+	}
+	return false;
+}
+
--- TerrainSDK/vtdata/Structure.h
+++ TerrainSDK/vtdata/Structure.h
@@ -12,18 +12,57 @@
 
 #include "vtdata/MathTypes.h"
 #include "Selectable.h"
+#include "Content.h"
 
 class vtBuilding;
 class vtFence;
+
+/**
+ * This class represents a reference to an external model, such as a unique
+ * building which has been created in a 3D Modelling Tool.  It is derived from
+ * vtTagArray which provides a set of arbitrary tags (name/value pairs).
+ * At least one of the following two tags should be present:
+ * - filename, which contains a resolvable path to an external 3d model file.
+ *	 An example is filename="Data/Buildings/GasStation.3ds"
+ * - itemname, which contains the name of a content item which will be resolved
+ *	 by a list maintained by a vtContentManager.  An example is
+ *	 itemname="Gemini Observatory"
+ */
+class vtStructInstance : public vtTagArray
+{
+public:
+	vtStructInstance();
+
+	void WriteXML(FILE *fp, bool bDegrees);
+	bool GetExtents(DRECT &rect);
+
+	DPoint2	m_p;			// earth position
+	float	m_fRotation;	// in radians
+};
 
 enum vtStructureType
 {
 	ST_BUILDING,
 	ST_FENCE,
-	ST_WALL,
 	ST_INSTANCE
 };
 
+/**
+ * The vtStructure class represents any "built structure".  These are
+ * generally immobile, artificial entities of human-scale and larger, such
+ * as buildings and fences.
+ * \par
+ * Structures are implemented as 3 types:
+ *  - Buildings (vtBuilding)
+ *  - Fences and walls (vtFence)
+ *  - Instances (vtStructureInstance)
+ * \par
+ * For enclosed and linear structures which can be well-described
+ * parametrically, vtBuilding and vtFence provide efficient data representation.
+ * For other structures which are not easily reduced to parameters, the Instance
+ * type allows you to reference any external model, such as a unique building
+ * which has been created in a 3D Modelling Tool.
+ */
 class vtStructure : public Selectable
 {
 public:
@@ -31,17 +70,21 @@ public:
 
 	void SetBuilding(vtBuilding *bld) { m_pBuilding = bld; m_type = ST_BUILDING; }
 	void SetFence(vtFence *fen) { m_pFence = fen; m_type = ST_FENCE; }
+	void SetInstance(vtStructInstance *inst) { m_pInstance = inst; m_type = ST_INSTANCE; }
 
 	vtStructureType GetType() { return m_type; }
 	vtBuilding *GetBuilding() { if (m_type == ST_BUILDING) return m_pBuilding; else return NULL; }
 	vtFence *GetFence() { if (m_type == ST_FENCE) return m_pFence; else return NULL; }
+	vtStructInstance *GetInstance() { if (m_type == ST_INSTANCE) return m_pInstance; else return NULL; }
 
 	virtual bool GetExtents(DRECT &rect);
+	bool IsContainedBy(const DRECT &rect);
 
 protected:
 	union {
 		vtBuilding *m_pBuilding;
 		vtFence *m_pFence;
+		vtStructInstance *m_pInstance;
 	};
 	vtStructureType m_type;
 };
