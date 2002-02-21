e010e96cf258acaf16876bcc0cc446c79cc24634
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-02-21 08:41:55 +0000

    removed unnecessary extra Clear (give small but measurable speedup!)
    git-svn-id: http://vtp.googlecode.com/svn/trunk@285 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/Enviro/wx/canvas.cpp
+++ TerrainApps/Enviro/wx/canvas.cpp
@@ -133,8 +133,6 @@ void vtGLCanvas::OnPaint( wxPaintEvent& event )
 
 		m_bPainting = true;
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		// Render the Scene Graph
 		vtGetScene()->DoUpdate();
 
