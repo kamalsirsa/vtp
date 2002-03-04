811007c62c1d51eeee18bdfc0de927015288344b
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-03-04 03:09:46 +0000

    added support for instantiating Instances, hightlighting instances, offsetting and repositioning instances
    git-svn-id: http://vtp.googlecode.com/svn/trunk@313 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtlib/core/Structure3d.cpp
+++ TerrainSDK/vtlib/core/Structure3d.cpp
@@ -13,33 +13,87 @@
 
 vtStructInstance3d::vtStructInstance3d() : vtStructInstance()
 {
-	m_pTransform = NULL;
+	m_pContainer = NULL;
+	m_pHighlight = NULL;
+	m_pModel = NULL;
 }
 
 bool vtStructInstance3d::CreateShape(vtHeightField *pHeightField)
 {
-	FPoint3 surface_pos;
-	pHeightField->ConvertEarthToSurfacePoint(m_p, surface_pos);
+	if (!m_pContainer)
+	{
+		// constructing for the first time
+		m_pContainer = new vtTransform();
+	}
 
-	const char *filename = this->GetValue("filename");
+	const char *filename = GetValue("filename");
 	if (filename)
 	{
-		vtNode *model = vtLoadModel(filename);
-		if (!model)
+		m_pModel = vtLoadModel(filename);
+		if (!m_pModel)
 			return false;
-		m_pTransform = new vtTransform();
-		m_pTransform->AddChild(model);
-		if (m_fRotation != 0.0f)
-			m_pTransform->Rotate2(FPoint3(0,1,0), m_fRotation);
-		m_pTransform->SetTrans(surface_pos);
-		return true;
+		m_pContainer->AddChild(m_pModel);
 	}
-	const char *itemname = this->GetValue("itemname");
+	const char *itemname = GetValue("itemname");
 	if (itemname)
 	{
 		// TODO: use ContentManager to create model
 	}
+
+	UpdateTransform(pHeightField);
 	return true;
+}
+
+void vtStructInstance3d::UpdateTransform(vtHeightField *pHeightField)
+{
+	m_pContainer->Identity();
+	m_pContainer->Scale3(WORLD_SCALE, WORLD_SCALE, WORLD_SCALE);
+
+	// try to work around 3DS coordinate axes difference problem
+	vtString fname2 = GetValue("filename");
+	int dot = fname2.ReverseFind('.');
+	if (dot > 0)
+	{
+		vtString ext = fname2.Mid(dot+1);
+		if (ext.CompareNoCase("3ds") == 0)
+		{
+			// Must rotate by 90 degrees for 3DS MAX -> OpenGL
+			m_pContainer->Rotate2(FPoint3(1.0f, 0.0f, 0.0f), -PID2f);
+		}
+	}
+
+	if (m_fRotation != 0.0f)
+		m_pContainer->Rotate2(FPoint3(0,1,0), m_fRotation);
+
+	FPoint3 surface_pos;
+	pHeightField->ConvertEarthToSurfacePoint(m_p, surface_pos);
+	m_pContainer->SetTrans(surface_pos);
+}
+
+/**
+ * Display some bounding wires around the object to highlight it.
+ */
+void vtStructInstance3d::ShowBounds(bool bShow)
+{
+	if (bShow)
+	{
+		if (!m_pHighlight)
+		{
+			// the highlight geometry doesn't exist, so create it
+			// get bounding sphere
+			FSphere sphere;
+			m_pModel->GetBoundSphere(sphere);
+
+			m_pHighlight = CreateBoundSphereGeom(sphere);
+			m_pContainer->AddChild(m_pHighlight);
+		}
+		m_pHighlight->SetEnabled(true);
+	}
+	else
+	{
+		if (m_pHighlight)
+			m_pHighlight->SetEnabled(false);
+	}
 }
 
 
@@ -58,7 +112,8 @@ bool vtStructure3d::CreateNode(vtHeightField *hf, const char *options)
 		bool roof = (strstr(options, "roof") != NULL);
 		bool walls = (strstr(options, "walls") != NULL);
 		bool details = (strstr(options, "detail") != NULL);
-		return bld->CreateShape(hf, roof, walls, details);
+		bld->CreateShape(hf, roof, walls, details);
+		return true;
 	}
 	if (m_type == ST_FENCE)
 	{
@@ -68,7 +123,20 @@ bool vtStructure3d::CreateNode(vtHeightField *hf, const char *options)
 	if (m_type == ST_INSTANCE)
 	{
 		vtStructInstance3d *inst = (vtStructInstance3d *) m_pInstance;
-		return inst->CreateShape(hf);
+		bool bSuccess = inst->CreateShape(hf);
+		if (!bSuccess && options != NULL)
+		{
+			// try again with the supplied path
+			vtTag *tag = inst->FindTag("filename");
+			if (tag)
+			{
+				vtString path = options;
+				tag->value = path + tag->value;
+			}
+			// try again
+			bSuccess = inst->CreateShape(hf);
+		}
+		return bSuccess;
 	}
 	return false;
 }
@@ -109,6 +177,21 @@ void vtStructure3d::DeleteNode()
 	}
 }
 
+vtBuilding3d *vtStructure3d::GetBuilding()
+{
+	return (vtBuilding3d *) vtStructure::GetBuilding();
+}
+
+vtFence3d *vtStructure3d::GetFence()
+{
+	return (vtFence3d *) vtStructure::GetFence();
+}
+
+vtStructInstance3d *vtStructure3d::GetInstance()
+{
+	return (vtStructInstance3d *) vtStructure::GetInstance();
+}
+
 /////////////////////////////////////////////////////////////////////
 
 vtBuilding *vtStructureArray3d::NewBuilding()
@@ -139,7 +222,7 @@ void vtStructureArray3d::ReConstructStructure(vtStructure3d *str,
 	str->CreateNode(m_pHeightField, options);
 }
 
-void vtStructureArray3d::OffsetSelectedBuildings(DPoint2 &offset)
+void vtStructureArray3d::OffsetSelectedStructures(const DPoint2 &offset)
 {
 	vtStructure *str;
 	for (int i = 0; i < GetSize(); i++)
@@ -147,14 +230,62 @@ void vtStructureArray3d::OffsetSelectedBuildings(DPoint2 &offset)
 		str = GetAt(i);
 		if (!str->IsSelected())
 			continue;
-		if (str->GetType() != ST_BUILDING)
-			continue;
+		if (str->GetType() == ST_BUILDING)
+		{
+			vtBuilding3d *bld = GetBuilding(i);
+			bld->Offset(offset);
+			bld->AdjustHeight(m_pHeightField);
 
-		vtBuilding3d *bld = GetBuilding(i);
-		bld->Offset(offset);
-		bld->AdjustHeight(m_pHeightField);
-
-		// Should really move the building to a new cell in the LOD Grid,
-		// but unless it's moving really far we don't need to worry about this.
+			// Should really move the building to a new cell in the LOD
+			// Grid, but unless it's moving really far we don't need to
+			// worry about this.
+		}
+		if (str->GetType() == ST_FENCE)
+		{
+			vtFence3d *fen = GetFence(i);
+			// TODO
+		}
+		if (str->GetType() == ST_INSTANCE)
+		{
+			vtStructInstance3d *inst = GetInstance(i);
+			inst->Offset(offset);
+			inst->UpdateTransform(m_pHeightField);
+		}
 	}
 }
+
+void vtStructureArray3d::VisualDeselectAll()
+{
+	for (int i = 0; i < GetSize(); i++)
+	{
+		vtStructure3d *str = (vtStructure3d *) GetAt(i);
+		str->Select(false);
+
+		vtBuilding3d *bld = str->GetBuilding();
+		if (bld)
+			bld->ShowBounds(false);
+
+		// TODO: visual highlighting of fences
+
+		vtStructInstance3d *inst = str->GetInstance();
+		if (inst)
+			inst->ShowBounds(false);
+	}
+}
+
+void vtStructureArray3d::VisualSelect(vtStructure3d *str)
+{
+	str->Select(true);
+
+	vtBuilding3d *bld = str->GetBuilding();
+	if (bld)
+		bld->ShowBounds(true);
+
+	// TODO: visual highlighting of fences
+
+	vtStructInstance3d *inst = str->GetInstance();
+	if (inst)
+		inst->ShowBounds(true);
+}
+
+
--- TerrainSDK/vtlib/core/Structure3d.h
+++ TerrainSDK/vtlib/core/Structure3d.h
@@ -24,10 +24,15 @@ public:
 	vtStructInstance3d();
 
 	bool CreateShape(vtHeightField *pHeightField);
-	vtTransform *GetTransform() { return m_pTransform; }
+	vtTransform *GetTransform() { return m_pContainer; }
+
+	void UpdateTransform(vtHeightField *pHeightField);
+	void ShowBounds(bool bShow);
 
 protected:
-	vtTransform *m_pTransform;
+	vtTransform *m_pContainer;
+	vtGeom		*m_pHighlight;	// The wireframe highlight
+	vtNode		*m_pModel; // the contained model
 };
 
 class vtStructure3d : public vtStructure
@@ -39,6 +44,10 @@ public:
 	vtTransform *GetTransform();
 	vtGeom *GetGeom();
 	void DeleteNode();
+
+	vtBuilding3d *GetBuilding();
+	vtFence3d *GetFence();
+	vtStructInstance3d *GetInstance();
 
 protected:
 	vtTransform *m_pNode;
@@ -52,14 +61,19 @@ public:
 	virtual vtFence *NewFence();
 	virtual vtStructInstance *NewInstance();
 
+	vtStructure3d *GetStructure(int i) { return (vtStructure3d *) GetAt(i); }
 	vtBuilding3d *GetBuilding(int i) { return (vtBuilding3d *) GetAt(i)->GetBuilding(); }
 	vtFence3d *GetFence(int i) { return (vtFence3d *) GetAt(i)->GetFence(); }
+	vtStructInstance3d *GetInstance(int i) { return (vtStructInstance3d *) GetAt(i)->GetInstance(); }
 
 	void SetHeightField(vtHeightField *hf) { m_pHeightField = hf; }
 
 	bool ConstructStructure(vtStructure3d *str, const char *options = "");
 	void ReConstructStructure(vtStructure3d *str, const char *options);
-	void OffsetSelectedBuildings(DPoint2 &offset);
+	void OffsetSelectedStructures(const DPoint2 &offset);
+
+	void VisualDeselectAll();
+	void VisualSelect(vtStructure3d *str);
 
 protected:
 	vtHeightField *m_pHeightField;
