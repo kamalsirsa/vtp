a435667ecc0ce901196312c9e9e7b35d8bca240c
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-03-10 06:37:02 +0000

    added doc comments
    git-svn-id: http://vtp.googlecode.com/svn/trunk@328 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtlib/core/Structure3d.h
+++ TerrainSDK/vtlib/core/Structure3d.h
@@ -18,15 +18,25 @@ class vtFence3d;
 class vtNode;
 class vtHeightField;
 
+/**
+ * This class extends vtStructInstance with the ability to construct and
+ * manage 3d representations of the instance.
+ */
 class vtStructInstance3d : public vtStructInstance
 {
 public:
 	vtStructInstance3d();
 
+	/// Create the node(s) and position them on the indicated heightfield
 	bool CreateShape(vtHeightField *pHeightField);
+
+	/// Access the Transform node for this structure
 	vtTransform *GetTransform() { return m_pContainer; }
 
+	/// (Re-)position the instance on the indicated heightfield
 	void UpdateTransform(vtHeightField *pHeightField);
+
+	/// Pass true to turn on a wireframe hightlight geometry for this instance
 	void ShowBounds(bool bShow);
 
 protected:
@@ -35,13 +45,22 @@ protected:
 	vtNode		*m_pModel; // the contained model
 };
 
+/**
+ * This class extends vtStructure with the ability to construct and
+ * manage 3d representations of the structure.
+ */
 class vtStructure3d : public vtStructure
 {
 public:
 	vtStructure3d();
 
+	/// Create the node(s) and position them on the indicated heightfield
 	bool CreateNode(vtHeightField *hf, const char *options = "");
+
+	/// Access the Transform node for this structure, if it has one
 	vtTransform *GetTransform();
+
+	/// Access the Geometry node for this structure, if it has one
 	vtGeom *GetGeom();
 	void DeleteNode();
 
@@ -53,6 +72,10 @@ protected:
 	vtTransform *m_pNode;
 };
 
+/**
+ * This class extends vtStructureArray with the ability to construct and
+ * manage 3d representations of the structures.
+ */
 class vtStructureArray3d : public vtStructureArray
 {
 public:
@@ -66,13 +89,18 @@ public:
 	vtFence3d *GetFence(int i) { return (vtFence3d *) GetAt(i)->GetFence(); }
 	vtStructInstance3d *GetInstance(int i) { return (vtStructInstance3d *) GetAt(i)->GetInstance(); }
 
+	/// Indicate the heightfield which will be used for the structures in this array
 	void SetHeightField(vtHeightField *hf) { m_pHeightField = hf; }
 
+	/// Construct an individual structure, return true if successful
 	bool ConstructStructure(vtStructure3d *str, const char *options = "");
 	void ReConstructStructure(vtStructure3d *str, const char *options);
 	void OffsetSelectedStructures(const DPoint2 &offset);
 
+	/// Deselect all structures including turning off their visual highlights
 	void VisualDeselectAll();
+
+	/// Select a single structure, and visually highlight it
 	void VisualSelect(vtStructure3d *str);
 
 protected:
