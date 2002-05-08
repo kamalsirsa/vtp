8ef69824c19303b151ed2a47cf368acfae87aa01
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-05-08 01:25:18 +0000

    exposed docs
    git-svn-id: http://vtp.googlecode.com/svn/trunk@431 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtdata/vtDIB.cpp
+++ TerrainSDK/vtdata/vtDIB.cpp
@@ -11,9 +11,9 @@
 #include "ByteOrder.h"
 
 
-//
-// create new empty DIB wrapper
-//
+/**
+ * Create a new empty DIB wrapper.
+ */
 vtDIB::vtDIB()
 {
 	m_bLoadedSuccessfully = false;
@@ -22,9 +22,9 @@ vtDIB::vtDIB()
 }
 
 
-//
-// load new DIB from file
-//
+/**
+ * Load a new DIB from a file.
+ */
 vtDIB::vtDIB(const char *fname)
 {
 	// assume failure
@@ -36,16 +36,16 @@ vtDIB::vtDIB(const char *fname)
 		m_bLoadedSuccessfully = true;
 }
 
-//
-// create new DIB in memory
-//
+/**
+ * Create a new DIB in memory.
+ */
 vtDIB::vtDIB(int xsize, int ysize, int bitdepth, bool create_palette)
 {
 	m_iWidth = xsize;
 	m_iHeight = ysize;
 	m_iBitCount = bitdepth;
 
-	ComputeByteWidth();
+	_ComputeByteWidth();
 
 	int ImageSize = m_iByteWidth * m_iHeight;
 
@@ -87,7 +87,7 @@ vtDIB::vtDIB(void *pDIB)
 	m_iHeight = m_Hdr->biHeight;
 	m_iBitCount = m_Hdr->biBitCount;
 
-	ComputeByteWidth();
+	_ComputeByteWidth();
 }
 
 
@@ -102,6 +102,9 @@ vtDIB::~vtDIB()
 }
 
 
+/**
+ * Read a MSWindows-style .bmp file into the DIB.
+ */
 bool vtDIB::ReadBMP(const char *fname)
 {
 	BITMAPFILEHEADER	bitmapHdr;
@@ -117,8 +120,8 @@ bool vtDIB::ReadBMP(const char *fname)
 
 	if (fread(&bitmapHdr, 14, 1, fp) == 0)
 		goto ErrExit;
-	bitmapHdr.bfType    = SwapBytes( (short)bitmapHdr.bfType  , BO_LE, BO_CPU );
-	bitmapHdr.bfSize    = SwapBytes( (long)bitmapHdr.bfSize   , BO_LE, BO_CPU );
+	bitmapHdr.bfType	= SwapBytes( (short)bitmapHdr.bfType  , BO_LE, BO_CPU );
+	bitmapHdr.bfSize	= SwapBytes( (long)bitmapHdr.bfSize   , BO_LE, BO_CPU );
 	bitmapHdr.bfOffBits = SwapBytes( (long)bitmapHdr.bfOffBits, BO_LE, BO_CPU );
 
 	if (bitmapHdr.bfType != 0x4d42)
@@ -126,17 +129,17 @@ bool vtDIB::ReadBMP(const char *fname)
 
 	if (fread(m_Hdr, sizeof(BITMAPINFOHEADER), 1, fp) == 0)
 		goto ErrExit;
-	m_Hdr->biSize          = SwapBytes( (long )m_Hdr->biSize         ,BO_LE, BO_CPU );
-	m_Hdr->biWidth         = SwapBytes( (long )m_Hdr->biWidth        ,BO_LE, BO_CPU );
-	m_Hdr->biHeight        = SwapBytes( (long )m_Hdr->biHeight       ,BO_LE, BO_CPU );
-	m_Hdr->biPlanes        = SwapBytes( (short)m_Hdr->biPlanes       ,BO_LE, BO_CPU );
-	m_Hdr->biBitCount      = SwapBytes( (short)m_Hdr->biBitCount     ,BO_LE, BO_CPU );
-	m_Hdr->biCompression   = SwapBytes( (long )m_Hdr->biCompression  ,BO_LE, BO_CPU );
-	m_Hdr->biSizeImage     = SwapBytes( (long )m_Hdr->biSizeImage    ,BO_LE, BO_CPU );
-	m_Hdr->biXPelsPerMeter = SwapBytes( (long )m_Hdr->biXPelsPerMeter,BO_LE, BO_CPU );
-	m_Hdr->biYPelsPerMeter = SwapBytes( (long )m_Hdr->biYPelsPerMeter,BO_LE, BO_CPU );
-	m_Hdr->biClrUsed       = SwapBytes( (long )m_Hdr->biClrUsed      ,BO_LE, BO_CPU );
-	m_Hdr->biClrImportant  = SwapBytes( (long )m_Hdr->biClrImportant ,BO_LE, BO_CPU );
+	m_Hdr->biSize		  	= SwapBytes( (long )m_Hdr->biSize		,BO_LE, BO_CPU );
+	m_Hdr->biWidth			= SwapBytes( (long )m_Hdr->biWidth		,BO_LE, BO_CPU );
+	m_Hdr->biHeight			= SwapBytes( (long )m_Hdr->biHeight		,BO_LE, BO_CPU );
+	m_Hdr->biPlanes			= SwapBytes( (short)m_Hdr->biPlanes		,BO_LE, BO_CPU );
+	m_Hdr->biBitCount		= SwapBytes( (short)m_Hdr->biBitCount	,BO_LE, BO_CPU );
+	m_Hdr->biCompression	= SwapBytes( (long )m_Hdr->biCompression,BO_LE, BO_CPU );
+	m_Hdr->biSizeImage		= SwapBytes( (long )m_Hdr->biSizeImage	,BO_LE, BO_CPU );
+	m_Hdr->biXPelsPerMeter	= SwapBytes( (long )m_Hdr->biXPelsPerMeter,BO_LE, BO_CPU );
+	m_Hdr->biYPelsPerMeter	= SwapBytes( (long )m_Hdr->biYPelsPerMeter,BO_LE, BO_CPU );
+	m_Hdr->biClrUsed		= SwapBytes( (long )m_Hdr->biClrUsed	  ,BO_LE, BO_CPU );
+	m_Hdr->biClrImportant	= SwapBytes( (long )m_Hdr->biClrImportant ,BO_LE, BO_CPU );
 
 	if (m_Hdr->biCompression != BI_RGB)
 	{
@@ -187,7 +190,7 @@ bool vtDIB::ReadBMP(const char *fname)
 	m_iBitCount = m_Hdr->biBitCount;
 	m_iPaletteSize = sizeof(RGBQUAD) * m_Hdr->biClrUsed;
 
-	ComputeByteWidth();
+	_ComputeByteWidth();
 	fclose(fp);
 	return true;
 
@@ -196,6 +199,9 @@ ErrExit:
 	return false;
 }
 
+/**
+ * Write a MSWindows-style .bmp file from the DIB.
+ */
 bool vtDIB::WriteBMP(const char *fname)
 {
 	FILE *fp = fopen(fname, "wb");
@@ -209,8 +215,8 @@ bool vtDIB::WriteBMP(const char *fname)
 	bitmapHdr.bfOffBits = 14 + 40 + m_iPaletteSize;		/* Header and colormap */
 	bitmapHdr.bfSize = bitmapHdr.bfOffBits + m_iByteWidth * m_iHeight;
 
-	bitmapHdr.bfType    = SwapBytes( (short)bitmapHdr.bfType  , BO_CPU, BO_LE );
-	bitmapHdr.bfSize    = SwapBytes( (long)bitmapHdr.bfSize   , BO_CPU, BO_LE );
+	bitmapHdr.bfType	= SwapBytes( (short)bitmapHdr.bfType  , BO_CPU, BO_LE );
+	bitmapHdr.bfSize	= SwapBytes( (long)bitmapHdr.bfSize   , BO_CPU, BO_LE );
 	bitmapHdr.bfOffBits = SwapBytes( (long)bitmapHdr.bfOffBits, BO_CPU, BO_LE );
 
 	fwrite(&bitmapHdr, 14, 1, fp);
@@ -218,16 +224,16 @@ bool vtDIB::WriteBMP(const char *fname)
 	// write bitmap header
 	BITMAPINFOHEADER t_Hdr = *m_Hdr;
 
-	t_Hdr.biSize          = SwapBytes( (long )t_Hdr.biSize         ,BO_CPU, BO_LE );
-	t_Hdr.biWidth         = SwapBytes( (long )t_Hdr.biWidth        ,BO_CPU, BO_LE );
-	t_Hdr.biHeight        = SwapBytes( (long )t_Hdr.biHeight       ,BO_CPU, BO_LE );
-	t_Hdr.biPlanes        = SwapBytes( (short)t_Hdr.biPlanes       ,BO_CPU, BO_LE );
-	t_Hdr.biBitCount      = SwapBytes( (short)t_Hdr.biBitCount     ,BO_CPU, BO_LE );
+	t_Hdr.biSize		  = SwapBytes( (long )t_Hdr.biSize		 ,BO_CPU, BO_LE );
+	t_Hdr.biWidth		 = SwapBytes( (long )t_Hdr.biWidth		,BO_CPU, BO_LE );
+	t_Hdr.biHeight		= SwapBytes( (long )t_Hdr.biHeight	   ,BO_CPU, BO_LE );
+	t_Hdr.biPlanes		= SwapBytes( (short)t_Hdr.biPlanes	   ,BO_CPU, BO_LE );
+	t_Hdr.biBitCount	  = SwapBytes( (short)t_Hdr.biBitCount	 ,BO_CPU, BO_LE );
 	t_Hdr.biCompression   = SwapBytes( (long )t_Hdr.biCompression  ,BO_CPU, BO_LE );
-	t_Hdr.biSizeImage     = SwapBytes( (long )t_Hdr.biSizeImage    ,BO_CPU, BO_LE );
+	t_Hdr.biSizeImage	 = SwapBytes( (long )t_Hdr.biSizeImage	,BO_CPU, BO_LE );
 	t_Hdr.biXPelsPerMeter = SwapBytes( (long )t_Hdr.biXPelsPerMeter,BO_CPU, BO_LE );
 	t_Hdr.biYPelsPerMeter = SwapBytes( (long )t_Hdr.biYPelsPerMeter,BO_CPU, BO_LE );
-	t_Hdr.biClrUsed       = SwapBytes( (long )t_Hdr.biClrUsed      ,BO_CPU, BO_LE );
+	t_Hdr.biClrUsed	   = SwapBytes( (long )t_Hdr.biClrUsed	  ,BO_CPU, BO_LE );
 	t_Hdr.biClrImportant  = SwapBytes( (long )t_Hdr.biClrImportant ,BO_CPU, BO_LE );
 
 	fwrite(&t_Hdr, 40, 1, fp);
@@ -247,47 +253,54 @@ bool vtDIB::WriteBMP(const char *fname)
 	return true;
 }
 
-void vtDIB::ComputeByteWidth()
+void vtDIB::_ComputeByteWidth()
 {
 	m_iByteWidth = (((m_iWidth)*(m_iBitCount) + 31) / 32 * 4);
 }
 
+/**
+ * Pass true to indicate that the DIB should not free its internal memory
+ * when the object is deleted.
+ */
 void vtDIB::LeaveInternalDIB(bool bLeaveIt)
 {
 	m_bLeaveIt = bLeaveIt;
 }
 
-//
-// Get/Set a 24-bit RGB value in a 24-bit bitmap
-//
+/**
+ * Get a 24-bit RGB value from a 24-bit bitmap.
+ */
 dword vtDIB::GetPixel24(int x, int y)
 {
 	register byte* adr;
 
 	// note: Most processors don't support unaligned int/float reads, and on
-	//       those that do, it's slower than aligned reads.
+	//	   those that do, it's slower than aligned reads.
 	adr = ((byte *)m_Data) + (m_iHeight-y-1)*m_iByteWidth + x+x+x;
 	return (*(byte *)(adr+0)) << 16 |
-	       (*(byte *)(adr+1)) <<  8 |
-	       (*(byte *)(adr+2));
+		   (*(byte *)(adr+1)) <<  8 |
+		   (*(byte *)(adr+2));
 }
 
 
+/**
+ * Set a 24-bit RGB value in a 24-bit bitmap.
+ */
 void vtDIB::SetPixel24(int x, int y, dword color)
 {
 	register byte* adr;
 
 	// note: Most processors don't support unaligned int/float writes, and on
-	//       those that do, it's slower than unaligned writes.
+	//	   those that do, it's slower than unaligned writes.
 	adr = ((byte *)m_Data) + (m_iHeight-y-1)*m_iByteWidth + x+x+x;
 	*((byte *)(adr+0)) = color >> 16;
 	*((byte *)(adr+1)) = color >>  8;
 	*((byte *)(adr+2)) = color;
 }
 
-//
-// Get/Set a single byte in an 8-bit bitmap
-//
+/**
+ * Get a single byte from an 8-bit bitmap.
+ */
 byte vtDIB::GetPixel8(int x, int y)
 {
 	register byte* adr;
@@ -297,6 +310,9 @@ byte vtDIB::GetPixel8(int x, int y)
 }
 
 
+/**
+ * Set a single byte in an 8-bit bitmap.
+ */
 void vtDIB::SetPixel8(int x, int y, byte value)
 {
 	register byte* adr;
@@ -305,9 +321,9 @@ void vtDIB::SetPixel8(int x, int y, byte value)
 	*adr = value;
 }
 
-//
-// Get/Set a single bit in a 1-bit bitmap
-//
+/**
+ * Get a single bit from a 1-bit bitmap.
+ */
 bool vtDIB::GetPixel1(int x, int y)
 {
 	// untested - just my guess
@@ -320,6 +336,9 @@ bool vtDIB::GetPixel1(int x, int y)
 }
 
 
+/**
+ * Set a single bit in a 1-bit bitmap.
+ */
 void vtDIB::SetPixel1(int x, int y, bool value)
 {
 	// untested - just my guess
