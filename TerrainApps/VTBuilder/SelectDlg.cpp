4d9f1c3ca7b604a5a9e61412b0c088c18240a0f5
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-05-23 04:21:18 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@552 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/VTBuilder/SelectDlg.cpp
@@ -0,0 +1,113 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:		SelectDlg.cpp
+/////////////////////////////////////////////////////////////////////////////
+
+#ifdef __GNUG__
+	#pragma implementation "SelectDlg.cpp"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#include "SelectDlg.h"
+#include "RawLayer.h"
+
+// WDR: class implementations
+
+//----------------------------------------------------------------------------
+// SelectDlg
+//----------------------------------------------------------------------------
+
+// WDR: event table for SelectDlg
+
+BEGIN_EVENT_TABLE(SelectDlg,AutoDialog)
+	EVT_CHOICE( ID_FIELD, SelectDlg::OnChoiceField )
+END_EVENT_TABLE()
+
+SelectDlg::SelectDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+	const wxPoint &position, const wxSize& size, long style ) :
+	AutoDialog( parent, id, title, position, size, style )
+{
+	SelectDialogFunc( this, TRUE );
+	m_bSetting = true;
+	m_pLayer = NULL;
+}
+
+void SelectDlg::SetRawLayer(vtRawLayer *pRL)
+{
+	m_pLayer = pRL;
+}
+
+void SelectDlg::OnInitDialog(wxInitDialogEvent& event)
+{
+	int i;
+
+	m_iField = 0;
+	m_iCondition = 0;
+	m_strValue = "";
+
+	for (i = 0; i < m_pLayer->GetNumFields(); i++)
+	{
+		Field *field = m_pLayer->GetField(i);
+		GetField()->Append((const char *) field->m_name);
+	}
+	GetField()->SetSelection(0);
+
+	GetCondition()->Append(" = ");
+	GetCondition()->Append(" > ");
+	GetCondition()->Append(" < ");
+	GetCondition()->Append(" >= ");
+	GetCondition()->Append(" <= ");
+	GetCondition()->Append(" <> ");
+	GetCondition()->SetSelection(0);
+
+	FillValuesControl();
+
+	AddValidator(ID_FIELD, &m_iField);
+	AddValidator(ID_CONDITION, &m_iCondition);
+	AddValidator(ID_COMBO_VALUE, &m_strValue);
+
+	wxDialog::OnInitDialog(event);	// calls TransferValuesToWindow
+
+	m_bSetting = false;
+}
+
+void SelectDlg::FillValuesControl()
+{
+	int i, values = 0;
+	vtString str;
+	wxString str2;
+
+	GetComboValue()->Clear();
+
+	for (i = 0; i < m_pLayer->NumEntities(); i++)
+	{
+		m_pLayer->GetValueAsString(i, m_iField, str);
+
+		str2 = (const char *) str;
+		if (str2 == "")
+			continue;
+
+		if (GetComboValue()->FindString(str2) == -1)
+		{
+			GetComboValue()->Append(str2);
+			values++;
+		}
+		if (values == 100)
+			break;
+	}
+	GetComboValue()->SetSelection(0);
+}
+
+
+// WDR: handler implementations for SelectDlg
+
+void SelectDlg::OnChoiceField( wxCommandEvent &event )
+{
+	if (m_bSetting)
+		return;
+
+	TransferDataFromWindow();
+	FillValuesControl();
+}
+
---
+++ TerrainApps/VTBuilder/SelectDlg.h
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:		SelectDlg.h
+// Author:	  XX
+// Created:	 XX/XX/XX
+// Copyright:   XX
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef __SelectDlg_H__
+#define __SelectDlg_H__
+
+#ifdef __GNUG__
+	#pragma interface "SelectDlg.cpp"
+#endif
+
+#ifndef WX_PRECOMP
+	#include "wx/wx.h"
+#endif
+
+#include "VTBuilder_wdr.h"
+#include "AutoDialog.h"
+
+class vtRawLayer;
+
+// WDR: class declarations
+
+//----------------------------------------------------------------------------
+// SelectDlg
+//----------------------------------------------------------------------------
+
+class SelectDlg: public AutoDialog
+{
+public:
+	// constructors and destructors
+	SelectDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxDEFAULT_DIALOG_STYLE );
+	
+	// WDR: method declarations for SelectDlg
+	wxComboBox* GetComboValue()  { return (wxComboBox*) FindWindow( ID_COMBO_VALUE ); }
+	wxChoice* GetCondition()  { return (wxChoice*) FindWindow( ID_CONDITION ); }
+	wxChoice* GetField()  { return (wxChoice*) FindWindow( ID_FIELD ); }
+	void FillValuesControl();
+	void SetRawLayer(vtRawLayer *pRL);
+
+	int			m_iField;
+	int			m_iCondition;
+	wxString	m_strValue;
+
+private:
+	// WDR: member variable declarations for SelectDlg
+	vtRawLayer	*m_pLayer;
+	bool		m_bSetting;
+
+private:
+	// WDR: handler declarations for SelectDlg
+	void OnChoiceField( wxCommandEvent &event );
+	void OnInitDialog(wxInitDialogEvent& event);
+
+private:
+	DECLARE_EVENT_TABLE()
+};
+
+
+
+
+#endif
