06b8c6ca1e63e24880de250b9754a6eb490340e9
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-10-04 03:27:58 +0000

    found issue with color control being drawn black, as a result of roofs with edges of different colors, solved by making color control multi-banded
    git-svn-id: http://vtp.googlecode.com/svn/trunk@990 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtui/BuildingDlg.h
+++ TerrainSDK/vtui/BuildingDlg.h
@@ -1,5 +1,5 @@
 //
-// Name:        BuildingDlg.h
+// Name:		BuildingDlg.h
 //
 // Copyright (c) 2001-2002 Virtual Terrain Project
 // Free for all uses, see license.txt for details.
@@ -9,7 +9,7 @@
 #define __BuildingDlg_H__
 
 #ifdef __GNUG__
-    #pragma interface "BuildingDlg.cpp"
+	#pragma interface "BuildingDlg.cpp"
 #endif
 
 #include "vtui_wdr.h"
@@ -25,60 +25,61 @@
 class BuildingDlg: public AutoDialog
 {
 public:
-    // constructors and destructors
-    BuildingDlg( wxWindow *parent, wxWindowID id, const wxString &title,
-        const wxPoint& pos = wxDefaultPosition,
-        const wxSize& size = wxDefaultSize,
-        long style = wxDEFAULT_DIALOG_STYLE );
-    
-    // WDR: method declarations for BuildingDlg
-    wxButton* GetLevelCopy()  { return (wxButton*) FindWindow( ID_LEVEL_COPY ); }
-    wxButton* GetLevelDel()  { return (wxButton*) FindWindow( ID_LEVEL_DEL ); }
-    wxButton* GetLevelDown()  { return (wxButton*) FindWindow( ID_LEVEL_DOWN ); }
-    wxButton* GetLevelUp()  { return (wxButton*) FindWindow( ID_LEVEL_UP ); }
-    wxStaticBitmap* GetColorBitmap()  { return (wxStaticBitmap*) FindWindow( ID_COLOR ); }
-    wxListBox* GetLevelCtrl()  { return (wxListBox*) FindWindow( ID_LEVEL ); }
-    void Setup(vtBuilding *bld);
-    void EditColor();
-    void SetLevel(int i);
-    void UpdateSlopes();
-    void RefreshLevelsBox();
-    void HighlightSelectedLevel();
+	// constructors and destructors
+	BuildingDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxDEFAULT_DIALOG_STYLE );
+	
+	// WDR: method declarations for BuildingDlg
+	wxButton* GetLevelCopy()  { return (wxButton*) FindWindow( ID_LEVEL_COPY ); }
+	wxButton* GetLevelDel()  { return (wxButton*) FindWindow( ID_LEVEL_DEL ); }
+	wxButton* GetLevelDown()  { return (wxButton*) FindWindow( ID_LEVEL_DOWN ); }
+	wxButton* GetLevelUp()  { return (wxButton*) FindWindow( ID_LEVEL_UP ); }
+	wxStaticBitmap* GetColorBitmap()  { return (wxStaticBitmap*) FindWindow( ID_COLOR ); }
+	wxListBox* GetLevelCtrl()  { return (wxListBox*) FindWindow( ID_LEVEL ); }
+	void Setup(vtBuilding *bld);
+	void EditColor();
+	void SetLevel(int i);
+	void UpdateSlopes();
+	void RefreshLevelsBox();
+	void HighlightSelectedLevel();
+	void UpdateColorControl();
 
 protected:
-    // WDR: member variable declarations for BuildingDlg
-    vtBuilding  *m_pBuilding;
-    vtLevel     *m_pLevel;
+	// WDR: member variable declarations for BuildingDlg
+	vtBuilding  *m_pBuilding;
+	vtLevel     *m_pLevel;
 
-    int        m_iLevel;
-    int        m_iStories;
-    float       m_fStoryHeight;
-    wxString    m_strMaterial;
-    wxString    m_strEdgeSlopes;
-    wxColour    m_Color;
+	int		    m_iLevel;
+	int		    m_iStories;
+	float	    m_fStoryHeight;
+	wxString	m_strMaterial;
+	wxString	m_strEdgeSlopes;
+	wxColour	m_Color;
 
-    wxStaticBitmap  *m_pColorBitmapControl;
-    wxListBox    *m_pLevelListBox;
+	wxStaticBitmap  *m_pColorBitmapControl;
+	wxListBox	*m_pLevelListBox;
 
-    bool m_bSetting;
+	bool m_bSetting;
 
 protected:
-    // WDR: handler declarations for BuildingDlg
-    void OnLevelDown( wxCommandEvent &event );
-    void OnLevelDelete( wxCommandEvent &event );
-    void OnLevelUp( wxCommandEvent &event );
-    void OnLevelCopy( wxCommandEvent &event );
-    void OnStoryHeight( wxCommandEvent &event );
-    void OnSpinStories( wxCommandEvent &event );
-    void OnColor1( wxCommandEvent &event );
-    virtual void OnOK( wxCommandEvent &event );
-    void OnInitDialog(wxInitDialogEvent& event);
-    void OnLevel( wxCommandEvent &event );
-    void OnSetEdgeSlopes( wxCommandEvent &event );
-    void OnSetMaterial( wxCommandEvent &event );
+	// WDR: handler declarations for BuildingDlg
+	void OnLevelDown( wxCommandEvent &event );
+	void OnLevelDelete( wxCommandEvent &event );
+	void OnLevelUp( wxCommandEvent &event );
+	void OnLevelCopy( wxCommandEvent &event );
+	void OnStoryHeight( wxCommandEvent &event );
+	void OnSpinStories( wxCommandEvent &event );
+	void OnColor1( wxCommandEvent &event );
+	virtual void OnOK( wxCommandEvent &event );
+	void OnInitDialog(wxInitDialogEvent& event);
+	void OnLevel( wxCommandEvent &event );
+	void OnSetEdgeSlopes( wxCommandEvent &event );
+	void OnSetMaterial( wxCommandEvent &event );
 
 private:
-    DECLARE_EVENT_TABLE()
+	DECLARE_EVENT_TABLE()
 };
 
 
