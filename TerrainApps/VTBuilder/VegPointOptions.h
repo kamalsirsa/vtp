77506b3054c5342bc0b8d42c7b04601c72722cd5
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2002-10-27 04:58:05 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@1014 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/VTBuilder/VegPointOptions.h
@@ -0,0 +1,31 @@
+//
+// Name: VegFieldsDlg.h
+//
+
+#ifndef __VegPointOptions_H__
+#define __VegPointOptions_H__
+
+#include "vtdata/vtString.h"
+
+/* This structure stores a description of how an imported file should be
+   intepreted as vegetation point data. */
+class VegPointOptions
+{
+public:
+	bool bFixedSpecies;
+	vtString strFixedSpeciesName;
+
+	int iSpeciesFieldIndex;
+
+	// 0 - species ID (int)
+	// 1 - species name (string)
+	// 2 - common name (string)
+	// 3 - biotype id (int)
+	// 4 - biotype name (string)
+	int iInterpretSpeciesField;
+
+	bool bHeightRandom;
+	int iHeightFieldIndex;
+};
+
+#endif // __VegPointOptions_H__
