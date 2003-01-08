66b1c0a7129716caae5377819e268e095e3bdf26
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-01-08 04:19:28 +0000

    created (just an idea for now..)
    git-svn-id: http://vtp.googlecode.com/svn/trunk@1139 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainSDK/vtdata/Vocab.h
@@ -0,0 +1,61 @@
+
+#include "vtString.h"
+#include "Array.h"
+#include "Building.h"
+
+enum TokenType
+{
+	LITERAL,
+	COUNTER,
+	NUMBER,
+	COLOR,
+	MATERIAL,
+	ROOFTYPE
+};
+
+class InputToken
+{
+public:
+	enum TokenType type;
+	vtString str;
+
+	// Don't bother with a union, no need to be efficient here
+	int counter;
+	float number;
+	RGBi color;
+	BldMaterial material;
+	RoofType rooftype;
+};
+
+class MatchToken : public InputToken
+{
+public:
+	bool m_bRequired;
+	Array<vtString *> alternates;
+};
+
+typedef Array<InputToken *> SentenceIn;
+class SentenceMatch : public Array<MatchToken *>
+{
+public:
+	MatchToken *AddLiteral(bool required, const char *str1,
+		const char *str2 = NULL, const char *str3 = NULL, const char *str4 = NULL);
+	MatchToken *AddToken(bool required, enum TokenType type);
+};
+
+class TestParser
+{
+public:
+	SentenceIn sen;
+	int iWords;
+
+	int IsCounter(InputToken &t);
+	int IsNumber(InputToken &t);
+	int IsColor(int i);
+	int IsMaterial(int i);
+
+	void ParseInput(const char *string);
+	void Apply();
+	bool Matches(SentenceMatch &pattern);
+};
+
