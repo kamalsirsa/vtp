36cb8e090c55ffeec92f4980790cf732cab5ec87
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-04-13 03:34:29 +0000

    expanded Plant Option dialog to expose height variance in the UI, and prepare for more sophisticated planting
    git-svn-id: http://vtp.googlecode.com/svn/trunk@1340 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/Enviro/PlantingOptions.h
@@ -0,0 +1,14 @@
+
+#ifndef __PlantingOptions_h__
+#define __PlantingOptions_h__
+
+struct PlantingOptions
+{
+	int		m_iMode;
+	int		m_iSpecies;
+	float	m_fHeight;
+    int		m_iVariance;
+	float	m_fSpacing;
+};
+
+#endif __PlantingOptions_h__
