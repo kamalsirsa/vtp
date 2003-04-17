058f1d66697afacec19e38a956229b799dd554ff
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-04-17 05:05:27 +0000

    added redirect of CPL (GDAL/OGR/PROJ) errors into VTLOG
    git-svn-id: http://vtp.googlecode.com/svn/trunk@1361 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtdata/vtLog.cpp
+++ TerrainSDK/vtdata/vtLog.cpp
@@ -1,16 +1,31 @@
 //
 // Log - simple message logging to a file.
 //
-// Copyright (c) 2002 Virtual Terrain Project
+// Copyright (c) 2002-2003 Virtual Terrain Project
 // Free for all uses, see license.txt for details.
 //
 
+#include "cpl_error.h"	// From GDAL/include
 #include "vtLog.h"
 #include <stdarg.h>
 
 #ifdef _MSC_VER
-#include <windows.h>
+#include <windows.h>	// for OutputDebugString, unfortunately
 #endif
+
+vtLog g_Log;
+
+void cpl_error_handler(CPLErr eErrClass, int err_no, const char *msg)
+{
+	if (eErrClass == CE_Debug)
+		g_Log._Log("CPL Debug: ");
+	else if (eErrClass == CE_Warning)
+		g_Log.Printf("CPL Warning %d: ", err_no);
+	else
+		g_Log.Printf("CPL Error %d: ", err_no);
+	g_Log._Log(msg);
+	g_Log._Log("\n");
+}
 
 vtLog::vtLog()
 {
@@ -20,6 +35,7 @@ vtLog::vtLog()
 void vtLog::_StartLog(const char *fname)
 {
 	m_log = fopen(fname, "wb");
+	CPLPushErrorHandler(cpl_error_handler);
 }
 
 void vtLog::_Log(const char *str)
@@ -45,4 +61,3 @@ void vtLog::Printf(const char *pFormat, ...)
 	_Log(ach);
 }
 
-vtLog g_Log;
