6e783b4cb56aecc0ff8d1f1a69d25296f0a6ce28
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-05-19 03:31:28 +0000

    moved Globe module down from Enviro
    git-svn-id: http://vtp.googlecode.com/svn/trunk@1600 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/Enviro/Engines.cpp
+++ TerrainApps/Enviro/Engines.cpp
@@ -10,10 +10,10 @@
 #include "vtlib/vtlib.h"
 #include "vtlib/core/Route.h"
 #include "vtlib/core/Roads.h"
+#include "vtlib/core/Globe.h"
 
 #include "Engines.h"
 #include "Hawaii.h"
-#include "Globe.h"
 #include "Enviro.h"	// for GetCurrentTerrain
 
 //////////////////////////////////////////////////////////////
