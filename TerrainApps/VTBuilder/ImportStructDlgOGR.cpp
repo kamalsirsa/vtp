7d85927b6f9ea4a89ebafbfc9e19b48a1179942c
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-05-22 22:59:37 +0000

    created by RJ
    git-svn-id: http://vtp.googlecode.com/svn/trunk@1631 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/VTBuilder/ImportStructDlgOGR.cpp
@@ -0,0 +1,206 @@
+//
+// Name: ImportStructDlgOGR.cpp
+//
+// Copyright (c) 2003 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#ifdef __GNUG__
+	#pragma implementation "ImportStructDlgOGR.cpp"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+	#pragma hdrstop
+#endif
+
+#include "ImportStructDlgOGR.h"
+#include "StructLayer.h"
+
+#include "ogrsf_frmts.h"
+
+// WDR: class implementations
+
+//----------------------------------------------------------------------------
+// ImportStructDlgOGR
+//----------------------------------------------------------------------------
+
+// WDR: event table for ImportStructDlgOGR
+
+BEGIN_EVENT_TABLE(ImportStructDlgOGR,AutoDialog)
+	EVT_RADIOBUTTON( ID_TYPE_BUILDING, ImportStructDlgOGR::OnRadio )
+	EVT_RADIOBUTTON( ID_TYPE_LINEAR, ImportStructDlgOGR::OnRadio )
+	EVT_RADIOBUTTON( ID_TYPE_INSTANCE, ImportStructDlgOGR::OnRadio )
+	EVT_CHOICE( ID_CHOICE_HEIGHT_FIELD, ImportStructDlgOGR::OnChoiceHeightField )
+	EVT_CHOICE( ID_CHOICE_FILE_FIELD, ImportStructDlgOGR::OnChoiceFileField )
+	EVT_CHOICE( ID_ELEVATION_FIELDNAME, ImportStructDlgOGR::OnChoiceElevationFieldname )
+	EVT_CHOICE( ID_LAYERNAME, ImportStructDlgOGR::OnChoiceLayerName )
+END_EVENT_TABLE()
+
+ImportStructDlgOGR::ImportStructDlgOGR( wxWindow *parent, wxWindowID id, const wxString &title,
+	const wxPoint &position, const wxSize& size, long style ) :
+	AutoDialog( parent, id, title, position, size, style )
+{
+	ImportStructFuncOGR( this, TRUE ); 
+}
+
+bool ImportStructDlgOGR::GetRadio(int id)
+{
+	wxRadioButton *button = (wxRadioButton*) FindWindow(id);
+	if (!button)
+		return false;
+	return button->GetValue();
+}
+
+
+// WDR: handler implementations for ImportStructDlgOGR
+
+void ImportStructDlgOGR::OnChoiceLayerName( wxCommandEvent &event )
+{
+	wxString2 str = GetLayername()->GetStringSelection();
+	m_opt.m_strLayerName = str.mb_str();
+	UpdateFieldNames();
+	UpdateEnables();
+}
+
+void ImportStructDlgOGR::OnChoiceElevationFieldname( wxCommandEvent &event )
+{
+	wxString2 str = GetElevationFieldname()->GetStringSelection();
+	m_opt.m_strFieldNameElevation = str.mb_str();
+}
+
+void ImportStructDlgOGR::OnChoiceFileField( wxCommandEvent &event )
+{
+	wxString2 str = GetChoiceFileField()->GetStringSelection();
+	m_opt.m_strFieldNameFile = str.mb_str();
+}
+
+void ImportStructDlgOGR::OnChoiceHeightField( wxCommandEvent &event )
+{
+	wxString2 str = GetChoiceHeightField()->GetStringSelection();
+	m_opt.m_strFieldNameHeight = str.mb_str();
+}
+
+void ImportStructDlgOGR::OnRadio( wxCommandEvent &event )
+{
+	if (GetRadio(ID_TYPE_BUILDING))
+		m_iType = 1;
+	if (GetRadio(ID_TYPE_LINEAR))
+		m_iType = 2;
+	if (GetRadio(ID_TYPE_INSTANCE))
+		m_iType = 3;
+	UpdateEnables();
+}
+
+void ImportStructDlgOGR::OnInitDialog(wxInitDialogEvent& event)
+{
+	int i;
+	int iNumLayers;
+
+	m_opt.m_HeightType = StructImportOptions::METERS;
+	m_opt.m_ElevationType = StructImportOptions::ETMETERS;
+	m_opt.bFlip = false;
+	m_opt.bInsideOnly = false;
+	m_opt.bBuildFoundations = false;;
+	m_opt.bUse25DForElevation = false;
+
+	AddValidator(ID_INSIDE_AREA, &m_opt.bInsideOnly);
+	AddValidator(ID_FLIP, &m_opt.bFlip);
+	AddValidator(ID_BUILD_FOUNDATIONS, &m_opt.bBuildFoundations);
+	AddValidator(ID_USE_25D, &m_opt.bUse25DForElevation);
+	AddValidator(ID_CHOICE_HEIGHT_TYPE, (int *)&m_opt.m_HeightType);
+	AddValidator(ID_ELEVATION_UNITS, (int *)&m_opt.m_ElevationType);
+
+	// Select one of the radio buttons, whichever is enabled
+	m_iType = 1;
+	GetTypeBuilding()->SetValue(true);
+
+	UpdateEnables();
+
+	iNumLayers = m_pDatasource->GetLayerCount();
+	for (i = 0 ; i < iNumLayers; i++)
+		GetLayername()->Append(m_pDatasource->GetLayer(i)->GetLayerDefn()->GetName());
+
+	GetLayername()->Enable(iNumLayers > 1);
+
+	GetLayername()->SetSelection(0);
+	OnChoiceLayerName(wxCommandEvent());
+
+	UpdateFieldNames();
+
+	UpdateEnables();
+
+	TransferDataToWindow();
+}
+
+void ImportStructDlgOGR::UpdateFieldNames()
+{
+	OGRLayer *pLayer;
+	OGRFeatureDefn *pFeatureDefn;
+	OGRFieldDefn *pFieldDefn;
+	OGRFieldType FieldType;
+	int iNumFields;
+	int i;
+	int iCount;
+	
+	pLayer = m_pDatasource->GetLayer(GetLayername()->GetSelection());
+
+	if (NULL != pLayer)
+	{
+		pFeatureDefn = pLayer->GetLayerDefn();
+
+		if (NULL != pFeatureDefn)
+		{
+			iNumFields = pFeatureDefn->GetFieldCount();
+
+			GetUse25d()->Enable((pFeatureDefn->GetGeomType() & wkb25DBit) > 0);
+
+			iCount = GetChoiceHeightField()->GetCount();
+			for (i = 0; i < iCount; i++)
+				GetChoiceHeightField()->Delete(0);
+			iCount = GetElevationFieldname()->GetCount();
+			for (i = 0; i < iCount; i++)
+				GetElevationFieldname()->Delete(0);
+			iCount = GetChoiceFileField()->GetCount();
+			for (i = 0; i < iCount; i++)
+				GetChoiceFileField()->Delete(0);
+
+			GetChoiceHeightField()->Append(_T("(none)"));
+			GetChoiceFileField()->Append(_T("(none)"));
+			GetElevationFieldname()->Append(_T("(none)"));
+
+			for (i = 0; i < iNumFields; i++)
+			{
+				pFieldDefn = pFeatureDefn->GetFieldDefn(i);
+				if (NULL != pFieldDefn)
+				{
+					FieldType = pFieldDefn->GetType();
+					if ((FieldType == OFTInteger) || (FieldType == OFTReal))
+					{
+						GetChoiceHeightField()->Append(pFieldDefn->GetNameRef());
+						GetElevationFieldname()->Append(pFieldDefn->GetNameRef());
+					}
+					else if ((FieldType == OFTString) || (FieldType == OFTWideString))
+						GetChoiceFileField()->Append(pFieldDefn->GetNameRef());
+				}
+			}
+		}
+	}
+	GetChoiceFileField()->SetSelection(0);
+	OnChoiceFileField(wxCommandEvent());
+	GetChoiceHeightField()->SetSelection(0);
+	OnChoiceHeightField(wxCommandEvent());
+	GetElevationFieldname()->SetSelection(0);
+	OnChoiceElevationFieldname(wxCommandEvent());
+}
+
+void ImportStructDlgOGR::UpdateEnables()
+{
+	GetBuildFoundations()->Enable(m_iType == 1);
+	GetChoiceHeightField()->Enable(GetChoiceHeightField()->GetCount() > 1);
+	GetElevationFieldname()->Enable(GetElevationFieldname()->GetCount() > 1);
+	GetChoiceFileField()->Enable((GetChoiceFileField()->GetCount() > 1) && (m_iType == 3));
+}
+
---
+++ TerrainApps/VTBuilder/ImportStructDlgOGR.h
@@ -0,0 +1,78 @@
+//
+// Name: ImportStructDlgOGR.h
+//
+// Copyright (c) 2003 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#ifndef __ImportStructDlgOGR_H__
+#define __ImportStructDlgOGR_H__
+
+#ifdef __GNUG__
+	#pragma interface "ImportStructDlgOGR.cpp"
+#endif
+
+#ifndef WX_PRECOMP
+	#include "wx/wx.h"
+#endif
+
+#include "VTBuilder_wdr.h"
+#include "vtui/AutoDialog.h"
+#include "vtui/wxString2.h"
+#include "vtdata/StructArray.h"
+
+// WDR: class declarations
+
+//----------------------------------------------------------------------------
+// ImportStructDlgOGR
+//----------------------------------------------------------------------------
+
+class ImportStructDlgOGR: public AutoDialog
+{
+public:
+	ImportStructDlgOGR( wxWindow *parent, wxWindowID id, const wxString &title,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxDEFAULT_DIALOG_STYLE );
+	
+	// WDR: method declarations for ImportStructDlgOGR
+	wxChoice* GetLayername()  { return (wxChoice*) FindWindow( ID_LAYERNAME ); }
+	wxCheckBox* GetUse25d()  { return (wxCheckBox*) FindWindow( ID_USE_25D ); }
+	wxCheckBox* GetBuildFoundations()  { return (wxCheckBox*) FindWindow( ID_BUILD_FOUNDATIONS ); }
+	wxChoice* GetElevationUnits()  { return (wxChoice*) FindWindow( ID_ELEVATION_UNITS ); }
+	wxChoice* GetElevationFieldname()  { return (wxChoice*) FindWindow( ID_ELEVATION_FIELDNAME ); }
+	wxChoice* GetChoiceHeightType()  { return (wxChoice*) FindWindow( ID_CHOICE_HEIGHT_TYPE ); }
+	wxChoice* GetChoiceHeightField()  { return (wxChoice*) FindWindow( ID_CHOICE_HEIGHT_FIELD ); }
+	wxChoice* GetChoiceFileField()  { return (wxChoice*) FindWindow( ID_CHOICE_FILE_FIELD ); }
+	wxRadioButton* GetTypeInstance()  { return (wxRadioButton*) FindWindow( ID_TYPE_INSTANCE ); }
+	wxRadioButton* GetTypeLinear()  { return (wxRadioButton*) FindWindow( ID_TYPE_LINEAR ); }
+	wxRadioButton* GetTypeBuilding()  { return (wxRadioButton*) FindWindow( ID_TYPE_BUILDING ); }
+	void SetDatasource(OGRDataSource *pDatasource) { m_pDatasource = pDatasource; }
+	void UpdateFieldNames();
+	void UpdateEnables();
+	bool GetRadio(int id);
+
+	int	 m_iType;
+//	int	 m_iHeightType; // 0 = stories, 1 = meters, 2 = feet
+//	int	 m_iElevationUnits; // 0 = meters, 1 = feet
+
+	StructImportOptions m_opt;
+
+private:
+	// WDR: member variable declarations for ImportStructDlgOGR
+	OGRDataSource *m_pDatasource;
+
+private:
+	// WDR: handler declarations for ImportStructDlgOGR
+	void OnChoiceLayerName( wxCommandEvent &event );
+	void OnChoiceElevationFieldname( wxCommandEvent &event );
+	void OnChoiceFileField( wxCommandEvent &event );
+	void OnChoiceHeightField( wxCommandEvent &event );
+	void OnRadio( wxCommandEvent &event );
+	void OnInitDialog(wxInitDialogEvent& event);
+
+	DECLARE_EVENT_TABLE()
+};
+
+#endif	// __ImportStructDlgOGR_H__
+
