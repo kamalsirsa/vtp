799bf5bfe3a8507f49039e7f3b44f2d6b2e8e6d2
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-07-16 03:02:21 +0000

    automatically copy files up from their release folders
    git-svn-id: http://vtp.googlecode.com/svn/trunk@2045 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- mk_vtp_bin_zip.sh
+++ mk_vtp_bin_zip.sh
@@ -14,26 +14,40 @@ DIST_FILE3=${TARGETDIR}/vtp-apps-bin-${DATE}.zip
 # Create the archive containing the App Binaries
 rm -f $DIST_FILE3
 
+cp VTP/TerrainApps/BExtractor/Release/BExtractor.exe VTP/TerrainApps/BExtractor
 zip $DIST_FILE3 VTP/TerrainApps/BExtractor/BE.ini
 zip $DIST_FILE3 VTP/TerrainApps/BExtractor/BExtractor.exe
 zip $DIST_FILE3 VTP/TerrainApps/BExtractor/license.txt
 zip $DIST_FILE3 VTP/TerrainApps/BExtractor/Docs/*
 
+cp VTP/TerrainApps/CManager/Release_Unicode/CManager_u.exe VTP/TerrainApps/CManager
 zip $DIST_FILE3 VTP/TerrainApps/CManager/CManager_u.exe
-zip $DIST_FILE3 VTP/TerrainApps/CManager/CManager.ini
 zip $DIST_FILE3 VTP/TerrainApps/CManager/itemtypes.txt
 zip $DIST_FILE3 VTP/TerrainApps/CManager/Docs/*
 
-zip $DIST_FILE3 VTP/TerrainApps/Enviro/Enviro.ini
+cp VTP/TerrainApps/Enviro/Release_Unicode/wxEnviro_u.exe VTP/TerrainApps/Enviro
 zip $DIST_FILE3 VTP/TerrainApps/Enviro/wxEnviro_u.exe
 zip $DIST_FILE3 VTP/TerrainApps/Enviro/license.txt
 zip $DIST_FILE3 VTP/TerrainApps/Enviro/Docs/*
 zip $DIST_FILE3 VTP/TerrainApps/Enviro/Docs/Navigation/*
 
+# deal specially with Enviro.ini
+mv VTP/TerrainApps/Enviro/Enviro.ini VTP/TerrainApps/Enviro/Enviro_dev.ini
+cp VTP/TerrainApps/Enviro/Ship/Enviro.ini VTP/TerrainApps/Enviro
+zip $DIST_FILE3 VTP/TerrainApps/Enviro/Enviro.ini
+rm VTP/TerrainApps/Enviro/Enviro.ini
+mv VTP/TerrainApps/Enviro/Enviro_dev.ini VTP/TerrainApps/Enviro/Enviro.ini
+
+cp VTP/TerrainApps/VTBuilder/Release_Unicode/VTBuilder_u.exe VTP/TerrainApps/VTBuilder
 zip $DIST_FILE3 VTP/TerrainApps/VTBuilder/license.txt
 zip $DIST_FILE3 VTP/TerrainApps/VTBuilder/VTBuilder_u.exe
 zip $DIST_FILE3 VTP/TerrainApps/VTBuilder/VTBuilder.ini
 zip $DIST_FILE3 VTP/TerrainApps/VTBuilder/Docs/*
 zip $DIST_FILE3 VTP/TerrainApps/VTBuilder/Docs/images/*
+
+#deal specially with docs
+mv VTP/Docs/*.html VTP/TerrainApps
+zip $DIST_FILE3 VTP/TerrainApps/*.html
+mv VTP/TerrainApps/*.html VTP/Docs
 
 echo $DIST_FILE3 ready.
