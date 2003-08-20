9ef826c4fb039f388da83ee3fdbcddc9b7a8a877
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-08-20 02:26:44 +0000

    more fixes to the problematic wxString2::mb_str
    git-svn-id: http://vtp.googlecode.com/svn/trunk@2159 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtui/wxString2.cpp
+++ TerrainSDK/vtui/wxString2.cpp
@@ -129,7 +129,26 @@ wxString2::operator const char*() const
 char wxString2::s_buffer[MAX_WXSTRING2_SIZE];
 const char *wxString2::mb_str() const
 {
+#if SUPPORT_WSTRING
+	wstring2 ws(c_str());
+	return ws.to_utf8();
+#else
 	int result = wxWC2MB(s_buffer, c_str(), MAX_WXSTRING2_SIZE);
 	return s_buffer;
+#endif
 }
 #endif
+
+vtString wxString2::vt_str() const
+{
+#if SUPPORT_WSTRING && wxUSE_UNICODE
+	wstring2 ws(c_str());
+	vtString result(ws.to_utf8());
+	return result;
+#else
+	vtString result(mb_str());
+	return result;
+#endif
+}
+
+
