8a4b4ed183e733980a763c808a3c9799f5ecb1f6
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2003-11-21 04:01:32 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@2437 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/Enviro/wx/TerrManDlg.cpp
@@ -0,0 +1,290 @@
+//
+// Name: TerrManDlg.cpp
+//
+// Copyright (c) 2003 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#ifdef __GNUG__
+	#pragma implementation "TerrManDlg.cpp"
+#endif
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#include "Vtdata/TParams.h"
+#include "vtui/wxString2.h"
+#include "TerrManDlg.h"
+#include "../Options.h"
+#include "app.h"
+
+/////////////////////////
+
+class TMTreeItemData : public wxTreeItemData
+{
+public:
+	vtString m_strFile;
+	vtString m_strName;
+};
+
+// WDR: class implementations
+
+//---------------------------------------------------------------------------
+// TerrainManagerDlg
+//---------------------------------------------------------------------------
+
+// WDR: event table for TerrainManagerDlg
+
+BEGIN_EVENT_TABLE(TerrainManagerDlg,wxDialog)
+	EVT_INIT_DIALOG (TerrainManagerDlg::OnInitDialog)
+	EVT_TREE_SEL_CHANGED( ID_TREECTRL, TerrainManagerDlg::OnSelChanged )
+	EVT_TREE_DELETE_ITEM( ID_TREECTRL, TerrainManagerDlg::OnDeleteItem )
+	EVT_BUTTON( ID_ADD_PATH, TerrainManagerDlg::OnAddPath )
+	EVT_BUTTON( ID_ADD_TERRAIN, TerrainManagerDlg::OnAddTerrain )
+	EVT_BUTTON( ID_DELETE, TerrainManagerDlg::OnDelete )
+	EVT_BUTTON( ID_EDIT_PARAMS, TerrainManagerDlg::OnEditParams )
+	EVT_BUTTON( ID_COPY, TerrainManagerDlg::OnCopy )
+	EVT_BUTTON( wxID_OK, TerrainManagerDlg::OnOK )
+END_EVENT_TABLE()
+
+TerrainManagerDlg::TerrainManagerDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+	const wxPoint &position, const wxSize& size, long style ) :
+	wxDialog( parent, id, title, position, size, style | wxRESIZE_BORDER )
+{
+	// WDR: dialog function TerrManFunc for TerrainManagerDlg
+	TerrManFunc( this, TRUE ); 
+	m_iSelect = 0;
+}
+
+void TerrainManagerDlg::RefreshTreeContents()
+{
+	m_pTree->DeleteAllItems();
+
+	vtStringArray &paths = g_Options.m_DataPaths;
+	int i, num = paths.size();
+	wxString2 wstr, wstr2;
+
+	m_Root = m_pTree->AddRoot(_T("Terrain Data Paths"));
+
+	for (i = 0; i < num; i++)
+	{
+		vtString str = paths[i];
+
+		wstr = str;
+		wxTreeItemId hPath = m_pTree->AppendItem(m_Root, wstr);
+
+		vtString directory = str + "Terrains";
+		for (dir_iter it((const char *)directory); it != dir_iter(); ++it)
+		{
+			if (it.is_hidden() || it.is_directory())
+				continue;
+
+			std::string name1 = it.filename();
+			vtString name = name1.c_str();
+
+			// only look for ".ini" files
+			if (name.GetLength() < 5 || name.Right(4).CompareNoCase(".ini"))
+				continue;
+
+			wstr = name;
+			TParams params;
+			if (params.LoadFromFile(directory + "/" + name))
+			{
+				wstr += _T(" (");
+				wstr2.from_utf8(params.m_strName);
+				wstr += wstr2;
+				wstr += _T(")");
+			}
+
+			wxTreeItemId hItem = m_pTree->AppendItem(hPath, wstr);
+			TMTreeItemData *data = new TMTreeItemData;
+			data->m_strFile = name;
+			data->m_strName = params.m_strName;
+			m_pTree->SetItemData(hItem, data);
+		}
+		m_pTree->Expand(hPath);
+	}
+	m_pTree->Expand(m_Root);
+}
+
+wxString TerrainManagerDlg::GetCurrentPath()
+{
+	wxTreeItemId parent = m_pTree->GetItemParent(m_Selected);
+	return m_pTree->GetItemText(parent);
+}
+
+wxString TerrainManagerDlg::GetCurrentTerrainPath()
+{
+	wxString2 path = GetCurrentPath();
+	TMTreeItemData *data = (TMTreeItemData *) m_pTree->GetItemData(m_Selected);
+	path += "Terrains/";
+	path += data->m_strFile;
+	return path;
+}
+
+// WDR: handler implementations for TerrainManagerDlg
+
+void TerrainManagerDlg::OnCopy( wxCommandEvent &event )
+{
+	if (m_iSelect != 2)
+		return;
+
+	TMTreeItemData *data = (TMTreeItemData *) m_pTree->GetItemData(m_Selected);
+	wxString2 file = data->m_strFile;
+
+	wxString2 msg = "Please enter the name for the terrain copy.";
+	wxString2 str = wxGetTextFromUser(msg, _T("Add Copy of Terrain"), file);
+	if (str == _T(""))
+		return;
+
+	TParams params;
+	params.LoadFromFile(GetCurrentTerrainPath().mb_str());
+
+	wxString newpath = GetCurrentPath();
+	newpath += "Terrains/";
+	newpath += str;
+	params.SaveToFile(newpath.mb_str());
+	RefreshTreeContents();
+}
+
+void TerrainManagerDlg::OnEditParams( wxCommandEvent &event )
+{
+	if (m_iSelect != 2)
+		return;
+
+	EditTerrainParameters(this, GetCurrentTerrainPath().mb_str());
+}
+
+void TerrainManagerDlg::OnDelete( wxCommandEvent &event )
+{
+	vtStringArray &paths = g_Options.m_DataPaths;
+	if (m_iSelect == 1)
+	{
+		// remove path
+		wxString2 path = m_pTree->GetItemText(m_Selected);
+		for (vtStringArray::iterator it = paths.begin(); it != paths.end(); it++)
+		{
+			if (*it == vtString(path.mb_str()))
+			{
+				paths.erase(it);
+				break;
+			}
+		}
+	}
+	if (m_iSelect == 2)
+	{
+		// delete terrain .ini file
+		wxTreeItemId parent = m_pTree->GetItemParent(m_Selected);
+		wxString2 path = m_pTree->GetItemText(parent);
+		path += "Terrains/";
+
+		TMTreeItemData *data = (TMTreeItemData *) m_pTree->GetItemData(m_Selected);
+		path += data->m_strFile;
+		vtDeleteFile(path.mb_str());
+	}
+	RefreshTreeContents();
+}
+
+void TerrainManagerDlg::OnAddTerrain( wxCommandEvent &event )
+{
+	wxString2 msg = "Please enter the name for a new terrain .ini file.";
+	wxString2 str = wxGetTextFromUser(msg, _T("Add Terrain"));
+	if (str == _T(""))
+		return;
+
+	if (str.Right(4).CmpNoCase(".ini"))
+		str += ".ini";
+
+	wxString2 path = m_pTree->GetItemText(m_Selected);
+	path += "Terrains";
+
+	// Make sure Terrains directory exists
+	vtCreateDir(path.mb_str());
+
+	path += "/";
+	path += str;
+
+	TParams params;
+	params.m_strName = "Untitled";
+	params.SaveToFile(path.mb_str());
+
+	RefreshTreeContents();
+}
+
+void TerrainManagerDlg::OnAddPath( wxCommandEvent &event )
+{
+	wxString2 msg = "Please enter an absolute or relative path.";
+	wxString2 str = wxGetTextFromUser(msg, _T("Add Path"));
+	if (str == _T(""))
+		return;
+
+	// Make sure there is a trailing slash
+	if (str.Length() > 1)
+	{
+		char ch = str.GetChar(str.Length()-1);
+		if (ch != '/' && ch != '\\')
+			str += _T("/");
+	}
+
+	g_Options.m_DataPaths.push_back(vtString(str.mb_str()));
+	RefreshTreeContents();
+}
+
+void TerrainManagerDlg::OnInitDialog(wxInitDialogEvent& event) 
+{
+	m_pTree = GetTree();
+
+	RefreshTreeContents();
+	UpdateEnabling();
+	wxWindow::OnInitDialog(event);
+}
+
+void TerrainManagerDlg::OnOK( wxCommandEvent &event )
+{
+	wxDialog::OnOK(event);
+}
+
+
+//
+// Tree events
+//
+void TerrainManagerDlg::OnDeleteItem( wxTreeEvent &event )
+{
+}
+
+void TerrainManagerDlg::OnSelChanged( wxTreeEvent &event )
+{
+	wxTreeItemId item = event.GetItem();
+	m_Selected = item;
+
+//  MyTreeItemData *data = (MyTreeItemData *)m_pTree->GetItemData(item);
+
+	if (item == m_Root)
+		m_iSelect = 0;
+	else
+	{
+		item = m_pTree->GetItemParent(item);
+		if (item == m_Root)
+			m_iSelect = 1;
+		else
+		{
+			item = m_pTree->GetItemParent(item);
+			if (item == m_Root)
+				m_iSelect = 2;
+		}
+	}
+	UpdateEnabling();
+}
+
+void TerrainManagerDlg::UpdateEnabling()
+{
+	GetAddTerrain()->Enable(m_iSelect == 1);
+	GetCopy()->Enable(m_iSelect == 2);
+	GetDelete()->Enable(m_iSelect == 1 || m_iSelect == 2);
+	if (m_iSelect == 1)
+		GetDelete()->SetLabel(_T("Remove"));
+	else if (m_iSelect == 2)
+		GetDelete()->SetLabel(_T("Delete"));
+	GetEditParams()->Enable(m_iSelect == 2);
+}
+
