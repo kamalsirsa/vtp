b44aff271c3a380498c91d858fbf7fc887d569d1
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2004-02-22 16:04:56 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@2868 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainSDK/vtdata/vtTime.cpp
@@ -0,0 +1,94 @@
+//
+// vtTime.cpp
+//
+// Copyright (c) 2004 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#include "vtTime.h"
+
+///////////////////////////////////////////////////////
+
+time_t vtTime::s_DifferenceFromGMT = (time_t) -1;
+
+vtTime::vtTime()
+{
+	// default to current time
+	GetSystemTime();
+}
+
+void vtTime::_UpdateTM()
+{
+	struct tm *t = gmtime(&m_time);
+	m_tm = *t;
+}
+
+void vtTime::Increment(int secs)
+{
+	m_time += secs;
+	_UpdateTM();
+}
+
+void vtTime::GetSystemTime()
+{
+	time(&m_time);
+	_UpdateTM();
+}
+
+void vtTime::SetDate(int year, int month, int day)
+{
+	m_tm.tm_year = year - 1900;
+	m_tm.tm_mon = month;
+	m_tm.tm_mday = day;
+	m_time = mktime(&m_tm);
+	_UpdateTM();
+}
+
+void vtTime::GetDate(int &year, int &month, int &day) const
+{
+	year = 1900 + m_tm.tm_year;
+	month = m_tm.tm_mon;
+	day = m_tm.tm_mday;
+}
+
+void vtTime::SetTimeOfDay(int hr, int min, int sec)
+{
+	if (s_DifferenceFromGMT == (time_t) -1)
+	{
+		// Determine the offset between local (meaning the timezome of the
+		//  computer which is running this software) and Greenwich Mean time,
+		//  and use it to compensate for the fact mktime always tries to
+		//  factor in the "local time zone".
+		time_t dummy = 20000;
+		struct tm tm_gm, tm_local;
+		tm_gm = *gmtime(&dummy);
+		tm_local = *localtime(&dummy);
+		s_DifferenceFromGMT = mktime(&tm_local) - mktime(&tm_gm);
+	}
+
+	m_tm.tm_hour = hr;
+	m_tm.tm_min = min;
+	m_tm.tm_sec = sec;
+	m_time = mktime(&m_tm);
+
+	m_time += s_DifferenceFromGMT;
+	_UpdateTM();
+}
+
+void vtTime::GetTimeOfDay(int &hr, int &min, int &sec) const
+{
+	hr = m_tm.tm_hour;
+	min = m_tm.tm_min;
+	sec = m_tm.tm_sec;
+}
+
+int vtTime::GetSecondOfDay() const
+{
+	return (m_tm.tm_hour * 60 + m_tm.tm_min) * 60 + m_tm.tm_sec;
+}
+
+time_t vtTime::GetTime() const
+{
+	return m_time;
+}
+
---
+++ TerrainSDK/vtdata/vtTime.h
@@ -0,0 +1,49 @@
+//
+// vtTime.h
+//
+// Copyright (c) 2004 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#ifndef VTTIMEH
+#define VTTIMEH
+
+#include <time.h>
+
+///////////////////////////////////////////////////
+
+/**
+ * This class encapsulates time (in increments of whole seconds.)
+ * It always stores UTC (GMT).  If you need "local time" for some specific
+ *  time zone, you will need to call other methods to compute that.
+ */
+class vtTime
+{
+public:
+	vtTime();
+
+	void _UpdateTM();
+
+	void GetSystemTime();
+
+	void SetDate(int year, int month, int day);
+	void GetDate(int &year, int &month, int &day) const;
+
+//	void SetLocalTime(int hr, int min, int sec);
+	void SetTimeOfDay(int hr, int min, int sec);
+
+//	void SetTime(time_t time);
+	void GetTimeOfDay(int &hr, int &min, int &sec) const;
+	int GetSecondOfDay() const;
+
+	time_t GetTime() const;
+	void Increment(int secs);
+
+	tm m_tm;			// always stores GMT
+	time_t m_time;		// always stores GMT
+
+	static time_t s_DifferenceFromGMT;
+};
+
+#endif // VTTIMEH
+
