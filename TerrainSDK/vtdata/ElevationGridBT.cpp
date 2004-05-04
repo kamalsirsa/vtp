2fb353cfb886a3d37886214a3abb42ece715f911
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2004-05-04 05:45:17 +0000

    split off parts of ElevationGridIO into ElevationGridDEM and ElevationGridBT
    git-svn-id: http://vtp.googlecode.com/svn/trunk@3262 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainSDK/vtdata/ElevationGridBT.cpp
@@ -0,0 +1,371 @@
+//
+// vtElevationGridDEM.cpp
+//
+// This modules contains the implementations of the BT file I/O methods of
+// the class vtElevationGrid.
+//
+// Copyright (c) 2001-2004 Virtual Terrain Project.
+// Free for all uses, see license.txt for details.
+//
+
+#include "ElevationGrid.h"
+#include "ByteOrder.h"
+
+
+/** Loads just the header information from a BT (Binary Terrain) file.
+ * \par
+ * This can be useful if you want to check the information such as the
+ * size of the data, without reading the entire file.
+ * This method works whether it is given a normal BT file, or one which
+ * has been compressed with gzip.
+ * \returns \c true if the header was successfully parsed.
+ */
+bool vtElevationGrid::LoadBTHeader(const char *szFileName)
+{
+	// Avoid trouble with '.' and ',' in Europe
+	LocaleWrap normal_numbers(LC_NUMERIC, "C");
+
+	// The gz functions (gzopen etc.) behave exactly like the stdlib
+	//  functions (fopen etc.) in the case where the input file is not in
+	//  gzip format, so we can simply use them without worry.
+	gzFile fp = gzopen(szFileName, "rb");
+	if (!fp)
+		return false;		// Cannot Open File
+
+	char buf[11];
+	gzread(fp, buf, 10);
+	buf[10] = '\0';
+
+	if (strncmp(buf, "binterr", 7))
+	{
+		gzclose(fp);
+		return false;		// Not a current BT file
+	}
+
+	// Get version
+	int iMajor = (int) buf[7]-'0';
+	int iMinor = (int) buf[9]-'0';
+
+	// NOTE:  BT format is little-endian
+	GZFRead(&m_iColumns, DT_INT, 1, fp, BO_LITTLE_ENDIAN);
+	GZFRead(&m_iRows,	   DT_INT, 1, fp, BO_LITTLE_ENDIAN);
+
+	// Default to internal projection
+	short external = 0;
+
+	short svalue, proj_type, zone, datum;
+	int ivalue;
+	float ftmp;
+	if (iMajor == 1 && iMinor == 0)		// Version 1.0
+	{
+		// data size
+		GZFRead(&ivalue, DT_INT, 1, fp, BO_LITTLE_ENDIAN);
+
+		// UTM flag
+		GZFRead(&svalue, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+		proj_type = (svalue == 1);
+
+		// UTM zone
+		GZFRead(&zone, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+
+		// 1.0 didn't support Datum, so assume WGS84
+		datum = EPSG_DATUM_WGS84;
+
+		// coordinate extents left-right
+		GZFRead(&ftmp, DT_FLOAT, 1, fp, BO_LITTLE_ENDIAN);
+		m_EarthExtents.left = ftmp;
+		GZFRead(&ftmp, DT_FLOAT, 1, fp, BO_LITTLE_ENDIAN);
+		m_EarthExtents.right = ftmp;
+
+		// coordinate extents bottom-top
+		GZFRead(&ftmp, DT_FLOAT, 1, fp, BO_LITTLE_ENDIAN);
+		m_EarthExtents.bottom = ftmp;
+		GZFRead(&ftmp, DT_FLOAT, 1, fp, BO_LITTLE_ENDIAN);
+		m_EarthExtents.top = ftmp;
+
+		// is the data floating point or integers?
+		GZFRead(&m_bFloatMode, DT_INT, 1, fp, BO_LITTLE_ENDIAN);
+		if (m_bFloatMode != true)
+			m_bFloatMode = false;
+	}
+	else if (iMajor == 1 && (iMinor == 1 || iMinor == 2 || iMinor == 3))
+	{
+		// Version 1.1, 1.2, or 1.3
+
+		// data size
+		GZFRead(&svalue, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+
+		// Is floating point data?
+		GZFRead(&svalue, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+		m_bFloatMode = (svalue == 1);
+
+		// Projection (0 = geo, 1 = utm, 2 = feet, 3 = u.s. feet)
+		GZFRead(&proj_type, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+
+		// UTM zone (ignore unless projection == 1)
+		GZFRead(&zone, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+
+		// Datum (ignore unless projection == 0 or 1)
+		GZFRead(&datum, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+
+		// coordinate extents
+		GZFRead(&m_EarthExtents.left, DT_DOUBLE, 1, fp, BO_LITTLE_ENDIAN);
+		GZFRead(&m_EarthExtents.right, DT_DOUBLE, 1, fp, BO_LITTLE_ENDIAN);
+		GZFRead(&m_EarthExtents.bottom, DT_DOUBLE, 1, fp, BO_LITTLE_ENDIAN);
+		GZFRead(&m_EarthExtents.top, DT_DOUBLE, 1, fp, BO_LITTLE_ENDIAN);
+
+		// External projection flag
+		GZFRead(&external, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+	}
+	if (iMajor == 1 && iMinor == 3)
+	{
+		GZFRead(&m_fVMeters, DT_FLOAT, 1, fp, BO_LITTLE_ENDIAN);
+	}
+
+	// Set up projection
+	if (external == 1)
+	{
+		// Read external projection (.prj) file
+		if (!m_proj.ReadProjFile(szFileName))
+			return false;
+	}
+	else
+	{
+		// Internal specification: proj_type 0 = Geo, 1 = UTM
+		m_proj.SetProjectionSimple(proj_type == 1, zone, datum);
+	}
+
+	ComputeCornersFromExtents();
+
+	gzclose(fp);
+	return true;
+}
+
+
+/** Loads from a BT (Binary Terrain) file.
+ * \par
+ * Both the current version (1.1) and older BT versions are supported.
+ * This method works whether it is given a normal BT file, or one which
+ * has been compressed with gzip.
+ * \returns \c true if the file was successfully opened and read.
+ */
+bool vtElevationGrid::LoadFromBT(const char *szFileName, void progress_callback(int))
+{
+	// First load the header
+	if (!LoadBTHeader(szFileName))
+		return false;
+
+	gzFile fp = gzopen(szFileName, "rb");
+	if (!fp)
+		return false;		// Cannot Open File
+
+	// elevation data always starts at offset 256
+	gzseek(fp, 256, SEEK_SET);
+
+	_AllocateArray();
+
+	int i;
+#if 0
+	// slow way
+	int value;
+	float fvalue;
+	for (i = 0; i < m_iColumns; i++)
+	{
+		if (progress_callback != NULL) progress_callback(i * 100 / m_iColumns);
+		for (j = 0; j < m_iRows; j++)
+		{
+			if (m_bFloatMode)
+			{
+				GZFRead(&fvalue, DT_FLOAT, 1, fp, BO_LITTLE_ENDIAN);
+				SetFValue(i, j, fvalue);
+			}
+			else
+			{
+				GZFRead(&svalue, DT_SHORT, 1, fp, BO_LITTLE_ENDIAN);
+				SetValue(i, j, svalue);
+			}
+		}
+	}
+#else
+	// fast way
+	if (m_bFloatMode)
+	{
+		for (i = 0; i < m_iColumns; i++)
+		{
+			if (progress_callback != NULL) progress_callback(i * 100 / m_iColumns);
+			GZFRead(m_pFData + (i*m_iRows), DT_FLOAT, m_iRows, fp, BO_LITTLE_ENDIAN);
+		}
+	}
+	else
+	{
+		for (i = 0; i < m_iColumns; i++)
+		{
+			if (progress_callback != NULL) progress_callback(i * 100 / m_iColumns);
+			GZFRead(m_pData + (i*m_iRows), DT_SHORT, m_iRows, fp, BO_LITTLE_ENDIAN);
+		}
+	}
+#endif
+
+	// be sure to close the file!
+	gzclose(fp);
+
+	ComputeHeightExtents();
+	return true;
+}
+
+
+/**
+ * Writes the grid to a BT (Binary Terrain) file.
+ * The current BT format version (1.3) is written.
+ *
+ * \param szFileName		The file name to write to.
+ * \param progress_callback If supplied, this function will be called back
+ *				with a value of 0 to 100 as the operation progresses.
+ * \param bGZip			If true, the data will be compressed with gzip.
+ *				If true, you should Use a filename ending with ".gz".
+ */
+bool vtElevationGrid::SaveToBT(const char *szFileName,
+							   void progress_callback(int), bool bGZip)
+{
+	int w = m_iColumns;
+	int h = m_iRows;
+	short zone = m_proj.GetUTMZone();
+	short datum = (short) m_proj.GetDatum();
+	short isfloat = (short) IsFloatMode();
+	short external = 1;		// always true: we always write an external .prj file
+
+	LinearUnits units = m_proj.GetUnits();
+	int hunits = (int) units;
+
+	// Latest header, version 1.2
+	short datasize = m_bFloatMode ? 4 : 2;
+
+	if (bGZip == false)
+	{
+		// Use conventional IO
+		FILE *fp = fopen(szFileName, "wb");
+		if (!fp)
+			return false;
+
+		fwrite("binterr1.3", 10, 1, fp);
+		fwrite(&w, 4, 1, fp);
+		fwrite(&h, 4, 1, fp);
+		fwrite(&datasize, 2, 1, fp);
+		fwrite(&isfloat, 2, 1, fp);
+		fwrite(&hunits, 2, 1, fp);		// Horizontal Units (0, 1, 2, 3)
+		fwrite(&zone, 2, 1, fp);		// UTM zone
+		fwrite(&datum, 2, 1, fp);		// Datum
+
+		// coordinate extents
+		fwrite(&m_EarthExtents.left, 8, 1, fp);
+		fwrite(&m_EarthExtents.right, 8, 1, fp);
+		fwrite(&m_EarthExtents.bottom, 8, 1, fp);
+		fwrite(&m_EarthExtents.top, 8, 1, fp);
+
+		fwrite(&external, 2, 1, fp);	// External projection specification
+		fwrite(&m_fVMeters, 4, 1, fp);	// External projection specification
+
+		// now write the data: always starts at offset 256
+		fseek(fp, 256, SEEK_SET);
+
+#if 0
+		// slow way, one heixel at a time
+		for (int i = 0; i < w; i++)
+		{
+			if (progress_callback != NULL) progress_callback(i * 100 / w);
+			for (j = 0; j < h; j++)
+			{
+				if (m_bFloatMode) {
+					fvalue = GetFValue(i, j);
+					fwrite(&fvalue, datasize, 1, fp);
+				} else {
+					svalue = GetValue(i, j);
+					fwrite(&svalue, datasize, 1, fp);
+				}
+			}
+		}
+#else
+		// fast way, with the assumption that the data is stored column-first in memory
+		if (m_bFloatMode)
+		{
+			for (int i = 0; i < w; i++)
+			{
+				if (progress_callback != NULL) progress_callback(i * 100 / w);
+				fwrite(m_pFData + (i * m_iRows), 4, m_iRows, fp);
+			}
+		}
+		else
+		{
+			for (int i = 0; i < w; i++)
+			{
+				if (progress_callback != NULL) progress_callback(i * 100 / w);
+				fwrite(m_pData + (i * m_iRows), 2, m_iRows, fp);
+			}
+		}
+#endif
+		fclose(fp);
+	}
+	else
+	{
+		// Use GZip IO
+		gzFile fp = gzopen(szFileName, "wb");
+		if (!fp)
+			return false;
+
+		gzwrite(fp, (void *)"binterr1.3", 10);
+		gzwrite(fp, &w, 4);
+		gzwrite(fp, &h, 4);
+		gzwrite(fp, &datasize, 2);
+		gzwrite(fp, &isfloat, 2);
+		gzwrite(fp, &hunits, 2);		// Horizontal Units (0, 1, 2, 3)
+		gzwrite(fp, &zone, 2);		// UTM zone
+		gzwrite(fp, &datum, 2);		// Datum
+
+		// coordinate extents
+		gzwrite(fp, &m_EarthExtents.left, 8);
+		gzwrite(fp, &m_EarthExtents.right, 8);
+		gzwrite(fp, &m_EarthExtents.bottom, 8);
+		gzwrite(fp, &m_EarthExtents.top, 8);
+
+		gzwrite(fp, &external, 2);	// External projection specification
+		gzwrite(fp, &m_fVMeters, 4);	// External projection specification
+
+		// now write the data: always starts at offset 256
+		gzseek(fp, 256, SEEK_SET);
+
+		// fast way, with the assumption that the data is stored column-first in memory
+		if (m_bFloatMode)
+		{
+			for (int i = 0; i < w; i++)
+			{
+				if (progress_callback != NULL) progress_callback(i * 100 / w);
+				gzwrite(fp, m_pFData + (i * m_iRows), 4 * m_iRows);
+			}
+		}
+		else
+		{
+			for (int i = 0; i < w; i++)
+			{
+				if (progress_callback != NULL) progress_callback(i * 100 / w);
+				gzwrite(fp, m_pData + (i * m_iRows), 2 * m_iRows);
+			}
+		}
+		gzclose(fp);
+	}
+
+	if (external)
+	{
+		// Write external projection file (.prj)
+		char prj_name[256];
+		strcpy(prj_name, szFileName);
+		int len = strlen(prj_name);
+		if (bGZip)
+			strcpy(prj_name + len - 6, ".prj"); // overwrite the .bt.gz
+		else
+			strcpy(prj_name + len - 3, ".prj"); // overwrite the .bt
+		m_proj.WriteProjFile(prj_name);
+	}
+
+	return true;
+}
+
---
+++ TerrainSDK/vtdata/ElevationGridDEM.cpp
@@ -0,0 +1,410 @@
+//
+// vtElevationGridDEM.cpp
+//
+// This modules contains the implementations of the DEM file I/O methods of
+// the class vtElevationGrid.
+//
+// Copyright (c) 2001-2004 Virtual Terrain Project.
+// Free for all uses, see license.txt for details.
+//
+
+#include "ElevationGrid.h"
+#include "vtLog.h"
+
+// ************** DConvert - DEM Helper function ****************
+
+double DConvert(FILE *fp, int length)
+{
+	char szCharString[64];
+
+	fread(szCharString, length, 1, fp);
+	szCharString[length] = 0;
+
+	return atof(szCharString);
+}
+
+int IConvert(FILE *fp, int length)
+{
+	char szCharString[64];
+
+	fread(szCharString, length, 1, fp);
+	szCharString[length] = 0;
+
+	return atoi(szCharString);
+}
+
+/**
+ * Loads elevation from a USGS DEM file.
+ *
+ * Some non-standard variations of the DEM format are supported.
+ * \returns \c true if the file was successfully opened and read.
+ */
+bool vtElevationGrid::LoadFromDEM(const char *szFileName,
+								  void progress_callback(int))
+{
+	if (progress_callback != NULL) progress_callback(0);
+
+	FILE *fp = fopen(szFileName,"rb");
+	if (!fp)		// Cannot Open File
+		return false;
+
+	// check for version of DEM format
+	int		iRow, iColumn;
+	char buffer[158];
+
+	fseek(fp, 864, 0);
+	fread(buffer, 144, 1, fp);
+	bool bOldFormat = (strncmp(buffer, "     1     1", 12) == 0);
+	bool bNewFormat = false;
+	bool bFixedLength = true;
+	int  iDataStartOffset;
+	int  i, j;
+
+	if (bOldFormat)
+		iDataStartOffset = 1024;	// 1024 is record length
+	else
+	{
+		fseek(fp, 1024, 0); 		// Check for New Format
+		iRow = IConvert(fp, 6);
+		iColumn = IConvert(fp, 6);
+		if (iRow==1 && iColumn==1)	// File OK?
+		{
+			bNewFormat = true;
+			iDataStartOffset = 1024;
+		}
+		else
+		{
+			// might be the Non-fixed-length record format
+			// Record B can start anywhere from 865 to 1023
+			// Record B is identified by starting with the row/column
+			//  of its first profile, "     1     1"
+			fseek(fp, 865, 0);
+			fread(buffer, 158, 1, fp);
+			for (i = 0; i < 158-12; i++)
+			{
+				if (!strncmp(buffer+i, "     1     1", 12))
+				{
+					// Found it
+					bFixedLength = false;
+					iDataStartOffset = 865+i;
+					break;
+				}
+			}
+			if (i == 144)
+			{
+				// Not a DEM file
+				fclose(fp);
+				return false;
+			}
+		}
+	}
+
+	// Read the embedded DEM name
+	char szName[41];
+	fseek(fp, 0, 0);
+	fgets(szName, 41, fp);
+	int len = strlen(szName);	// trim trailing whitespace
+	while (len > 0 && szName[len-1] == ' ')
+	{
+		szName[len-1] = 0;
+		len--;
+	}
+	m_strOriginalDEMName = szName;
+
+	fseek(fp, 156, 0);
+	int iCoordSystem = IConvert(fp, 6);
+	int iUTMZone = IConvert(fp, 6);
+
+	fseek(fp, 168, 0);
+	double dProjParams[15];
+	for (i = 0; i < 15; i++)
+		dProjParams[i] = DConvert(fp, 24);
+
+	int iDatum = EPSG_DATUM_NAD27;	// default
+
+	// OLD format header ends at byte 864 (0x360); new format has Datum
+	if (bNewFormat)
+	{
+		// year of data compilation
+		char szDateBuffer[5];
+		fseek(fp, 876, 0);		// 0x36C
+		fread(szDateBuffer, 4, 1, fp);
+		szDateBuffer[4] = 0;
+
+		// Horizontal datum
+		// 1=North American Datum 1927 (NAD 27)
+		// 2=World Geodetic System 1972 (WGS 72)
+		// 3=WGS 84
+		// 4=NAD 83
+		// 5=Old Hawaii Datum
+		// 6=Puerto Rico Datum
+		fseek(fp, 890, 0);	// 0x37A
+		int datum = IConvert(fp, 2);
+		VTLOG("DEM Reader: Read Datum Value %d\n", datum);
+		switch (datum)
+		{
+			case 1: iDatum = EPSG_DATUM_NAD27; break;
+			case 2: iDatum = EPSG_DATUM_WGS72; break;
+			case 3: iDatum = EPSG_DATUM_WGS84; break;
+			case 4:	iDatum = EPSG_DATUM_NAD83; break;
+			case 5: iDatum = EPSG_DATUM_OLD_HAWAIIAN; break;
+			case 6: iDatum = EPSG_DATUM_PUERTO_RICO; break;
+		}
+	}
+
+	// Set up the projection
+	bool bGeographic = false;
+	bool bSuccessfulCRS = true;
+	switch (iCoordSystem)
+	{
+	case 0:		// geographic (lat-lon)
+		bGeographic = true;
+		iUTMZone = -1;
+		bSuccessfulCRS = m_proj.SetProjectionSimple(false, iUTMZone, iDatum);
+		break;
+	case 1:		// utm
+		m_proj.SetProjectionSimple(true, iUTMZone, iDatum);
+		break;
+	case 3:		// Albers Conical Equal Area
+		{
+			double semi_major = dProjParams[0];
+			double eccentricity = dProjParams[1];
+			double lat_1st_std_parallel = dProjParams[2];
+			double lat_2nd_std_parallel = dProjParams[3];
+			double lon_central_meridian = dProjParams[4];
+			double lat_origin = dProjParams[5];
+			double false_easting = dProjParams[6];
+			double false_northing = dProjParams[7];
+
+			m_proj.SetGeogCSFromDatum(iDatum);
+			m_proj.SetACEA(lat_1st_std_parallel, lat_2nd_std_parallel, lat_origin,
+				lon_central_meridian, false_easting, false_northing);
+		}
+		break;
+	case 2:		// State Plane (!)
+	case 4:		// Lambert Conformal
+	case 5:		// Mercator
+	case 6:		// Polar Stereographic
+	case 7:		// Polyconic
+	case 8:		// Equidistant Conic Type A / B
+	case 9:		// Transverse Mercator
+	case 10:	// Stereographic
+	case 11:	// Lambert Azimuthal Equal-Area
+	case 12:	// Azimuthal Equidistant
+	case 13:	// Gnomonic
+	case 14:	// Orthographic
+	case 15:	// General Vertical Near-Side Perspective
+	case 16:	// Sinusoidal (Plate Caree)
+	case 17:	// Equirectangular
+	case 18:	// Miller Cylindrical
+	case 19:	// Van Der Grinten I
+	case 20:	// Oblique Mercator
+		VTLOG("Warning!  We don't yet support DEM coordinate system %d.\n", iCoordSystem);
+		break;
+	}
+
+	// We must have a functional CRS, or it will sebsequently fail
+	if (!bSuccessfulCRS)
+		return false;
+
+	fseek(fp, 528, 0);
+	int iGUnit = IConvert(fp, 6);
+	int iVUnit = IConvert(fp, 6);
+
+	// Ground (Horizontal) Units in meters
+	double	fGMeters;
+	switch (iGUnit)
+	{
+	case 0: fGMeters = 1.0;		break;	// 0 = radians (never encountered)
+	case 1: fGMeters = 0.3048;	break;	// 1 = feet
+	case 2: fGMeters = 1.0;		break;	// 2 = meters
+	case 3: fGMeters = 30.922;	break;	// 3 = arc-seconds
+	}
+
+	// Vertical Units in meters
+	double	fVertUnits;
+	switch (iVUnit)
+	{
+	case 1:  fVertUnits = 0.3048f; break;	// feet to meter conversion
+	case 2:  fVertUnits = 1.0f;	   break;	// meters == meters
+	default: fVertUnits = 1.0f;	   break;	// anything else, assume meters
+	}
+
+	fseek(fp, 816, 0);
+	double dxdelta = DConvert(fp, 12);
+	double dydelta = DConvert(fp, 12);
+	double dzdelta = DConvert(fp, 12);
+
+	m_bFloatMode = false;
+
+	// Read the coordinates of the 4 corners
+	DPoint2	corners[4];			// SW, NW, NE, SE
+	fseek(fp, 546, 0);
+	for (i = 0; i < 4; i++)
+	{
+		corners[i].x = DConvert(fp, 24);
+		corners[i].y = DConvert(fp, 24);
+	}
+
+	if (bGeographic)
+	{
+		for (i = 0; i < 4; i++)
+		{
+			// convert arcseconds to degrees
+			m_Corners[i].x = corners[i].x / 3600.0;
+			m_Corners[i].y = corners[i].y / 3600.0;
+		}
+	}
+	else
+	{
+		// some linear coordinate system
+		for (i = 0; i < 4; i++)
+			m_Corners[i] = corners[i];
+	}
+
+	double dElevMin = DConvert(fp, 24);
+	double dElevMax = DConvert(fp, 24);
+
+	fseek(fp, 852, 0);
+	int rows = IConvert(fp, 6);
+	int iProfiles = IConvert(fp, 6);
+
+	m_iColumns = iProfiles;
+
+	// values we'll need while scanning the elevation profiles
+	int		iProfileRows, iProfileCols;
+	int		iElev;
+	double	dLocalDatumElev, dProfileMin, dProfileMax;
+	int		ygap;
+	double	dMinY;
+	DPoint2 start;
+
+	if (bGeographic)
+	{
+		// If it's in degrees, it's flush square, so we can simply
+		// derive the extents (m_EarthExtents) from the quad corners (m_Corners)
+		ComputeExtentsFromCorners();
+		dMinY = std::min(corners[0].y, corners[3].y);
+	}
+	else
+	{
+		m_EarthExtents.SetRect(1E9, -1E9, -1E9, 1E9);
+
+		if (!bFixedLength)
+			fseek(fp, iDataStartOffset, 0);
+		// Need to scan over all the profiles, accumulating the TRUE
+		// extents of the actual data points.
+		int record = 0;
+		int data_len;
+		for (i = 0; i < iProfiles; i++)
+		{
+			if (bFixedLength)
+				fseek(fp, iDataStartOffset + (record * 1024), 0);
+
+			// We cannot use IConvert here, because there *might* be a spurious LF
+			// after the number - seen in some rare files.
+			fscanf(fp, "%d", &iRow);
+			iColumn = IConvert(fp, 6);
+			// assert(iColumn == i+1);
+			iProfileRows = IConvert(fp, 6);
+			iProfileCols = IConvert(fp, 6);
+
+			start.x = DConvert(fp, 24);
+			start.y = DConvert(fp, 24);
+			m_EarthExtents.GrowToContainPoint(start);
+			start.y += (iProfileRows * dydelta);
+			m_EarthExtents.GrowToContainPoint(start);
+
+			if (bFixedLength)
+			{
+				record++;
+				data_len = 144 + (iProfileRows * 6);
+				while (data_len > 1020)	// max bytes in a record
+				{
+					data_len -= 1020;
+					record++;
+				}
+			}
+			else
+			{
+				dLocalDatumElev = DConvert(fp, 24);
+				dProfileMin = DConvert(fp, 24);
+				dProfileMax = DConvert(fp, 24);
+				for (j = 0; j < iProfileRows; j++)
+				{
+					// We cannot use IConvert here, because there *might* be a spurious LF
+					// after the number - seen in some rare files.
+					fscanf(fp, "%d", &iElev);
+				}
+			}
+		}
+		dMinY = m_EarthExtents.bottom;
+	}
+
+	// Compute number of rows
+	double	fRows;
+	if (bGeographic)
+	{
+		// degrees
+		fRows = m_EarthExtents.Height() / dxdelta * 3600.0f;
+		m_iRows = (int)fRows + 1;	// 1 more than quad spacing
+	}
+	else
+	{
+		// some linear coordinate system
+		fRows = m_EarthExtents.Height() / dydelta;
+		m_iRows = (int)(fRows + 0.5) + 1;	// round to the nearest integer
+	}
+
+	// safety check
+	if (m_iRows > 5000)
+		return false;
+
+	_AllocateArray();
+
+	// jump to start of actual data
+	fseek(fp, iDataStartOffset, 0);
+
+	for (i = 0; i < iProfiles; i++)
+	{
+		if (progress_callback != NULL)
+			progress_callback(i*100/m_iColumns);
+
+		// We cannot use IConvert here, because there *might* be a spurious LF
+		// after the number - seen in some rare files.
+		fscanf(fp, "%d", &iRow);
+		iColumn = IConvert(fp, 6);
+		//assert(iColumn == i+1);
+
+		iProfileRows = IConvert(fp, 6);
+		iProfileCols = IConvert(fp, 6);
+
+		start.x = DConvert(fp, 24);
+		start.y = DConvert(fp, 24);
+		dLocalDatumElev = DConvert(fp, 24);
+		dProfileMin = DConvert(fp, 24);
+		dProfileMax = DConvert(fp, 24);
+
+		ygap = (int)((start.y - dMinY)/dydelta);
+
+		for (j = ygap; j < (ygap + iProfileRows); j++)
+		{
+			//assert(j >=0 && j < m_iRows);	// useful safety check
+
+			// We cannot use IConvert here, because there *might* be a spurious LF
+			// after the number - seen in some rare files.
+			fscanf(fp, "%d", &iElev);
+			if (iElev == -32767 || iElev == -32768)
+				SetValue(i, j, INVALID_ELEVATION);
+			else
+				SetValue(i, j, iElev);
+		}
+	}
+	fclose(fp);
+
+	m_fVMeters = (float) (fVertUnits * dzdelta);
+	ComputeHeightExtents();
+
+	return true;
+}
+
