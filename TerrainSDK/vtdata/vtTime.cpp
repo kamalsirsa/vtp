fe5a41356d0f18966b8cf90618df04ed7b94957b
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2004-05-19 11:07:39 +0000

    yet another fix for time, this one more radical, replacing mktime()
    git-svn-id: http://vtp.googlecode.com/svn/trunk@3281 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtdata/vtTime.cpp
+++ TerrainSDK/vtdata/vtTime.cpp
@@ -8,6 +8,147 @@
 #include "vtTime.h"
 #include "vtLog.h"
 
+#define _BASE_YEAR			70		/* 1970 is the base year */
+#define _MAX_YEAR			138		/* 2038 is the max year */
+#define _LEAP_YEAR_ADJUST	17		/* Leap years 1900 - 1970 */
+static int _days[] = {
+	-1, 30, 58, 89, 119, 150, 180, 211, 242, 272, 303, 333, 364
+};
+#define ChkAdd(dest, src1, src2)   ( ((src1 >= 0L) && (src2 >= 0L) \
+    && (dest < 0L)) || ((src1 < 0L) && (src2 < 0L) && (dest >= 0L)) )
+#define ChkMul(dest, src1, src2)   ( src1 ? (dest/src1 != src2) : 0 )
+
+/*
+ * Supply a function that is missing from the ANSI C standard library:
+ *  convert a structure tm in GMT directly into a time_t.  This does the
+ *  reverse operation of gmtime().
+ */
+static time_t make_time_t(struct tm *tb)
+{
+	time_t tmptm1, tmptm2, tmptm3;
+	struct tm *tbtemp;
+
+	/*
+	* First, make sure tm_year is reasonably close to being in range.
+	*/
+	if ( ((tmptm1 = tb->tm_year) < _BASE_YEAR - 1) || (tmptm1 > _MAX_YEAR + 1) )
+		goto err_mktime;
+
+	/*
+	 * Adjust month value so it is in the range 0 - 11.  This is because
+	 * we don't know how many days are in months 12, 13, 14, etc.
+	 */
+
+	if ( (tb->tm_mon < 0) || (tb->tm_mon > 11) )
+	{
+		/*
+		 * no danger of overflow because the range check above.
+		 */
+		tmptm1 += (tb->tm_mon / 12);
+
+		if ( (tb->tm_mon %= 12) < 0 ) {
+			tb->tm_mon += 12;
+			tmptm1--;
+		}
+
+		/*
+		 * Make sure year count is still in range.
+		 */
+		if ( (tmptm1 < _BASE_YEAR - 1) || (tmptm1 > _MAX_YEAR + 1) )
+			goto err_mktime;
+	}
+
+	/***** HERE: tmptm1 holds number of elapsed years *****/
+
+	/*
+	 * Calculate days elapsed minus one, in the given year, to the given
+	 * month. Check for leap year and adjust if necessary.
+	 */
+	tmptm2 = _days[tb->tm_mon];
+	if ( !(tmptm1 & 3) && (tb->tm_mon > 1) )
+		tmptm2++;
+
+	/*
+	 * Calculate elapsed days since base date (midnight, 1/1/70, UTC)
+	 *
+	 * 365 days for each elapsed year since 1970, plus one more day for
+	 * each elapsed leap year. no danger of overflow because of the range
+	 * check (above) on tmptm1.
+	 */
+	tmptm3 = (tmptm1 - _BASE_YEAR) * 365L + ((tmptm1 - 1L) >> 2)
+	- _LEAP_YEAR_ADJUST;
+
+	/*
+	 * elapsed days to current month (still no possible overflow)
+	 */
+	tmptm3 += tmptm2;
+
+	/*
+	 * elapsed days to current date. overflow is now possible.
+	 */
+	tmptm1 = tmptm3 + (tmptm2 = (time_t)(tb->tm_mday));
+	if ( ChkAdd(tmptm1, tmptm3, tmptm2) )
+		goto err_mktime;
+
+	/***** HERE: tmptm1 holds number of elapsed days *****/
+
+	/*
+	 * Calculate elapsed hours since base date
+	 */
+	tmptm2 = tmptm1 * 24L;
+	if ( ChkMul(tmptm2, tmptm1, 24L) )
+		goto err_mktime;
+
+	tmptm1 = tmptm2 + (tmptm3 = (time_t)tb->tm_hour);
+	if ( ChkAdd(tmptm1, tmptm2, tmptm3) )
+		goto err_mktime;
+
+	/***** HERE: tmptm1 holds number of elapsed hours *****/
+
+	/*
+	 * Calculate elapsed minutes since base date
+	 */
+
+	tmptm2 = tmptm1 * 60L;
+	if ( ChkMul(tmptm2, tmptm1, 60L) )
+		goto err_mktime;
+
+	tmptm1 = tmptm2 + (tmptm3 = (time_t)tb->tm_min);
+	if ( ChkAdd(tmptm1, tmptm2, tmptm3) )
+		goto err_mktime;
+
+	/***** HERE: tmptm1 holds number of elapsed minutes *****/
+
+	/*
+	 * Calculate elapsed seconds since base date
+	 */
+
+	tmptm2 = tmptm1 * 60L;
+	if ( ChkMul(tmptm2, tmptm1, 60L) )
+		goto err_mktime;
+
+	tmptm1 = tmptm2 + (tmptm3 = (time_t)tb->tm_sec);
+	if ( ChkAdd(tmptm1, tmptm2, tmptm3) )
+		goto err_mktime;
+
+	/***** HERE: tmptm1 holds number of elapsed seconds *****/
+
+	if ( (tbtemp = gmtime(&tmptm1)) == NULL )
+		goto err_mktime;
+
+	/***** HERE: tmptm1 holds number of elapsed seconds, adjusted *****/
+	/*****       for local time if requested                      *****/
+
+	*tb = *tbtemp;
+	return (time_t)tmptm1;
+
+err_mktime:
+	/*
+	* All errors come to here
+	*/
+	return (time_t)(-1);
+}
+
 ///////////////////////////////////////////////////////
 
 //time_t vtTime::s_DifferenceFromGMT = (time_t) -1;
@@ -41,7 +182,7 @@ vtTime::vtTime()
 
 void vtTime::_UpdateTM()
 {
-	struct tm *t = localtime(&m_time);
+	struct tm *t = gmtime(&m_time);
 	m_tm = *t;
 }
 
@@ -59,7 +200,7 @@ bool vtTime::SetFromString(const vtString &str)
 	if (num != 6)
 		return false;
 
-	m_time = mktime(&m_tm);
+	m_time = make_time_t(&m_tm);
 	if (m_time == -1)
 		return false;
 
@@ -102,7 +243,7 @@ void vtTime::SetDate(int year, int month, int day)
 	m_tm.tm_mon = month-1;
 
 	m_tm.tm_mday = day;
-	m_time = mktime(&m_tm);
+	m_time = make_time_t(&m_tm);
 //	m_time += s_DifferenceFromGMT;
 	_UpdateTM();
 }
@@ -122,7 +263,7 @@ void vtTime::SetTimeOfDay(int hr, int min, int sec)
 	m_tm.tm_hour = hr;
 	m_tm.tm_min = min;
 	m_tm.tm_sec = sec;
-	m_time = mktime(&m_tm);
+	m_time = make_time_t(&m_tm);
 
 //	m_time += s_DifferenceFromGMT;
 	_UpdateTM();
