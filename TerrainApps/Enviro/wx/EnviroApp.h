0e4b8b70db1118c49e0eed97b92cb0f340c7a86c
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2004-12-17 04:27:45 +0000

    renamed from app.cpp / app.h
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4011 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/Enviro/wx/EnviroApp.h
@@ -0,0 +1,52 @@
+//
+// Name: EnviroApp.h
+//
+// Copyright (c) 2001-2004 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+// Define a new application type
+class EnviroApp: public wxApp
+{
+public:
+	EnviroApp();
+
+	bool OnInit();
+	int OnExit();
+	void Args(int argc, wxChar **argv);
+
+	void RefreshTerrainList();
+	bool AskForTerrainName(wxWindow *pParent, wxString &strTerrainName);
+	vtString GetIniFileForTerrain(const vtString &name);
+
+	bool m_bShowStartupDialog;
+
+	vtStringArray terrain_files;
+	vtStringArray terrain_paths;
+	vtStringArray terrain_names;
+
+protected:
+	void SetupLocale();
+
+	wxLocale m_locale; // locale we'll be using
+	vtString m_locale_name;
+};
+
+// helpers
+int EditTerrainParameters(wxWindow *parent, const char *filename);
+
+#ifndef STRING_ORGNAME
+#define STRING_ORGNAME "VTP"
+#endif
+
+#ifndef STRING_APPNAME
+#define STRING_APPNAME "Enviro"
+#define ENVIRO_NATIVE
+#endif
+
+#ifndef ICON_NAME
+#define ICON_NAME "enviro"
+#endif
+
+#define WSTRING_APPORG _T(STRING_ORGNAME) _T(" ") _T(STRING_APPNAME)
+
