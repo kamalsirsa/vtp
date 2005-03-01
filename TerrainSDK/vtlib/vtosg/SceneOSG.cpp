ef17ff969b0057ad1e3c4eb506b281297878254a
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2005-03-01 10:40:22 +0000

    added log on bad winsize
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4240 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtlib/vtosg/SceneOSG.cpp
+++ TerrainSDK/vtlib/vtosg/SceneOSG.cpp
@@ -142,6 +142,8 @@ void vtScene::UpdateWindow(vtWindow *pWindow)
 
 	// window size
 	IPoint2 winsize = pWindow->GetSize();
+	if (winsize.x == 0 || winsize.y == 0)
+		VTLOG("Warning: winsize %d %d\n", winsize.x, winsize.y);
 	m_pOsgSceneView->setViewport(0, 0, winsize.x, winsize.y);
 
 	// As of OSG 0.9.5, we need to store our own camera params and recreate
