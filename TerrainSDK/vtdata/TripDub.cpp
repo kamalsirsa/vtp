5dd48cd33794d6e4e02f8307d8b05a015a4a7523
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2005-03-05 09:17:18 +0000

    added catch for code 204 "no data"
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4263 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtdata/TripDub.cpp
+++ TerrainSDK/vtdata/TripDub.cpp
@@ -289,6 +289,12 @@ bool ReqContext::DoQuery(vtBytes &data, int redirects)
 			VTLOG("  Timeout: more than %d seconds\n", DEFAULT_TIMEOUT);
 		return false;
 	}
+	if (s_last_status == HT_NO_DATA)	// "OK but no data was loaded"
+	{
+		if (m_iVerbosity > 0)
+			VTLOG("  OK but no data was loaded\n");
+		return false;
+	}
 
 	char *dataptr = HTChunk_data(chunk);
 	int len = HTChunk_size(chunk);
