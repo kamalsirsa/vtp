571fcb1a50e93335b08b4868ee3f7f87f81bce11
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2005-03-12 03:12:05 +0000

    doc improvement
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4304 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtlib/core/CoreScene.cpp
+++ TerrainSDK/vtlib/core/CoreScene.cpp
@@ -1,7 +1,7 @@
 //
 // CoreScene.cpp
 //
-// Copyright (c) 2001 Virtual Terrain Project
+// Copyright (c) 2001-2005 Virtual Terrain Project
 // Free for all uses, see license.txt for details.
 //
 
@@ -9,12 +9,12 @@
 \mainpage vtlib library documentation
 
 \section overview Overview
-The <b>vtlib</b> library is built on top of the <b>vtdata</b> library which
+The <b>vtlib</b> library is built on top of the <a href="../vtdata.html">vtdata</a> library which
 handles many kinds of geospatial data.  It extends <b>vtdata</b> with the
 ability to create 3D geometry of the data for interactive visualization.
 
 It is part of the <a href="http://vterrain.org/">Virtual Terrain Project</a>
-and distributed under a completely free <a href="../../license.txt">open
+and distributed under a completely free <a href="../license.txt">open
 source license</a>.
 
 <b>vtlib</b> contains an abstraction of a Scene Graph API, built on top of
@@ -23,14 +23,8 @@ Because it is higher-level, <b>vtlib</b> lets you construct 3D geometry
 and simulations much faster and easier than using the lower-level libraries
 directly.
 
-Some documentation for vtlib, and important discussion of how to use the
-library, is <a href="http://vterrain.org/Implementation/Libs/vtlib.html">
-online</a>.
-
-This folder contains documentation of the classes and methods - see the
-<a href="hierarchy.html">Class Hierarchy</a> or <a href="inherits.html">
-graphical class hierarchy</a>.
-
+In addition to these automatically generated pages, programmer documentation
+is also <a href="../vtlib.html">available online</a>.
 */
 
 #include "vtlib/vtlib.h"
