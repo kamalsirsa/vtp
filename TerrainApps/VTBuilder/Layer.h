fa9939e12ef60bdd3e0114d62b8f8e91d3b75d45
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2005-03-27 18:10:35 +0000

    chg JPEG file filter to default to .jpg first
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4327 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/VTBuilder/Layer.h
+++ TerrainApps/VTBuilder/Layer.h
@@ -37,7 +37,7 @@
 #define FSTRING_HTML	_T("HTML Files (*.html)|*.html")
 #define FSTRING_IGC		_T("IGC Files (*.igc)|*.igc")
 #define FSTRING_IMG		_T("IMG Files (*.img)|*.img")
-#define FSTRING_JPEG	_T("JPEG Files (*.jpeg, *.jpg)|*.jpeg;*.jpg")
+#define FSTRING_JPEG	_T("JPEG Files (*.jpg, *.jpeg)|*.jpg;*.jpeg")
 #define FSTRING_LULC	_T("LULC Files (*.gir)|*.gir")
 #define FSTRING_MEM		_T("Japanese DEM Files (*.mem)|*.mem")
 #define FSTRING_MI		_T("MapInfo Files (*.tab, *.mif)|*.tab;*.mif")
