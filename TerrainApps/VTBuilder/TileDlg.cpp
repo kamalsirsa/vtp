3d2c062e6b9783874348821681d0f1deb6bac34c
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2005-09-27 09:08:45 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4604 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/VTBuilder/TileDlg.cpp
@@ -0,0 +1,137 @@
+//
+// Name: TileDlg.cpp
+//
+// Copyright (c) 2005 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#include "TileDlg.h"
+
+// WDR: class implementations
+
+//----------------------------------------------------------------------------
+// TileDlg
+//----------------------------------------------------------------------------
+
+// WDR: event table for TileDlg
+
+BEGIN_EVENT_TABLE(TileDlg,AutoDialog)
+	EVT_BUTTON( ID_DOTDOTDOT, TileDlg::OnDotDotDot )
+	EVT_TEXT( ID_COLUMNS, TileDlg::OnSize )
+	EVT_TEXT( ID_ROWS, TileDlg::OnSize )
+	EVT_CHOICE( ID_CHOICE_LOD0_SIZE, TileDlg::OnLODSize )
+END_EVENT_TABLE()
+
+TileDlg::TileDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+	const wxPoint &position, const wxSize& size, long style ) :
+	AutoDialog( parent, id, title, position, size, style )
+{
+	// WDR: dialog function TileDialogFunc for TileDlg
+	TileDialogFunc( this, TRUE );
+
+	m_fEstX = -1;
+	m_fEstY = -1;
+
+	m_bSetting = false;
+
+	AddValidator(ID_TEXT_TO_FOLDER, &m_strToFolder);
+	AddNumValidator(ID_COLUMNS, &m_iColumns);
+	AddNumValidator(ID_ROWS, &m_iRows);
+	AddValidator(ID_CHOICE_LOD0_SIZE, &m_iLODChoice);
+	AddValidator(ID_SPIN_NUM_LODS, &m_iNumLODs);
+
+	// informations
+	AddNumValidator(ID_TOTALX, &m_iTotalX);
+	AddNumValidator(ID_TOTALY, &m_iTotalY);
+
+	AddNumValidator(ID_AREAX, &m_fAreaX);
+	AddNumValidator(ID_AREAY, &m_fAreaY);
+
+	AddNumValidator(ID_ESTX, &m_fEstX);
+	AddNumValidator(ID_ESTY, &m_fEstY);
+
+	AddNumValidator(ID_CURX, &m_fCurX);
+	AddNumValidator(ID_CURY, &m_fCurY);
+}
+
+void TileDlg::SetTilingOptions(TilingOptions &opt)
+{
+	m_iColumns = opt.cols;
+	m_iRows = opt.rows;
+	m_iLOD0Size = opt.lod0size;
+	m_iNumLODs = opt.numlods;
+	m_strToFolder = opt.dir;
+
+	m_iLODChoice = vt_log2(m_iLOD0Size)-6;
+
+	UpdateInfo();
+}
+
+void TileDlg::GetTilingOptions(TilingOptions &opt) const
+{
+	opt.cols = m_iColumns;
+	opt.rows = m_iRows;
+	opt.lod0size = m_iLOD0Size;
+	opt.numlods = m_iNumLODs;
+	opt.dir = m_strToFolder.mb_str();
+}
+
+void TileDlg::SetArea(const DRECT &area)
+{
+	m_area = area;
+
+	UpdateInfo();
+	TransferDataToWindow();
+}
+
+void TileDlg::UpdateInfo()
+{
+	m_iTotalX = m_iLOD0Size * m_iColumns + 1;
+	m_iTotalY = m_iLOD0Size * m_iRows + 1;
+
+	m_fAreaX = m_area.Width();
+	m_fAreaY = m_area.Height();
+
+	m_fCurX = m_fAreaX / (m_iTotalX - 1);
+	m_fCurY = m_fAreaY / (m_iTotalY - 1);
+
+	m_bSetting = true;
+	TransferDataToWindow();
+	m_bSetting = false;
+}
+
+// WDR: handler implementations for TileDlg
+
+void TileDlg::OnLODSize( wxCommandEvent &event )
+{
+	if (m_bSetting)
+		return;
+
+	TransferDataFromWindow();
+	m_iLOD0Size = 1 << (m_iLODChoice + 6);
+	UpdateInfo();
+}
+
+void TileDlg::OnSize( wxCommandEvent &event )
+{
+	if (m_bSetting)
+		return;
+
+	TransferDataFromWindow();
+	UpdateInfo();
+}
+
+void TileDlg::OnDotDotDot( wxCommandEvent &event )
+{
+	// ask the user for a directory
+	wxDirDialog getDir(NULL, _("Write Tiles to Directory"));
+	bool bResult = (getDir.ShowModal() == wxID_OK);
+	if (!bResult)
+		return;
+	m_strToFolder = getDir.GetPath();
+	TransferDataToWindow();
+}
+
---
+++ TerrainApps/VTBuilder/TileDlg.h
@@ -0,0 +1,72 @@
+//
+// Name: TileDlg.h
+//
+// Copyright (c) 2005 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#ifndef __TileDlg_H__
+#define __TileDlg_H__
+
+#include "VTBuilder_wdr.h"
+#include "vtdata/ElevationGrid.h"
+#include "vtui/AutoDialog.h"
+#include "vtui/wxString2.h"
+
+// WDR: class declarations
+
+//----------------------------------------------------------------------------
+// TileDlg
+//----------------------------------------------------------------------------
+
+class TileDlg: public AutoDialog
+{
+public:
+	// constructors and destructors
+	TileDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxDEFAULT_DIALOG_STYLE );
+	
+	// WDR: method declarations for TileDlg
+	wxChoice* GetChoiceLod0Size()  { return (wxChoice*) FindWindow( ID_CHOICE_LOD0_SIZE ); }
+
+	void SetTilingOptions(TilingOptions &opt);
+	void GetTilingOptions(TilingOptions &opt) const;
+	void SetArea(const DRECT &area);
+	void UpdateInfo();
+
+	double m_fEstX;
+	double m_fEstY;
+
+private:
+	// WDR: member variable declarations for TileDlg
+	wxString2 m_strToFolder;
+	int m_iColumns;
+	int m_iRows;
+	int m_iLODChoice;
+	int m_iLOD0Size;
+	int m_iNumLODs;
+
+	int m_iTotalX;
+	int m_iTotalY;
+	double m_fAreaX;
+	double m_fAreaY;
+	double m_fCurX;
+	double m_fCurY;
+
+	DRECT m_area;
+	bool m_bSetting;
+
+private:
+	// WDR: handler declarations for TileDlg
+	void OnLODSize( wxCommandEvent &event );
+	void OnSize( wxCommandEvent &event );
+	void OnDotDotDot( wxCommandEvent &event );
+
+private:
+	DECLARE_EVENT_TABLE()
+};
+
+#endif	// __TileDlg_H__
+
