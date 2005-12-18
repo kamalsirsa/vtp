537e77e04eed0fdbdb051beda79fbe165c4187a4
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2005-12-18 07:15:54 +0000

    chg from using libwww to libcurl
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4883 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtdata/TripDub.cpp
+++ TerrainSDK/vtdata/TripDub.cpp
@@ -4,60 +4,28 @@
 // Module for Double-You-Double-You-Double-You support (WWW)
 // Ref: http://web.yost.com/Misc/nix-on-www.html
 //
-// Copyright (c) 2002-2003 Virtual Terrain Project
+// Copyright (c) 2002-2005 Virtual Terrain Project
 // Free for all uses, see license.txt for details.
 //
 
 #include "config_vtdata.h"
 
-// The dependency on Libwww is optional.  If not desired, skip this file.
+// The dependency on LibCURL is optional.  If not desired, skip this file.
 #if SUPPORT_HTTP
 
 #include "TripDub.h"
 #include "vtdata/vtLog.h"
 
-//
-// Includes for libwww (used for HTTP requests)
-//
+#if 0
 
-// first, avoid preprocessor conflicts between GDAL(cpl) and WWWLib
-#undef HAVE_VPRINTF
-#undef STDC_HEADERS
-// avoid preprocessor conflict between MSVC errno.h and WWWlib
-#undef EINVAL
-
-#ifdef UNIX
-#include "w3c-libwww/WWWLib.h"
-#include "w3c-libwww/WWWInit.h"
-#else
-#include "WWWLib.h"
-#include "WWWInit.h"
-#endif
-
-#ifdef _MSC_VER
-// We will now need to link with several of the Libwww libraries.  This can
-// be done automatically with MSVC as follows:
-#pragma comment(lib, "wwwapp.lib")
-#pragma comment(lib, "wwwcore.lib")
-#pragma comment(lib, "wwwinit.lib")
-#pragma comment(lib, "wwwutils.lib")
-#pragma comment(lib, "wwwhttp.lib")
-#endif
-
-#define DEFAULT_TIMEOUT	90		// timeout in secs
-#define MILLIES			1000
-
-#ifndef DOXYGEN_SHOULD_SKIP_THIS
-struct MyCookie
+// Old cookie stuff, here just fstruct MyCookie
 {
 	vtString name;
 	vtString value;
 };
-#endif
 
 std::vector<MyCookie> g_cookies;
 static int s_last_status = 0;
-static HTAnchor *new_anchor = NULL;
 
 void AddCookie(const char *name, const char *value)
 {
@@ -78,264 +46,114 @@ void AddCookie(const char *name, const char *value)
 }
 
 
-PRIVATE int printer (const char * fmt, va_list pArgs)
+void ReqContext::AddHeader(const char *token, const char *value)
 {
-	char buf[20000];
-	int ret = vsprintf(buf, fmt, pArgs);
-	VTLOG1(buf);
-	return ret;
 }
 
-PRIVATE int tracer (const char * fmt, va_list pArgs)
+bool ReqContext::DoQuery(vtBytes &data, int redirects)
 {
-	char buf[20000];
-	int ret = vsprintf(buf, fmt, pArgs);
-	VTLOG1(buf);
-	return ret;
 }
 
-PRIVATE int term_handler (HTRequest * request, HTResponse * response,
-				   void * param, int status)
+#endif	// Old cookie stuff
+
+/////////////////////////////////////////////////////////////////////////////
+// LibCurl begins here
+
+#include "curl/curl.h"
+
+#ifdef _MSC_VER
+// We will now need to link with the LibCurl library.  This can
+// be done automatically with MSVC as follows:
+#pragma comment(lib, "libcurl.lib")
+#endif
+
+size_t write_as_string( void *ptr, size_t size, size_t nmemb, void *stream)
 {
-	ReqContext *context = (ReqContext *) HTRequest_context(request);
+	ReqContext *context = (ReqContext *)stream;
+	size_t length = size * nmemb;
 
-	// Check for status
-	if (context->m_iVerbosity > 0)
-		VTLOG("status %d\n", status);
-
-	s_last_status = status;
-	new_anchor = HTResponse_redirection(response);
-
-	// we're not handling other requests
-	HTEventList_stopLoop ();
-
-	// stop here
-	return HT_ERROR;
+	context->m_pDataString->Concat((pcchar) ptr, length);
+	return length;
 }
 
-
-PRIVATE BOOL setCookie(HTRequest * request, HTCookie * cookie, void * param)
+size_t write_as_bytes( void *ptr, size_t size, size_t nmemb, void *stream)
 {
-	ReqContext *context = (ReqContext *) HTRequest_context(request);
+	ReqContext *context = (ReqContext *)stream;
+	size_t length = size * nmemb;
 
-	if (!cookie)
-		return YES;
-
-	if (context->m_iVerbosity > 1)
-	{
-		char * addr = HTAnchor_address((HTAnchor *) HTRequest_anchor(request));
-		VTLOG("While accessing `%s\', we received a cookie with parameters:\n", addr);
-		if (HTCookie_name(cookie))
-			VTLOG("\tName   : `%s\'\n", HTCookie_name(cookie));
-		if (HTCookie_value(cookie))
-			VTLOG("\tValue  : `%s\'\n", HTCookie_value(cookie));
-		if (HTCookie_domain(cookie))
-			VTLOG("\tDomain : `%s\'\n", HTCookie_domain(cookie));
-		if (HTCookie_path(cookie))
-			VTLOG("\tPath   : `%s\'\n", HTCookie_path(cookie));
-		if (HTCookie_expiration(cookie) > 0) {
-			time_t t = HTCookie_expiration(cookie);
-			VTLOG("\tExpires: `%s\'\n", HTDateTimeStr(&t, NO));
-		}
-		VTLOG("\tCookie is %ssecure\n\n", HTCookie_isSecure(cookie) ? "" : "not ");
-		HT_FREE(addr);
-	}
-
-	// Work around bug in libwww cookie parsing: when it gets a line like this:
-	//		Set-Cookie: foo=;Expires=Thu. 01-Jan-1970...
-	// rather than give a blank string for foo, it gives "Expires"
-	if (!strcmp(HTCookie_value(cookie), "Expires"))
-		return YES;
-
-	// add it to our own list
-	AddCookie(HTCookie_name(cookie), HTCookie_value(cookie));
-
-	return YES;
+	context->m_pDataBytes->Append((unsigned char *) ptr, length);
+	return length;
 }
-
-PRIVATE HTAssocList *findCookie(HTRequest * request, void * param)
-{
-	ReqContext *context = (ReqContext *) HTRequest_context(request);
-
-	if (context->m_iVerbosity > 1)
-		VTLOG(" findCookie %s:\n", param);
-
-	HTAssocList * alist = HTAssocList_new(); // is deleted by the cookie module
-	unsigned int i;
-	for (i = 0; i < g_cookies.size(); i++)
-	{
-		HTAssocList_addObject(alist, g_cookies[i].name, g_cookies[i].value);
-		if (context->m_iVerbosity > 1)
-			VTLOG("   adding (%d) %s=%s\n", i,
-				(const char *)g_cookies[i].name,
-				(const char *)g_cookies[i].value);
-	}
-	return alist;
-}
-
-
-//---------------------------------------------------------------------------
-// ReqContext class
-//
 
 bool ReqContext::s_bFirst = true;
 
 ReqContext::ReqContext()
 {
-	m_iVerbosity = 1;
-
 	if (s_bFirst)
 		InitializeLibrary();
 
-	m_cwd = HTGetCurrentDirectoryURL();
+	m_curl = curl_easy_init(); 
 
-	// Bind the ConLine object together with the Request Object
-	m_request = HTRequest_new();
-	HTRequest_setOutputFormat(m_request, WWW_SOURCE);
+	CURLcode result;
+	result = curl_easy_setopt(m_curl, CURLOPT_WRITEDATA, this);
 
-	// Setting preemptive to NO doesn't seem to make a difference
-	HTRequest_setPreemptive(m_request, YES);
-
-	// Set a backpointer so we can find ourselves later
-	HTRequest_setContext(m_request, this);
+	// This tells CURL to give us an error if we get an e.g. 404, instead
+	//  of giving us the HTML page from the server, e.g. "The page cannot
+	//  be found..."
+	result = curl_easy_setopt(m_curl, CURLOPT_FAILONERROR, 1);
 }
 
 ReqContext::~ReqContext()
 {
-	HTRequest_delete(m_request);
-	HT_FREE(m_cwd);
-}
-
-
-void ReqContext::InitializeLibrary()
-{
-	// This one call does most of the work
-	HTProfile_newPreemptiveRobot("vtdata", __DATE__);
-//	HTProfile_newNoCacheClient("vtdata", __DATE__);
-
-	// Need our own trace and print functions
-	HTPrint_setCallback(printer);
-	HTTrace_setCallback(tracer);
-
-	HTAlertInit();
-	HTAlert_setInteractive(NO);
-
-#ifdef WIN32
-	// This is important!  Things don't work right on Windows without it.
-	HTEventInit();
-#endif
-
-	// Add our own filter to handle termination
-	HTNet_addAfter(term_handler, NULL, NULL, HT_ALL, HT_FILTER_LAST);
-
-	// Setup cookies
-	HTCookie_init();
-	HTCookie_setCallbacks(setCookie, NULL, findCookie, NULL);
-
-	// Don't prompt for cookies - just accept them!
-	//	HTCookieMode mode = HTCookie_cookieMode();
-	HTCookie_setCookieMode((HTCookieMode) (HT_COOKIE_ACCEPT | HT_COOKIE_SEND));
-
-	// Set event timeout
-	int timer = DEFAULT_TIMEOUT*MILLIES;
-	HTHost_setEventTimeout(timer);
-
-	// Don't know what this will do, just trying:
-	HTHost_setActiveTimeout(timer);
-
-	s_bFirst = false;
-}
-
-void ReqContext::AddHeader(const char *token, const char *value)
-{
-	HTRequest_addExtraHeader(m_request, (char *) token, (char *) value);
-}
-
-bool ReqContext::DoQuery(vtBytes &data, int redirects)
-{
-	const char *address;
-
-	address = HTAnchor_address(m_anchor);
-	if (m_iVerbosity > 0)
-		VTLOG("  Opening: '%s' ... ", address);
-
-	HTChunk *chunk = HTLoadAnchorToChunk(m_anchor, m_request);
-
-	// chunk had better not be NULL, that's where the data will go
-	if (!chunk)
-		return false;
-
-	// wait until the request is over
-	HTEventList_loop(m_request);
-
-	// check status - redirect?
-	if (s_last_status == HT_FOUND)	// also known as 302 Redirect
-	{
-		address = HTAnchor_address(new_anchor);
-		if (m_iVerbosity == 2)
-			VTLOG("  Redirected To: %s\n", address);
-		if (m_iVerbosity == 1)
-			VTLOG("  (Redirected)");
-
-		m_anchor = new_anchor;
-
-		// Delete any auth credendials as they get regenerated
-		HTRequest_deleteCredentialsAll(m_request);
-		HTRequest_deleteExtraHeaderAll(m_request);
-
-		return DoQuery(data, redirects + 1);
-	}
-	if (s_last_status == HT_TIMEOUT)
-	{
-		// too long, give up
-		if (m_iVerbosity > 0)
-			VTLOG("  Timeout: more than %d seconds\n", DEFAULT_TIMEOUT);
-	}
-	if (s_last_status == HT_NO_DATA)	// "OK but no data was loaded"
-	{
-		if (m_iVerbosity > 0)
-			VTLOG("  OK but no data was loaded\n");
-	}
-	if (s_last_status < 0)	// all values less than zero are errors
-		return false;
-
-	char *dataptr = HTChunk_data(chunk);
-	int len = HTChunk_size(chunk);
-
-	data.Put((unsigned char *)dataptr, len);
-	return true;
+	curl_easy_cleanup(m_curl); 
 }
 
 bool ReqContext::GetURL(const char *url, vtString &str)
 {
-	char *absolute_url = HTParse(url, m_cwd, PARSE_ALL);
-	if (!absolute_url)
-		return false;
+	m_pDataString = &str;
 
-	m_anchor = HTAnchor_findAddress(absolute_url);
-	HT_FREE(absolute_url);
+	CURLcode result;
+	char errorbuf[CURL_ERROR_SIZE];
+	result = curl_easy_setopt(m_curl, CURLOPT_URL, url);
+	result = curl_easy_setopt(m_curl, CURLOPT_WRITEFUNCTION, write_as_string);
+	result = curl_easy_setopt(m_curl, CURLOPT_ERRORBUFFER, errorbuf);
+	result = curl_easy_perform(m_curl);
 
-	vtBytes bytes;
-	if (DoQuery(bytes, 0))
-	{
-		str = bytes.Get();
+	if (result == 0)	// 0 means everything was ok
 		return true;
-	}
 	else
+	{
+		str = errorbuf;
 		return false;
+	}
 }
 
 bool ReqContext::GetURL(const char *url, vtBytes &data)
 {
-	char *absolute_url = HTParse(url, m_cwd, PARSE_ALL);
-	if (!absolute_url)
-		return false;
+	m_pDataBytes = &data;
 
-	m_anchor = HTAnchor_findAddress(absolute_url);
-	HT_FREE(absolute_url);
+	CURLcode result;
+	result = curl_easy_setopt(m_curl, CURLOPT_URL, url);
+	result = curl_easy_setopt(m_curl, CURLOPT_WRITEFUNCTION, write_as_bytes);
+	result = curl_easy_perform(m_curl);
 
-	return DoQuery(data, 0);
+	return (result == 0);	// 0 means everything was ok
+}
+
+bool ReqContext::DoQuery(vtBytes &data, int redirects)
+{
+	return false;
+}
+
+void ReqContext::InitializeLibrary()
+{
+	s_bFirst = false;
+	long flags = CURL_GLOBAL_ALL;	// Initialize everything possible. This sets all known bits. 
+	CURLcode result = curl_global_init(flags); 
+	if (result != 0)
+	{
+		// error
+	}
 }
 
 #endif	// SUPPORT_HTTP
-
--- TerrainSDK/vtdata/TripDub.h
+++ TerrainSDK/vtdata/TripDub.h
@@ -12,25 +12,29 @@
 
 #include "vtString.h"
 
-// The dependency on Libwww is optional.  If not desired, skip this file.
+// The dependency on LibCURL is optional.  If not desired, skip this file.
 #if SUPPORT_HTTP
-
-typedef struct _HTRequest HTRequest;
-typedef struct _HTAnchor	HTAnchor;
 
 class vtBytes
 {
 public:
-	vtBytes() { m_data = NULL; }
-	~vtBytes() { delete m_data; }
+	vtBytes() { m_data = NULL; m_len = 0; }
+	~vtBytes() { if (m_data) free(m_data); }
 
-	void Put(unsigned char *data, size_t len)
+	void Set(unsigned char *data, size_t len)
 	{
-		if (m_data)
-			delete [] m_data;
-		m_data = new unsigned char[len];
+		m_data = (unsigned char *) malloc(len);
 		memcpy(m_data, data, len);
 		m_len = len;
+	}
+	void Append(unsigned char *data, size_t len)
+	{
+		if (m_data)
+			m_data = (unsigned char *) realloc(m_data, m_len + len);
+		else
+			m_data = (unsigned char *) malloc(len);
+		memcpy(m_data + m_len, data, len);
+		m_len += len;
 	}
 	unsigned char *Get() { return m_data; }
 	size_t Len() { return m_len; }
@@ -40,13 +44,15 @@ protected:
 	size_t m_len;
 };
 
+
+typedef void CURL;
+
 class ReqContext
 {
 public:
 	ReqContext();
 	~ReqContext();
 
-	void AddHeader(const char *token, const char *value);
 	bool GetURL(const char *url, vtString &str);
 	bool GetURL(const char *url, vtBytes &data);
 	bool DoQuery(vtBytes &data, int redirects = 0);
@@ -54,9 +60,9 @@ public:
 	/// Set level of logging output: 0 (none) 1 (some) 2 (lots)
 	void SetVerbosity(int i) { m_iVerbosity = i; }
 
-	HTRequest *	m_request;
-	HTAnchor *	m_anchor;
-	char *		m_cwd;				  /* Current dir URL */
+	CURL *m_curl;
+	vtString *m_pDataString;
+	vtBytes *m_pDataBytes;
 
 	static bool s_bFirst;
 	int m_iVerbosity;
