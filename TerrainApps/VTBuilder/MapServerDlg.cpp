fece0d45540222fc6f87b2a42e8f34be6b8ce196
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2005-12-19 06:57:05 +0000

    more implementation work, mostly done now
    git-svn-id: http://vtp.googlecode.com/svn/trunk@4906 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/VTBuilder/MapServerDlg.cpp
+++ TerrainApps/VTBuilder/MapServerDlg.cpp
@@ -16,7 +16,9 @@
 #if SUPPORT_HTTP
 #include "vtdata/WFSClient.h"
 #endif
-#include "vtui/Helper.h"    // for progress dialog
+#include "vtui/Helper.h"	// for progress dialog
+#include "FileFilters.h"	// for FSTRING filters
+#include "Helper.h"
 
 // WDR: class implementations
 
@@ -27,53 +29,64 @@
 // WDR: event table for MapServerDlg.cpp
 
 BEGIN_EVENT_TABLE(MapServerDlg, AutoDialog)
-    EVT_INIT_DIALOG (MapServerDlg::OnInitDialog)
-    EVT_COMBOBOX( ID_BASE_URL, MapServerDlg::OnServer )
-    EVT_TEXT( ID_BASE_URL, MapServerDlg::OnBaseUrlText )
-    EVT_TEXT( ID_WIDTH, MapServerDlg::OnSize )
-    EVT_TEXT( ID_HEIGHT, MapServerDlg::OnSize )
-    EVT_CHOICE( ID_CHOICE_FORMAT, MapServerDlg::OnFormat )
-    EVT_LISTBOX( ID_LIST_LAYERS, MapServerDlg::OnLayer )
-    EVT_BUTTON( ID_QUERY_LAYERS, MapServerDlg::OnQueryLayers )
-    EVT_RADIOBUTTON( ID_RADIO_CREATE_NEW, MapServerDlg::OnRadio )
-    EVT_RADIOBUTTON( ID_RADIO_TO_FILE, MapServerDlg::OnRadio )
-    EVT_BUTTON( ID_DOTDOTDOT, MapServerDlg::OnDotDotDot )
-    EVT_TEXT( ID_TEXT_TO_FILE, MapServerDlg::OnTextToFile )
+	EVT_INIT_DIALOG (MapServerDlg::OnInitDialog)
+	EVT_COMBOBOX( ID_BASE_URL, MapServerDlg::OnServer )
+	EVT_TEXT( ID_BASE_URL, MapServerDlg::OnBaseUrlText )
+	EVT_TEXT( ID_WIDTH, MapServerDlg::OnSize )
+	EVT_TEXT( ID_HEIGHT, MapServerDlg::OnSize )
+	EVT_CHOICE( ID_CHOICE_FORMAT, MapServerDlg::OnFormat )
+	EVT_LISTBOX( ID_LIST_LAYERS, MapServerDlg::OnLayer )
+	EVT_BUTTON( ID_QUERY_LAYERS, MapServerDlg::OnQueryLayers )
+	EVT_RADIOBUTTON( ID_RADIO_CREATE_NEW, MapServerDlg::OnRadio )
+	EVT_RADIOBUTTON( ID_RADIO_TO_FILE, MapServerDlg::OnRadio )
+	EVT_BUTTON( ID_DOTDOTDOT, MapServerDlg::OnDotDotDot )
+	EVT_TEXT( ID_TEXT_TO_FILE, MapServerDlg::OnTextToFile )
 END_EVENT_TABLE()
 
 MapServerDlg::MapServerDlg( wxWindow *parent, wxWindowID id, const wxString &title,
-    const wxPoint &position, const wxSize& size, long style ) :
-    AutoDialog( parent, id, title, position, size, style )
+	const wxPoint &position, const wxSize& size, long style ) :
+	AutoDialog( parent, id, title, position, size, style )
 {
-    m_bSetting = false;
-    m_iXSize = 1024;
-    m_iYSize = 1024;
-    m_iServer = -1;
-    m_iLayer = -1;
-    m_iStyle = -1;
-    m_iFormat = 0;
+	m_bSetting = false;
+	m_iXSize = 1024;
+	m_iYSize = 1024;
+	m_iServer = -1;
+	m_iLayer = -1;
+	m_iStyle = -1;
+	m_iFormat = 0;
 	m_bNewLayer = true;
 	m_bToFile = false;
 
-    MapServerDialogFunc( this, TRUE );
+	MapServerDialogFunc( this, TRUE );
 
-    AddValidator(ID_RADIO_CREATE_NEW, &m_bNewLayer);
-    AddValidator(ID_RADIO_TO_FILE, &m_bToFile);
+	AddValidator(ID_RADIO_CREATE_NEW, &m_bNewLayer);
+	AddValidator(ID_RADIO_TO_FILE, &m_bToFile);
 
-    AddNumValidator(ID_WIDTH, &m_iXSize);
-    AddNumValidator(ID_HEIGHT, &m_iYSize);
+	AddNumValidator(ID_WIDTH, &m_iXSize);
+	AddNumValidator(ID_HEIGHT, &m_iYSize);
 
-    AddValidator(ID_QUERY, &m_query);   // query url string
-    AddValidator(ID_CHOICE_FORMAT, &m_iFormat);
+	AddValidator(ID_QUERY, &m_strQueryURL);   // query url string
+	AddValidator(ID_CHOICE_FORMAT, &m_iFormat);
+	AddValidator(ID_TEXT_TO_FILE, &m_strToFile);
+
+	GetListLayers()->Clear();
+	GetListLayers()->Append(_T("<none>"));
+	GetListLayers()->SetSelection(0);
+
+	GetFormat()->Clear();
+	GetFormat()->Append(_T("JPEG"));
+	GetFormat()->Append(_T("PNG"));
+	GetFormat()->Append(_T("GeoTIFF"));
+	GetFormat()->SetSelection(0);
 }
 
 void MapServerDlg::SetServerArray(OGCServerArray &array)
 {
-    m_pServers = &array;
+	m_pServers = &array;
 
-    // default to first server
-    if (m_pServers->size() > 0)
-        m_iServer = 0;
+	// default to first server
+	if (m_pServers->size() > 0)
+		m_iServer = 0;
 }
 
 void MapServerDlg::UpdateEnabling()
@@ -87,207 +100,248 @@ void MapServerDlg::UpdateEnabling()
 
 void MapServerDlg::OnTextToFile( wxCommandEvent &event )
 {
-    TransferDataFromWindow();
+	TransferDataFromWindow();
 }
 
 void MapServerDlg::OnDotDotDot( wxCommandEvent &event )
 {
-    //...
+	wxString filter = _("All Files|*.*");
+	if (m_iFormat == 0)
+		AddType(filter, FSTRING_JPEG);
+	if (m_iFormat == 1)
+		AddType(filter, FSTRING_PNG);
+	if (m_iFormat == 2)
+		AddType(filter, FSTRING_TIF);
+
+	// ask the user for a filename
+	wxFileDialog saveFile(NULL, _("Save Imagery"), _T(""), _T(""), filter, wxSAVE);
+	saveFile.SetFilterIndex(1);
+	bool bResult = (saveFile.ShowModal() == wxID_OK);
+	if (!bResult)
+		return;
+
+	m_strToFile = saveFile.GetPath();
+
+	// update controls
+	m_bSetting = true;
+	TransferDataToWindow();
+	m_bSetting = false;
 }
 
 void MapServerDlg::OnRadio( wxCommandEvent &event )
 {
-    TransferDataFromWindow();
-    UpdateEnabling();
+	TransferDataFromWindow();
+	UpdateEnabling();
 }
 
 void MapServerDlg::OnQueryLayers( wxCommandEvent &event )
 {
 #if SUPPORT_HTTP
-    OpenProgressDialog(_("Querying server..."), false, this);
+	OpenProgressDialog(_("Querying server..."), false, this);
 
-    wxString2 val = GetBaseUrl()->GetValue();
-    vtString url = val.mb_str();
-    vtString msg;
-    bool success = GetLayersFromWMS(url, m_pServers->at(m_iServer).m_layers,
+	wxString2 val = GetBaseUrl()->GetValue();
+	vtString url = val.mb_str();
+	vtString msg;
+	bool success = GetLayersFromWMS(url, m_pServers->at(m_iServer).m_layers,
 		msg, progress_callback);
 
-    CloseProgressDialog();
+	CloseProgressDialog();
 
-    if (success)
-    {
-        UpdateLayerList();
-        UpdateLayerDescription();
-        UpdateURL();
-    }
-    else
-    {
-        wxString2 str = msg;
-        wxMessageBox(str);
-    }
+	if (success)
+	{
+		UpdateLayerList();
+		UpdateLayerDescription();
+		UpdateURL();
+	}
+	else
+	{
+		wxString2 str = msg;
+		wxMessageBox(str);
+	}
 #endif // SUPPORT_HTTP
 }
 
 void MapServerDlg::OnServer( wxCommandEvent &event )
 {
-    m_iServer = GetBaseUrl()->GetSelection();
-    UpdateLayerList();
-    UpdateLayerDescription();
-    UpdateURL();
+	m_iServer = GetBaseUrl()->GetSelection();
+	m_pServers->m_iSelected = m_iServer;
+	UpdateLayerList();
+	UpdateLayerDescription();
+	UpdateURL();
 }
 
 void MapServerDlg::OnBaseUrlText( wxCommandEvent &event )
 {
-    TransferDataFromWindow();
-    wxString2 urlvalue = GetBaseUrl()->GetValue();
-    m_pServers->at(m_iServer).m_url = urlvalue.mb_str();
-    UpdateURL();
+	TransferDataFromWindow();
+	wxString2 urlvalue = GetBaseUrl()->GetValue();
+	m_pServers->at(m_iServer).m_url = urlvalue.mb_str();
+	UpdateURL();
 }
 
 void MapServerDlg::OnLayer( wxCommandEvent &event )
 {
-    m_iLayer = GetListLayers()->GetSelection();
-    UpdateLayerDescription();
-    UpdateURL();
+	if (m_iServer == -1)
+		return;
+
+	OGCLayerArray &layers = m_pServers->at(m_iServer).m_layers;
+	int num = layers.size();
+	if (num == 0)
+		return;
+
+	m_iLayer = GetListLayers()->GetSelection();
+	layers.m_iSelected = m_iLayer;
+	UpdateLayerDescription();
+	UpdateURL();
 }
 
 void MapServerDlg::OnFormat( wxCommandEvent &event )
 {
-    if (m_bSetting)
-        return;
+	if (m_bSetting)
+		return;
 
-    TransferDataFromWindow();
-    UpdateURL();
+	TransferDataFromWindow();
+	UpdateURL();
 }
 
 void MapServerDlg::OnInitDialog(wxInitDialogEvent& event)
 {
-    GetBaseUrl()->Clear();
-    for (unsigned int i = 0; i < m_pServers->size(); i++)
-    {
-        wxString str = m_pServers->at(i).m_url;
-        GetBaseUrl()->Append(str);
-    }
-    GetBaseUrl()->SetSelection(0);
-
-    GetListLayers()->Clear();
-    GetListLayers()->Append(_T("<none>"));
-    GetListLayers()->SetSelection(0);
-
-    GetFormat()->Clear();
-    GetFormat()->Append(_T("JPEG"));
-    GetFormat()->Append(_T("PNG"));
-    GetFormat()->SetSelection(0);
+	GetBaseUrl()->Clear();
+	int numservers = m_pServers->size();
+	for (int i = 0; i < numservers; i++)
+	{
+		wxString str = m_pServers->at(i).m_url;
+		GetBaseUrl()->Append(str);
+	}
+	// If no selection, pick the first server
+	if (m_pServers->m_iSelected == -1 && numservers > 0)
+		m_pServers->m_iSelected = 0;
+	// Keep selection in range
+	if (m_pServers->m_iSelected >= numservers)
+		m_pServers->m_iSelected = numservers-1;
+	// Apply selection
+	if (m_pServers->m_iSelected != -1)
+		GetBaseUrl()->SetSelection(m_pServers->m_iSelected);
+	m_iServer = m_pServers->m_iSelected;
 
 	UpdateEnabling();
+	UpdateLayerList();
+	UpdateLayerDescription();
 
-    wxWindow::OnInitDialog(event);
+	wxWindow::OnInitDialog(event);
 }
 
 void MapServerDlg::OnSize( wxCommandEvent &event )
 {
-    if (m_bSetting)
-        return;
+	if (m_bSetting)
+		return;
 
-    TransferDataFromWindow();
-    UpdateURL();
+	TransferDataFromWindow();
+	UpdateURL();
 }
 
 void MapServerDlg::UpdateLayerList()
 {
-    GetListLayers()->Clear();
-    if (m_iServer == -1)
-        return;
+	GetListLayers()->Clear();
+	if (m_iServer == -1)
+		return;
 
-    OGCLayerArray &layers = m_pServers->at(m_iServer).m_layers;
-    int num = layers.size();
-    if (num == 0)
-    {
-        GetListLayers()->Append(_("<none>"));
-        m_iLayer = -1;
-    }
-    else
-    {
-        for (int i = 0; i < num; i++)
-        {
-            vtString str;
-            vtTagArray *tags = layers[i];
-            vtTag *tag = tags->FindTag("Name");
-            if (tag)
-                GetListLayers()->Append(wxString2(tag->value));
-        }
-        m_iLayer = 0;
-    }
-    GetListLayers()->SetSelection(0);
+	OGCLayerArray &layers = m_pServers->at(m_iServer).m_layers;
+	int num = layers.size();
+	if (num == 0)
+	{
+		GetListLayers()->Append(_("<none>"));
+		m_iLayer = -1;
+	}
+	else
+	{
+		for (int i = 0; i < num; i++)
+		{
+			vtString str;
+			vtTagArray *tags = layers[i];
+			vtTag *tag = tags->FindTag("Name");
+			if (tag)
+				GetListLayers()->Append(wxString2(tag->value));
+		}
+	}
+	// If no selection, pick the first layer
+	if (layers.m_iSelected == -1 && num > 0)
+		layers.m_iSelected = 0;
+	// Keep selection in range
+	if (layers.m_iSelected >= num)
+		layers.m_iSelected = num-1;
+	// Apply selection
+	if (layers.m_iSelected != -1)
+		GetListLayers()->SetSelection(layers.m_iSelected);
+	m_iLayer = layers.m_iSelected;
 }
 
 void MapServerDlg::UpdateLayerDescription()
 {
-    wxString2 str;
-    GetLayerDesc()->SetValue(str);
+	wxString2 str;
+	GetLayerDesc()->SetValue(str);
 
-    GetLayerDesc()->Clear();
-    if (m_iServer == -1 || m_iLayer == -1)
-        return;
+	GetLayerDesc()->Clear();
+	if (m_iServer == -1 || m_iLayer == -1)
+		return;
 
-    vtTag *tag;
-    tag = m_pServers->at(m_iServer).m_layers.at(m_iLayer)->FindTag("Title");
-    if (tag)
-    {
-        str += "Title: ";
-        str += tag->value;
-        str += "\n";
-    }
-    tag = m_pServers->at(m_iServer).m_layers.at(m_iLayer)->FindTag("Abstract");
-    if (tag)
-    {
-        str += "Abstract: ";
-        str += tag->value;
-        str += "\n";
-    }
-    GetLayerDesc()->SetValue(str);
+	vtTag *tag;
+	tag = m_pServers->at(m_iServer).m_layers.at(m_iLayer)->FindTag("Title");
+	if (tag)
+	{
+		str += "Title: ";
+		str += tag->value;
+		str += "\n";
+	}
+	tag = m_pServers->at(m_iServer).m_layers.at(m_iLayer)->FindTag("Abstract");
+	if (tag)
+	{
+		str += "Abstract: ";
+		str += tag->value;
+		str += "\n";
+	}
+	GetLayerDesc()->SetValue(str);
 }
 
 void MapServerDlg::UpdateURL()
 {
-    if (m_iServer == -1)
-        return;
+	if (m_iServer == -1)
+		return;
 
-    OGCServer &server = m_pServers->at(m_iServer);
-    vtString url = server.m_url;
-    url += "?REQUEST=GetMap";
+	OGCServer &server = m_pServers->at(m_iServer);
+	vtString url = server.m_url;
+	url += "?REQUEST=GetMap";
 
-    url += "&LAYERS=";  // required, even if left blank
-    if (m_iLayer != -1)
-    {
-        vtTagArray *layer = server.m_layers.at(m_iLayer);
-        vtString layername = layer->GetValueString("Name");
-        url += layername;
+	url += "&LAYERS=";  // required, even if left blank
+	if (m_iLayer != -1)
+	{
+		vtTagArray *layer = server.m_layers.at(m_iLayer);
+		vtString layername = layer->GetValueString("Name");
+		url += layername;
 
-        url += "&STYLES=";  // required, even if left blank
-        if (m_iStyle != -1)
-        {
-            // TODO
-        }
-    }
-    url += "&SRS=EPSG:4326";    // 4326 = WGS84
+		url += "&STYLES=";  // required, even if left blank
+		if (m_iStyle != -1)
+		{
+			// TODO
+		}
+	}
+	url += "&SRS=EPSG:4326";	// 4326 = WGS84
 
-    vtString str;
-    str.Format("&BBOX=%lf,%lf,%lf,%lf", m_area.left, m_area.bottom, m_area.right, m_area.top);
-    url += str;
+	vtString str;
+	str.Format("&BBOX=%lf,%lf,%lf,%lf", m_area.left, m_area.bottom, m_area.right, m_area.top);
+	url += str;
 
-    str.Format("&WIDTH=%d&HEIGHT=%d", m_iXSize, m_iYSize);
-    url += str;
-    if (m_iFormat == 0)
-        url += "&FORMAT=image/jpeg";
-    else
-        url += "&FORMAT=image/png";
-    url += "&TRANSPARENT=TRUE&EXCEPTIONS=WMS_XML&";
+	str.Format("&WIDTH=%d&HEIGHT=%d", m_iXSize, m_iYSize);
+	url += str;
+	if (m_iFormat == 0) url += "&FORMAT=image/jpeg";
+	if (m_iFormat == 1) url += "&FORMAT=image/png";
+	if (m_iFormat == 2) url += "&FORMAT=image/geotiff";
 
-    m_query = url;
+	url += "&TRANSPARENT=TRUE&EXCEPTIONS=WMS_XML&";
 
-    m_bSetting = true;
-    TransferDataToWindow();
-    m_bSetting = false;
+	m_strQueryURL = url;
+
+	m_bSetting = true;
+	TransferDataToWindow();
+	m_bSetting = false;
 }
 
--- TerrainApps/VTBuilder/MapServerDlg.h
+++ TerrainApps/VTBuilder/MapServerDlg.h
@@ -23,61 +23,62 @@
 class MapServerDlg: public AutoDialog
 {
 public:
-    // constructors and destructors
-    MapServerDlg(wxWindow *parent, wxWindowID id, const wxString &title,
-        const wxPoint& pos = wxDefaultPosition,
-        const wxSize& size = wxDefaultSize,
-        long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER);
-    
-    // WDR: method declarations for MapServerDlg.cpp
-    wxButton* GetDotdotdot()  { return (wxButton*) FindWindow( ID_DOTDOTDOT ); }
-    wxTextCtrl* GetTextToFile()  { return (wxTextCtrl*) FindWindow( ID_TEXT_TO_FILE ); }
-    wxTextCtrl* GetStyleDesc()  { return (wxTextCtrl*) FindWindow( ID_STYLE_DESC ); }
-    wxListBox* GetListStyles()  { return (wxListBox*) FindWindow( ID_LIST_STYLES ); }
-    wxTextCtrl* GetLayerDesc()  { return (wxTextCtrl*) FindWindow( ID_LAYER_DESC ); }
-    wxListBox* GetListLayers()  { return (wxListBox*) FindWindow( ID_LIST_LAYERS ); }
-    wxTextCtrl* GetQuery()  { return (wxTextCtrl*) FindWindow( ID_QUERY ); }
-    wxComboBox* GetBaseUrl()  { return (wxComboBox*) FindWindow( ID_BASE_URL ); }
-    wxChoice* GetFormat()  { return (wxChoice*) FindWindow( ID_CHOICE_FORMAT ); }
+	// constructors and destructors
+	MapServerDlg(wxWindow *parent, wxWindowID id, const wxString &title,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER);
+	
+	// WDR: method declarations for MapServerDlg.cpp
+	wxButton* GetDotdotdot()  { return (wxButton*) FindWindow( ID_DOTDOTDOT ); }
+	wxTextCtrl* GetTextToFile()  { return (wxTextCtrl*) FindWindow( ID_TEXT_TO_FILE ); }
+	wxTextCtrl* GetStyleDesc()  { return (wxTextCtrl*) FindWindow( ID_STYLE_DESC ); }
+	wxListBox* GetListStyles()  { return (wxListBox*) FindWindow( ID_LIST_STYLES ); }
+	wxTextCtrl* GetLayerDesc()  { return (wxTextCtrl*) FindWindow( ID_LAYER_DESC ); }
+	wxListBox* GetListLayers()  { return (wxListBox*) FindWindow( ID_LIST_LAYERS ); }
+	wxTextCtrl* GetQuery()  { return (wxTextCtrl*) FindWindow( ID_QUERY ); }
+	wxComboBox* GetBaseUrl()  { return (wxComboBox*) FindWindow( ID_BASE_URL ); }
+	wxChoice* GetFormat()  { return (wxChoice*) FindWindow( ID_CHOICE_FORMAT ); }
 
-    void UpdateLayerList();
-    void UpdateLayerDescription();
-    void UpdateURL();
-    void SetServerArray(OGCServerArray &array);
-    void UpdateEnabling();
+	void UpdateLayerList();
+	void UpdateLayerDescription();
+	void UpdateURL();
+	void SetServerArray(OGCServerArray &array);
+	void UpdateEnabling();
 
-    bool m_bNewLayer;
-    bool m_bToFile;
+	bool m_bNewLayer;
+	bool m_bToFile;
 
-    int m_iXSize;
-    int m_iYSize;
-    wxString2 m_query;
-    DRECT m_area;
-    bool m_bSetting;
-    int m_iServer;
-    int m_iLayer;
-    int m_iStyle;
-    int m_iFormat;	// 0 = JPEG, 1 = PNG
-    OGCServerArray *m_pServers;
-
-private:
-    // WDR: member variable declarations for MapServerDlg.cpp
-    
-private:
-    // WDR: handler declarations for MapServerDlg.cpp
-    void OnTextToFile( wxCommandEvent &event );
-    void OnDotDotDot( wxCommandEvent &event );
-    void OnRadio( wxCommandEvent &event );
-    void OnQueryLayers( wxCommandEvent &event );
-    void OnLayer( wxCommandEvent &event );
-    void OnFormat( wxCommandEvent &event );
-    void OnServer( wxCommandEvent &event );
-    void OnSize( wxCommandEvent &event );
-    void OnBaseUrlText( wxCommandEvent &event );
-    void OnInitDialog(wxInitDialogEvent& event);
+	int m_iXSize;
+	int m_iYSize;
+	wxString2 m_strQueryURL;
+	DRECT m_area;
+	bool m_bSetting;
+	int m_iServer;
+	int m_iLayer;
+	int m_iStyle;
+	int m_iFormat;	// 0 = JPEG, 1 = PNG, 2 = GeoTIFF
+	wxString2 m_strToFile;
+	OGCServerArray *m_pServers;
 
 private:
-    DECLARE_EVENT_TABLE()
+	// WDR: member variable declarations for MapServerDlg.cpp
+	
+private:
+	// WDR: handler declarations for MapServerDlg.cpp
+	void OnTextToFile( wxCommandEvent &event );
+	void OnDotDotDot( wxCommandEvent &event );
+	void OnRadio( wxCommandEvent &event );
+	void OnQueryLayers( wxCommandEvent &event );
+	void OnLayer( wxCommandEvent &event );
+	void OnFormat( wxCommandEvent &event );
+	void OnServer( wxCommandEvent &event );
+	void OnSize( wxCommandEvent &event );
+	void OnBaseUrlText( wxCommandEvent &event );
+	void OnInitDialog(wxInitDialogEvent& event);
+
+private:
+	DECLARE_EVENT_TABLE()
 };
 
 #endif  // __MapServerDlg_H__
