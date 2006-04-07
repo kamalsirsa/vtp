8f7e4f67fbff03f77580521037860632203690d5
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2006-04-07 01:30:48 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@5067 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainSDK/vtdata/MiniDatabuf.cpp
@@ -0,0 +1,190 @@
+// (c) VTP and Stefan Roettger
+
+#include "MiniDatabuf.h"
+#include "vtLog.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <float.h>
+
+#define ERRORMSG() VTLOG1("Error!")
+
+int MiniDatabuf::MAGIC=12640;
+
+unsigned short int MiniDatabuf::INTEL_CHECK=1;
+
+// default constructor
+MiniDatabuf::MiniDatabuf()
+   {
+   xsize=ysize=zsize=0;
+   tsteps=0;
+   type=0;
+
+   swx=swy=0.0f;
+   nwx=nwy=0.0f;
+   nex=ney=0.0f;
+   sex=sey=0.0f;
+   h0=dh=0.0f;
+   t0=dt=0.0f;
+
+   scaling=1.0f;
+   bias=0.0f;
+
+   data=NULL;
+   bytes=0;
+   }
+
+MiniDatabuf::~MiniDatabuf()
+{
+	release();
+}
+
+// allocate a new memory chunk
+void MiniDatabuf::alloc(unsigned int xs,unsigned int ys,unsigned int zs,unsigned int ts,unsigned int ty)
+   {
+   unsigned int bs,cs;
+
+   unsigned char *byteptr;
+   short int *shortptr;
+   float *floatptr;
+
+   unsigned int count;
+
+   if (ty==0) bs=1;
+   else if (ty==1) bs=2;
+   else if (ty==2) bs=4;
+   else if (ty==3) bs=3;
+   else if (ty==4) bs=3;
+   else if (ty==5) bs=4;
+   else if (ty==6) bs=4;
+   else ERRORMSG();
+
+   cs=xs*ys*zs*ts;
+   bs*=cs;
+
+   if (ty==4 || ty==6) bs/=6;
+
+   if ((data=malloc(bs))==NULL) ERRORMSG();
+
+   if (ty==1)
+      for (shortptr=(short int *)data,count=0; count<cs; count++) *shortptr++=0;
+   else if (ty==2)
+      for (floatptr=(float *)data,count=0; count<cs; count++) *floatptr++=0.0f;
+   else
+      for (byteptr=(unsigned char *)data,count=0; count<bs; count++) *byteptr++=0;
+
+   bytes=bs;
+
+   xsize=xs;
+   ysize=ys;
+   zsize=zs;
+   tsteps=ts;
+   type=ty;
+   }
+
+// reset buffer
+void MiniDatabuf::reset()
+   {
+   data=NULL;
+   bytes=0;
+   }
+
+// release buffer
+void MiniDatabuf::release()
+   {
+   if (data!=NULL) free(data);
+
+   data=NULL;
+   bytes=0;
+   }
+
+// data is saved in MSB format
+void MiniDatabuf::savedata(const char *filename)
+   {
+   FILE *file;
+
+   if (data==NULL) return;
+
+   if (bytes==0) ERRORMSG();
+
+   if ((file=fopen(filename,"wb"))==NULL) ERRORMSG();
+
+   // save magic identifier
+   fprintf(file,"MAGIC=%d\n",MAGIC);
+
+   // save mandatory metadata
+   fprintf(file,"xsize=%d\n",xsize);
+   fprintf(file,"ysize=%d\n",ysize);
+   fprintf(file,"zsize=%d\n",zsize);
+   fprintf(file,"tsteps=%d\n",tsteps);
+   fprintf(file,"type=%d\n",type);
+
+   // save optional metadata
+   fprintf(file,"swx=%f\n",swx);
+   fprintf(file,"swy=%f\n",swy);
+   fprintf(file,"nwx=%f\n",nwx);
+   fprintf(file,"nwy=%f\n",nwy);
+   fprintf(file,"nex=%f\n",nex);
+   fprintf(file,"ney=%f\n",ney);
+   fprintf(file,"sex=%f\n",sex);
+   fprintf(file,"sey=%f\n",sey);
+   fprintf(file,"h0=%f\n",h0);
+   fprintf(file,"dh=%f\n",dh);
+   fprintf(file,"t0=%f\n",t0);
+   fprintf(file,"dt=%f\n",dt);
+
+   // save optional scaling
+   fprintf(file,"scaling=%f\n",scaling);
+   fprintf(file,"bias=%f\n",bias);
+
+   // save length of data chunk
+   fprintf(file,"bytes=%u\n",bytes);
+
+   // write zero byte
+   fputc(0, file);
+
+   // save data chunk
+   if (*((unsigned char *)(&INTEL_CHECK))==0)
+      {
+      if (fwrite(data,bytes,1,file)!=1) ERRORMSG();
+      fclose(file);
+      }
+   else
+      {
+      swapbytes();
+      if (fwrite(data,bytes,1,file)!=1) ERRORMSG();
+      swapbytes();
+
+      fclose(file);
+      }
+   }
+
+// swap byte ordering between MSB and LSB
+void MiniDatabuf::swapbytes()
+   {
+   unsigned int i,b;
+
+   unsigned char *ptr,tmp;
+
+   if (type==0 || (type>=3 && type<=6)) return;
+
+   if (type==1) b=2;
+   else if (type==2) b=4;
+   else ERRORMSG();
+
+   if (bytes==0 || bytes%b!=0) ERRORMSG();
+
+   ptr=(unsigned char *)data+bytes;
+
+   while (ptr!=data)
+      {
+      ptr-=b;
+
+      for (i=0; i<(b>>1); i++)
+         {
+         tmp=ptr[i];
+         ptr[i]=ptr[b-1-i];
+         ptr[b-1-i]=tmp;
+         }
+      }
+   }
---
+++ TerrainSDK/vtdata/MiniDatabuf.h
@@ -0,0 +1,61 @@
+// (c) VTP and Stefan Roettger
+
+#ifndef MINIDATABUF_H
+#define MINIDATABUF_H
+
+#include <stdio.h>
+
+/* This is a subset of a libMini class which represents a single 1D, 2D, 3D
+	or 4D data buffer, with associated metadata such as dimensions and type. */
+
+class MiniDatabuf
+{
+public:
+	//! default constructor
+	MiniDatabuf();
+	~MiniDatabuf();
+
+	// mandatory metadata
+	unsigned int xsize;  // data size along the x-axis of 1D data
+	unsigned int ysize;  // optional data size along the y-axis of 2D data
+	unsigned int zsize;  // optional data size along the z-axis of 3D data
+	unsigned int tsteps; // optional number of frames for time-dependent data
+	unsigned int type;   // 0 = unsigned byte, 1 = signed short, 2 = float, 3 = RGB, 4 = RGBA, 5 = compressed RGB, 6 = compressed RGBA
+
+	// optional metadata
+	float swx,swy; // SW corner of data brick
+	float nwx,nwy; // NW corner of data brick
+	float nex,ney; // NE corner of data brick
+	float sex,sey; // SE corner of data brick
+	float h0,dh;   // base elevation and height of data brick
+	float t0,dt;   // time frame start and exposure time
+
+	// optional scaling
+	float scaling; // scale factor of data values, default=1.0f
+	float bias;    // bias of data values, default=0.0f
+
+	// data chunk
+	void *data;         // pointer to raw data, null pointer indicates missing data
+	unsigned int bytes; // number of raw data bytes
+
+	// allocate a new memory chunk
+	void alloc(unsigned int xs,unsigned int ys,unsigned int zs,unsigned int ts=1,unsigned int ty=0);
+
+	// reset buffer
+	void reset();
+
+	// release buffer
+	void release();
+
+	// native input/output
+	void savedata(const char *filename); // data is saved in MSB format
+
+protected:
+	static int MAGIC;
+
+private:
+	static unsigned short int INTEL_CHECK;
+	void swapbytes();
+};
+
+#endif
