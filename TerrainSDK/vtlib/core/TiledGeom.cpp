53959e16be597e08dd105eebf9213be04f3a0102
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2006-04-11 11:39:49 +0000

    comment out PGM load - we are purely DB now
    git-svn-id: http://vtp.googlecode.com/svn/trunk@5084 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainSDK/vtlib/core/TiledGeom.cpp
+++ TerrainSDK/vtlib/core/TiledGeom.cpp
@@ -421,17 +421,17 @@ databuf vtTiledGeom::FetchAndCacheTile(const char *fname)
 #endif
 		m_iTileLoads++;
 
-		vtString strfname = fname;
-		if (strfname.Find(".db") != -1)
-		{
+		//vtString strfname = fname;
+		//if (strfname.Find(".db") != -1)
+		//{
 			// Load data buffer directly
 			result.loaddata(fname);
-		}
-		else if (strfname.Find(".ppm") != -1 || strfname.Find(".pgm") )
-		{
-			// Load from PGM / PPM
-			result.loadPNMdata(fname);
-		}
+		//}
+		//else if (strfname.Find(".ppm") != -1 || strfname.Find(".pgm") )
+		//{
+		//	// Load from PGM / PPM
+		//	result.loadPNMdata(fname);
+		//}
 
 		// max cache size of 0 disables caching
 		if (m_iMaxCacheSize == 0)
