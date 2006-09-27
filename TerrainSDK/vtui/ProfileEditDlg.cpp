cc5a285af7f0f1bd5995b5f1e6b62b7d27afe9a1
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2006-09-27 13:02:17 +0000

    added new ProfileEditDlg
    git-svn-id: http://vtp.googlecode.com/svn/trunk@5463 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainSDK/vtui/ProfileEditDlg.cpp
@@ -0,0 +1,248 @@
+//
+// Name: ProfileEditDlg.cpp
+//
+// Copyright (c) 2006 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#include "ProfileEditDlg.h"
+
+// WDR: class implementations
+
+BEGIN_EVENT_TABLE(ProfDlgView, wxScrolledWindow)
+	EVT_MOUSE_EVENTS(ProfDlgView::OnMouseEvent)
+	EVT_SIZE(ProfDlgView::OnSize)
+END_EVENT_TABLE()
+
+ProfDlgView::ProfDlgView(wxWindow* parent, wxWindowID id, const wxPoint& pos,
+	const wxSize& size, long style, const wxString& name) :
+		wxScrolledWindow(parent, id, pos, size, style, name)
+{
+	m_scale = 30.0f;
+	m_bDragging = false;
+	m_mode = 0;
+}
+
+void ProfDlgView::OnSize(wxSizeEvent& event)
+{
+	m_org.x = event.GetSize().x / 2;
+	m_org.y = event.GetSize().y / 3 * 2;
+	Refresh();
+}
+
+void ProfDlgView::OnDraw(wxDC &dc)
+{
+	// Draw grid
+	wxPen pen;
+	pen.SetColour(200,200,200);
+	dc.SetPen(pen);
+
+	int x, y;
+	for (x = -20; x <= 20; x++)
+		dc.DrawLine(m_org.x + x * m_scale, m_org.y + -500,
+					m_org.x + x * m_scale, m_org.y + 500);
+	for (y = -20; y <= 20; y++)
+		dc.DrawLine(m_org.x + -500, m_org.y + y * m_scale,
+					m_org.x + 500, m_org.y + y * m_scale);
+
+	// Draw axes
+	pen.SetColour(0,0,0);
+	dc.SetPen(pen);
+
+	dc.DrawLine(m_org.x + -500, m_org.y,
+				m_org.x + 500, m_org.y);
+	dc.DrawLine(m_org.x, m_org.y + -500,
+				m_org.x, m_org.y + 500);
+
+	// Draw profile
+	int i, size = m_profile.GetSize();
+	m_screen.SetSize(size);
+	for (i = 0; i < size; i++)
+	{
+		m_screen[i].x = m_org.x + m_profile[i].x * m_scale;
+		m_screen[i].y = m_org.y - m_profile[i].y * m_scale;
+	}
+
+	// line itself
+	pen.SetColour(0,0,255);
+	dc.SetPen(pen);
+	for (i = 0; i < size-1; i++)
+	{
+		dc.DrawLine(m_screen[i].x, m_screen[i].y,
+			m_screen[i+1].x, m_screen[i+1].y);
+	}
+	// and crossed point at each vertex
+	pen.SetColour(0,100,0);
+	pen.SetWidth(2);
+	dc.SetPen(pen);
+	for (i = 0; i < size; i++)
+	{
+		dc.DrawLine(m_screen[i].x-3, m_screen[i].y-3,
+			m_screen[i].x+3, m_screen[i].y+3);
+		dc.DrawLine(m_screen[i].x+3, m_screen[i].y-3,
+			m_screen[i].x-3, m_screen[i].y+3);
+	}
+}
+
+void ProfDlgView::OnMouseEvent(wxMouseEvent &event)
+{
+	wxCoord x, y;
+	event.GetPosition(&x, &y);
+	FPoint2 fp((x-m_org.x)/m_scale, -(y-m_org.y)/m_scale);
+
+	wxEventType  ev = event.GetEventType();
+	if (ev == wxEVT_LEFT_DOWN)
+	{
+		// Add
+		if (m_mode == 0)
+		{
+			int size = m_profile.GetSize();
+			float dist1, dist2;
+			int index1 = -1, index2 = -1;
+			FPoint2 intersection;
+			if (size < 2)
+			{
+				// simple case
+				m_profile.Append(fp);
+			}
+			else
+			{
+				m_profile.NearestPoint(fp, index1, dist1);
+				bool seg = m_profile.NearestSegment(fp, index2, dist2, intersection);
+				if (!seg || dist1 < dist2)
+				{
+					// not near any segment
+					if (index1 == 0)
+						m_profile.InsertPointAfter(-1, fp);
+					else
+						m_profile.Append(fp);
+				}
+				else if (seg)
+				{
+					// insert along the line
+					m_profile.InsertPointAfter(index2, fp);
+				}
+				else
+					m_profile.Append(fp);
+			}
+			Refresh();
+		}
+		// Move
+		if (m_mode == 1)
+		{
+			int index;
+			m_profile.NearestPoint(fp, index);
+			if (index != -1)
+			{
+				m_bDragging = true;
+				m_iDragging = index;
+			}
+		}
+		// Remove
+		if (m_mode == 2)
+		{
+			int index;
+			m_profile.NearestPoint(fp, index);
+			if (index != -1)
+			{
+				m_profile.RemoveAt(index);
+				Refresh();
+			}
+		}
+	}
+	else if (ev == wxEVT_LEFT_UP) {
+		m_bDragging = false;
+	} else if (ev == wxEVT_MIDDLE_DOWN) {
+	} else if (ev == wxEVT_MIDDLE_UP) {
+	} else if (ev == wxEVT_RIGHT_DOWN) {
+	} else if (ev == wxEVT_RIGHT_UP) {
+	} else if (ev == wxEVT_MOTION) {
+		if (m_bDragging)
+		{
+			m_profile[m_iDragging] = fp;
+			Refresh();
+		}
+	} else {
+		// ignored mouse events, such as wxEVT_LEAVE_WINDOW
+		return;
+	}
+
+	//if (event1.ControlDown())
+	//if (event1.ShiftDown())
+	//if (event1.AltDown())
+}
+
+
+//----------------------------------------------------------------------------
+// ProfileEditDlg
+//----------------------------------------------------------------------------
+
+// WDR: event table for ProfileEditDlg
+
+BEGIN_EVENT_TABLE(ProfileEditDlg,wxDialog)
+	EVT_INIT_DIALOG (ProfileEditDlg::OnInitDialog)
+    EVT_RADIOBUTTON( ID_ADD_POINT, ProfileEditDlg::OnAdd )
+    EVT_RADIOBUTTON( ID_MOVE_POINT, ProfileEditDlg::OnMove )
+    EVT_RADIOBUTTON( ID_REMOVE_POINT, ProfileEditDlg::OnRemove )
+    EVT_BUTTON( ID_SAVE_PROF, ProfileEditDlg::OnSave )
+    EVT_BUTTON( ID_SAVE_AS_PROF, ProfileEditDlg::OnSaveAs )
+    EVT_BUTTON( ID_LOAD_PROF, ProfileEditDlg::OnLoad )
+    EVT_BUTTON( wxID_OK, ProfileEditDlg::OnOK )
+END_EVENT_TABLE()
+
+ProfileEditDlg::ProfileEditDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+    const wxPoint &position, const wxSize& size, long style ) :
+    wxDialog( parent, id, title, position, size, style )
+{
+    // WDR: dialog function ProfileEditDialogFunc for ProfileEditDlg
+    ProfileEditDialogFunc( this, TRUE ); 
+
+	m_pView = (ProfDlgView *) FindWindow( ID_EDIT_PANEL );
+}
+
+// WDR: handler implementations for ProfileEditDlg
+
+void ProfileEditDlg::OnInitDialog(wxInitDialogEvent& event)
+{
+	wxSize s = m_pView->GetSize();
+	m_pView->SetSize(s);
+}
+
+void ProfileEditDlg::OnOK( wxCommandEvent &event )
+{
+	event.Skip();
+}
+
+void ProfileEditDlg::OnLoad( wxCommandEvent &event )
+{
+    
+}
+
+void ProfileEditDlg::OnSaveAs( wxCommandEvent &event )
+{
+    
+}
+
+void ProfileEditDlg::OnSave( wxCommandEvent &event )
+{
+    
+}
+
+void ProfileEditDlg::OnRemove( wxCommandEvent &event )
+{
+	m_pView->m_mode = 2;
+}
+
+void ProfileEditDlg::OnMove( wxCommandEvent &event )
+{
+	m_pView->m_mode = 1;
+}
+
+void ProfileEditDlg::OnAdd( wxCommandEvent &event )
+{
+	m_pView->m_mode = 0;
+}
+
---
+++ TerrainSDK/vtui/ProfileEditDlg.h
@@ -0,0 +1,72 @@
+//
+// Name: ProfileEditDlg.h
+//
+// Copyright (c) 2006 Virtual Terrain Project
+// Free for all uses, see license.txt for details.
+//
+
+#ifndef __ProfileEditDlg_H__
+#define __ProfileEditDlg_H__
+
+#include "vtui_wdr.h"
+#include "vtdata/MathTypes.h"
+
+// WDR: class declarations
+
+class ProfDlgView : public wxScrolledWindow
+{
+public:
+	ProfDlgView(wxWindow* parent, wxWindowID id = -1, const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize, long style = wxHSCROLL | wxVSCROLL, const wxString& name = _T(""));
+
+	void OnSize(wxSizeEvent& event);
+	void OnDraw(wxDC &dc);
+	void OnMouseEvent(wxMouseEvent &event);
+
+	FLine2 m_profile;
+	vtArray<wxPoint> m_screen;
+	wxPoint m_org;
+	float m_scale;
+    int m_mode;
+	bool m_bDragging;
+	int m_iDragging;
+
+private:
+    DECLARE_EVENT_TABLE()
+};
+
+//----------------------------------------------------------------------------
+// ProfileEditDlg
+//----------------------------------------------------------------------------
+
+class ProfileEditDlg: public wxDialog
+{
+public:
+    // constructors and destructors
+    ProfileEditDlg( wxWindow *parent, wxWindowID id, const wxString &title,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize,
+        long style = wxDEFAULT_DIALOG_STYLE );
+    
+    // WDR: method declarations for ProfileEditDlg
+    
+private:
+    // WDR: member variable declarations for ProfileEditDlg
+	ProfDlgView *m_pView;
+
+private:
+    // WDR: handler declarations for ProfileEditDlg
+    void OnOK( wxCommandEvent &event );
+    void OnLoad( wxCommandEvent &event );
+    void OnSaveAs( wxCommandEvent &event );
+    void OnSave( wxCommandEvent &event );
+    void OnRemove( wxCommandEvent &event );
+    void OnMove( wxCommandEvent &event );
+    void OnAdd( wxCommandEvent &event );
+	void OnInitDialog(wxInitDialogEvent& event);
+
+private:
+    DECLARE_EVENT_TABLE()
+};
+
+#endif	// __ProfileEditDlg_H__
