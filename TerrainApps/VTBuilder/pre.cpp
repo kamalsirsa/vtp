554152bbccaf16d3e8123547699931f2f8005214
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2006-10-03 11:07:57 +0000

    added _USE_32BIT_TIME_T for VC8/VC7 compatibility
    git-svn-id: http://vtp.googlecode.com/svn/trunk@5501 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/VTBuilder/pre.cpp
+++ TerrainApps/VTBuilder/pre.cpp
@@ -1,1 +1,8 @@
+
+// In Visual C++ 2005 and later, time_t is a 64-bit integer by default.
+// That prevents binary compatibility between VC7.x and VC8.0.
+// MSDN says: "defining _USE_32BIT_TIME_T changes time_t to __time32_t and
+// forces many time functions to call versions that take the 32-bit time_t."
+#define _USE_32BIT_TIME_T
+
 #include "wx/wxprec.h"
