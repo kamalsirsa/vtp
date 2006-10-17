bdf510c45f642126d900e0a0be67e3ae44789274
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2006-10-17 08:15:25 +0000

    removed _USE_32BIT_TIME_T
    git-svn-id: http://vtp.googlecode.com/svn/trunk@5537 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/VTBuilder/pre.cpp
+++ TerrainApps/VTBuilder/pre.cpp
@@ -1,8 +1,5 @@
-
-// In Visual C++ 2005 and later, time_t is a 64-bit integer by default.
-// That prevents binary compatibility between VC7.x and VC8.0.
-// MSDN says: "defining _USE_32BIT_TIME_T changes time_t to __time32_t and
-// forces many time functions to call versions that take the 32-bit time_t."
-#define _USE_32BIT_TIME_T
+// The purpose of this file is to build the precompiled headers through the
+//  indicate header.  It isn't needed for build systems that don't support
+//  precompiled headers.
 
 #include "wx/wxprec.h"
