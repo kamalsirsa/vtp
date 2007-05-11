2cc7e22ba5eab07c01c5d135d7ca36c7b7f95316
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2007-05-11 07:05:16 +0000

    git-svn-id: http://vtp.googlecode.com/svn/trunk@6028 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ Scripts/upload_plants.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -lt 1 ] ; then
+  echo "Usage: upload_plants.sh date"
+  exit
+fi
+
+DATE=$1
+PLANTZIP=/cygdrive/c/Distrib/vtp-plantlib-${DATE}.zip
+PLANTXML=/cygdrive/c/VTP/Scripts/plant_list.html
+
+lftp -c "open vterrain.org; \
+		 user vterrain; \
+		 put -O public_html/Implementation/Plants ${PLANTXML}; \
+		 put -O public_html/dist ${PLANTZIP}"
