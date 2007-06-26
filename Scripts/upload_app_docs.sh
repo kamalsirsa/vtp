d44be02c2c95e3a4c02218290a0120c1758da6bc
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2007-06-26 09:05:27 +0000

    change CVS to .svn
    git-svn-id: http://vtp.googlecode.com/svn/trunk@6134 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- Scripts/upload_app_docs.sh
+++ Scripts/upload_app_docs.sh
@@ -6,7 +6,7 @@ lftp -c "open vterrain.org; \
 		 user vterrain; \
 		 cd public_html/Doc; \
 		 lcd $TERRAINAPPS; \
-		 mirror -R --verbose --continue --no-perms --exclude CVS BExtractor/Docs BExtractor; \
-		 mirror -R --verbose --continue --no-perms --exclude CVS --exclude _private --exclude _vti_cnf --exclude _vti_pvt Enviro/Docs Enviro; \
-		 mirror -R --verbose --continue --no-perms --exclude CVS VTBuilder/Docs VTBuilder; \
-		 mirror -R --verbose --continue --no-perms --exclude CVS CManager/Docs CManager"
+		 mirror -R --verbose --continue --no-perms --exclude .svn BExtractor/Docs BExtractor; \
+		 mirror -R --verbose --continue --no-perms --exclude .svn --exclude _private --exclude _vti_cnf --exclude _vti_pvt Enviro/Docs Enviro; \
+		 mirror -R --verbose --continue --no-perms --exclude .svn VTBuilder/Docs VTBuilder; \
+		 mirror -R --verbose --continue --no-perms --exclude .svn CManager/Docs CManager"
