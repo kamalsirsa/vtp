5438178c15963c0808450e7d360138175066a941
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2007-09-01 08:30:58 +0000

    gave VTBuilder to option to use libMini for writing tiles
    git-svn-id: http://vtp.googlecode.com/svn/trunk@6368 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/VTBuilder/LocalDatabuf.h
@@ -0,0 +1,52 @@
+//
+// LocalDatabuf.h: a wrapper for the Databuf class which is found either in
+//  in vtdata or libMini.
+//
+
+#ifndef LocalDatabuf_H
+#define LocalDatabuf_H
+
+#define USE_LIBMINI_DATABUF 0
+
+#include "vtdata/MiniDatabuf.h"
+
+#if USE_LIBMINI_DATABUF
+
+#include "database.hpp"	// part of libMini
+
+#ifdef _MSC_VER
+  #if _MSC_VER >= 1400	// vc8
+	  #pragma message( "Adding link with libMini-vc8.lib" )
+	  #pragma comment( lib, "libMini-vc8.lib" )
+  #else					// vc71
+	  #pragma message( "Adding link with libMini.lib" )
+	  #pragma comment( lib, "libMini.lib" )
+  #endif
+  #pragma comment( lib, "glu32.lib" )
+#endif
+
+// Create our subclass to add methods 'databuf' doesn't have
+class vtMiniDatabuf: public databuf
+{
+public:
+	void set_extents(float left, float right, float top, float bottom)
+	{
+		swx = left;
+		swy = bottom;
+		nwx = left;
+		nwy = top;
+		sex = right;
+		sey = bottom;
+		nex = right;
+		ney = top;
+	}
+};
+
+#else
+
+// Simply use the one in vtdata
+class vtMiniDatabuf: public MiniDatabuf {};
+
+#endif	// USE_LIBMINI_DATABUF
+
+#endif // LocalDatabuf_H
