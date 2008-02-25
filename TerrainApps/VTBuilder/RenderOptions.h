717664e71763adafd288a24f66de2deb5a9e7fec
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2008-02-25 09:21:44 +0000

    created
    git-svn-id: http://vtp.googlecode.com/svn/trunk@6699 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainApps/VTBuilder/RenderOptions.h
@@ -0,0 +1,18 @@
+
+#ifndef __RenderOptions_H__
+#define __RenderOptions_H__
+
+class RenderOptions
+{
+public:
+	int  m_iSizeX;
+	int  m_iSizeY;
+	wxString   m_strColorMap;
+	bool m_bToFile;
+	RGBi m_ColorNODATA;
+	bool m_bShading;
+	wxString m_strToFile;
+	bool m_bJPEG;
+};
+
+#endif
