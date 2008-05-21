53061645b58e0236a23a2e549e774603f59c4244
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2008-05-21 23:24:28 +0000

    expanded developer docs
    git-svn-id: http://vtp.googlecode.com/svn/trunk@6837 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/VTBuilder/DevDoc.h
+++ TerrainApps/VTBuilder/DevDoc.h
@@ -30,16 +30,54 @@ a few of the most potentially useful classes and methods:
 	The global object <b>g_Options</b> contains all the user-level options.
 	It is of type <a href="http://vterrain.org/Doc/vtdata/classvtTagArray.html">vtTagArray</a>.
 
-	The following values may be especially useful to set.  They are true/false except as noted.
-	- TAG_USE_CURRENT_CRS
-	- TAG_LOAD_IMAGES_ALWAYS
-	- TAG_LOAD_IMAGES_NEVER
-	- TAG_REPRO_TO_FLOAT_ALWAYS
-	- TAG_REPRO_TO_FLOAT_NEVER
-	- TAG_SLOW_FILL_GAPS
-	- TAG_SAMPLING_N	(N > 1 for N*N multisampling)
-	- TAG_BLACK_TRANSP
+	The following values may be especially useful to set.  They are true/false,
+	default false, except as noted.
+
+	<table style="border-collapse: collapse;">
+	<tr>
+		<th>Option</th><th>Meaning</th>
+	</tr>
+	<tr>
+		<td>TAG_USE_CURRENT_CRS</td>
+		<td>If a layer is loaded and added to the Builder object, and it lacks a
+			CRS, then the Builder's current CRS is assumed.  Recommended value for
+			non-GUI usage: true.</td>
+	</tr>
+	<tr>
+		<td>TAG_LOAD_IMAGES_ALWAYS</td>
+		<td>Always load images into memory.</td>
+	</tr>
+	<tr>
+		<td>TAG_LOAD_IMAGES_NEVER</td>
+		<td>Always load images out-of-core.</td>
+	</tr>
+	<tr>
+		<td>TAG_REPRO_TO_FLOAT_ALWAYS</td>
+		<td>Always upgrade elevation layers with integer height values to floating point upon reprojection.</td>
+	</tr>
+	<tr>
+		<td>TAG_REPRO_TO_FLOAT_NEVER</td>
+		<td>Never upgrade elevation layers with integer height values to floating point upon reprojection.</td>
+	</tr>
+	<tr>
+		<td>TAG_SLOW_FILL_GAPS</td>
+		<td>Use the slower and smoother algorithm to fill gaps in elevation grids.</td>
+	</tr>
+	<tr>
+		<td>TAG_SAMPLING_N</td>
+		<td>Set to N for NxN multisampling for imagery.</td>
+	</tr>
+	<tr>
+		<td>TAG_BLACK_TRANSP</td>
+		<td>Treat black pixels in image layers as transparent, when sampling.</td>
+	</tr>
+	<tr>
+		<td>TAG_TIFF_COMPRESS</td>
+		<td>Write TIFF files with DEFLATE compression.</td>
+	</tr>
+	</table>
 
 	<b>Example:</b>
-	<code>g_Options.SetValueBool(TAG_REPRO_TO_FLOAT_ALWAYS, true);</code>
+	<p><code>g_Options.SetValueBool(TAG_REPRO_TO_FLOAT_ALWAYS, true);</code></p>
+	<p><code>g_Options.SetValueInt(TAG_SAMPLING_N, 3, true);</code></p>
  */*/
