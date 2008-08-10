75543705846511c5a27f3e414345a4e5abadb4f9
bdiscoe <bdiscoe@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2008-08-10 01:44:08 +0000

    added some modules branched from libmini
    git-svn-id: http://vtp.googlecode.com/svn/trunk@6925 2ba57974-ef2e-0410-970c-f3cb55f6a86a

---
+++ TerrainSDK/minidata/jpegbase.h
@@ -0,0 +1,15 @@
+// (c) by Stefan Roettger
+
+#ifndef JPEGBASE_H
+#define JPEGBASE_H
+
+namespace jpegbase {
+
+unsigned char *decompressJPEGimage(unsigned char *data,unsigned int bytes,int *width,int *height,int *components);
+void compressJPEGimage(unsigned char *image,int width,int height,int components,float quality,unsigned char **data,unsigned int *bytes);
+
+}
+
+using namespace jpegbase;
+
+#endif
