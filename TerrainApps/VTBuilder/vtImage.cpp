534f09e33220fe85fe64e62cc1fbc8b3d0cc3e3f
stefan@stereofx.org <stefan@stereofx.org@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2009-03-12 18:01:25 +0000

    fix boundary condition when sampling from a layer
    git-svn-id: http://vtp.googlecode.com/svn/trunk@7142 2ba57974-ef2e-0410-970c-f3cb55f6a86a

--- TerrainApps/VTBuilder/vtImage.cpp
+++ TerrainApps/VTBuilder/vtImage.cpp
@@ -729,15 +729,15 @@ bool vtImage::GetColorSolid(const DPoint2 &p, RGBi &rgb, double dRes)
 	const IPoint2 &size = m_Bitmaps[0].m_Size;
 
 	double u = (p.x - m_Extents.left) / spacing.x;
-	if (u < -0.5 || u > size.x+0.5) return false; // check extents
+	if (u < -0.5 || u > size.x-0.5) return false; // check extents
 	if (u < 0.0) u = 0.0; // adjust left edge
-	if (u >= size.x) u = size.x-1; // adjust right edge
+	if (u >= size.x-1) u = size.x-1; // adjust right edge
 	int ix = (int) u; // round to closest pixel
 
 	double v = (m_Extents.top - p.y) / spacing.y;
-	if (v < -0.5 || v > size.y+0.5) return false; // check extents
+	if (v < -0.5 || v > size.y-0.5) return false; // check extents
 	if (v < 0.0) v = 0.0; // adjust top edge
-	if (v >= size.y) v = size.y-1; // adjust bottom edge
+	if (v >= size.y-1) v = size.y-1; // adjust bottom edge
 	int iy = (int) v; // round to closest pixel
 
 	GetRGB(ix, iy, rgb, dRes);
